bdfa8dc5f4b512b50da13c1556535de100054024
Gilberto Echeverria <gilberto.echeverria@laas.fr>
2010-04-14 16:40:42 +0200

    [project @ gilberto.echeverria@laas.fr-20100414144042-9wxecqescm4mkx4p] The motion controller that reads a pocolib poster of GenPos

---
+++ components/controllers/pocolibs_control/Control_Poster/ors_genpos_poster.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ors_genpos_poster.h"
+
+//#include <posterLib.h>
+
+
+void* locate_poster (char*	poster_name)
+{
+	void* id;
+
+	STATUS s = posterFind (poster_name, &id);
+	if (s == ERROR)
+	{
+		char buf[1024];
+		h2getErrMsg(errnoGet(), buf, sizeof(buf));
+		printf ("Unable to locate the GENPOS poster : %s\n",buf);
+		return (NULL);
+	}
+	/*
+	else
+		printf ("INIT ID = %p (pointer)   %d(integer)\n", id);
+	*/
+
+	return (id);
+}
+
+
+/*
+//int read_genPos_data( void* id, float *v, float *w )
+PyObject* read_genPos_data( void* id, float v, float w )
+{
+	GENPOS_CART_SPEED local_genPos;
+	int offset = 0;
+
+	posterRead (id, offset, &local_genPos, sizeof(GENPOS_CART_SPEED));
+
+	// Read the variables we need for the speed
+	v = local_genPos.v;
+	w = local_genPos.w;
+
+	printf ("DATA READ FROM POSTER:");
+	printf ("\tv = %.4f", v);
+	printf ("\tw = %.4f\n", w);
+
+	PyObject *tuple, *list;
+
+	// tuple = Py_BuildValue("(iis)", 1, 2, "three");
+	// list = Py_BuildValue("[iis]", 1, 2, "three");
+	tuple = Py_BuildValue("(ii)", v, w);
+
+
+	PyObject* py_v = PyInt_FromLong(v);
+	PyObject* py_w = PyInt_FromLong(w);
+
+	PyObject* resTuple = PyTuple_New(2);
+
+	PyTuple_SetItem(resTuple, 0, py_v);
+	PyTuple_SetItem(resTuple, 1, py_w);
+
+	return (resTuple);
+}
+*/
+
+
+//int read_genPos_data( void* id, float *v, float *w )
+GENPOS_CART_SPEED read_genPos_data( void* id )
+{
+	GENPOS_CART_SPEED local_genPos;
+	int offset = 0;
+	float v;
+	float w;
+
+	posterRead (id, offset, &local_genPos, sizeof(GENPOS_CART_SPEED));
+
+	// Read the variables we need for the speed
+	v = local_genPos.v;
+	w = local_genPos.w;
+
+	// printf ("DATA READ FROM POSTER:");
+	// printf ("\tv = %.4f", v);
+	// printf ("\tw = %.4f\n", w);
+
+	return (local_genPos);
+}
+
+
+int finalize (void* id)
+{
+	posterDelete(id);
+
+	return 0;
+}
---
+++ components/controllers/pocolibs_control/Control_Poster/ors_genpos_poster.i
@@ -0,0 +1,20 @@
+%module ors_genpos_poster
+%include "typemaps.i"
+/* Apply a typemap that preserves the length of a binary string.
+   It also gets the length and passes it as an argument */
+%apply (char *STRING, int LENGTH) { (unsigned char *image_string, int length) };
+
+/*
+%include "carrays.i"
+%array_class(unsigned int, uintArray);
+*/
+
+%{
+/* Include the header in the wrapper code */
+#include "ors_genpos_poster.h"
+#include <genPos/genPosStruct.h>
+%}
+
+/* Parse the header file to generate wrappers */
+%include "ors_genpos_poster.h"
+%include "/home/gechever/openrobots/include/genPos/genPosStruct.h"
