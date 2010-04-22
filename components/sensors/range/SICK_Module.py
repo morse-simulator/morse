c3d346018d9c8e6d04c464c4d99c32be6eeee6dd
Gilberto Echeverria <gechever@samus.laas.fr>
2010-04-22 15:55:35 +0200

    Correction to the SICK sensor, so that it will not break if a ray finds a collision at the very center of the emiter. Also corrected the test blend file to be consistent with these changes.

--- components/sensors/range/SICK_Module.py
+++ components/sensors/range/SICK_Module.py
@@ -193,12 +193,13 @@ def arc_sweep(contr):
 						#print ("\tARC POINT: [%.2f, %.2f, %.2f]" % (arc_point[0], arc_point[1], arc_point[2]))
 
 						point_string = "[%.4f, %.4f, %.4f]\n" % (arc_point[0], arc_point[1], arc_point[2])
-						point_list.append(point_string)
+						#point_list.append(point_string)
 
-						# Do not move the point if it is at the origin
+						# Do not move the point if the ray intersection
+						#  happened at the origin
 						#  (because this breaks the arc and makes all
-						#  subsequent rays wrong.
-						if arc_point != [0.0, 0.0, 0.0]:
+						#  subsequent rays wrong)
+						if valid_range (arc_point, 0.1):
 							# Send the vertex to the new location
 							vertex.setXYZ(arc_point)
 
@@ -214,6 +215,12 @@ def arc_sweep(contr):
 						# Move the vertex to the computed position
 						vertex.setXYZ(vector_point)
 
+						# Add a point at 0,0,0 to the output file,
+						#  to mark that this ray did not find anything
+						point_string = "[%.4f, %.4f, %.4f]\n" % (0.0, 0.0, 0.0)
+
+					point_list.append(point_string)
+
 		# Write the detected points to a file
 		filename = "{0}_{1}.txt".format(parent,ob)
 		try:
@@ -221,6 +228,18 @@ def arc_sweep(contr):
 			write_points_to_file(filename, point_list, parent_position)
 		except KeyError:
 			print "SICK: Gyroscope not available"
+
+
+def valid_range(point_vector, radius):
+	""" A ray intersection will only be valid if it happens
+		outside of a certain radius from the source.
+		This radius should be equivalent to the size of
+		the laser emiter. """
+	if point_vector.length < radius:
+		return False
+	else:
+		return True
+
 
 
 def fill_vector(vector, point_list):
