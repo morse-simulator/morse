602116739f4372a80bee33d58a69fd39c0d1db56
Gilberto Echeverria <gechever@samus.laas.fr>
2010-05-04 18:38:23 +0200

    Beginnings of the work to implement the SICK poster Not functional yet

---
+++ components/sensors/range/Sick_Poster/ors_sick_poster.h
@@ -0,0 +1,44 @@
+#include <posterLib.h>
+#include <stdarg.h>
+#include <pom/pomStruct.h>
+#include <sick/sickStruct.h>
+#include <Python.h>
+
+
+void* init_data(char*  poster_name);
+
+struct pom_position {
+	double yaw, pitch, roll;
+	double x, y, z;
+};
+
+
+/*
+ * image_data is a RGBA image
+ * and will be exported into GREY images
+ */
+struct sick_struct {
+	size_t width, height;
+	int pom_tag;
+	const struct pom_position* sensor;
+	unsigned long tacq_sec;
+	unsigned long tacq_usec;
+};
+
+
+/*
+ * Create a full POM_SENSOR_POS
+ */
+int create_pom_sensor_pos( int blender_date, 
+							POM_SENSOR_POS* pos,			 
+							const struct pom_position* robot, 
+							const struct pom_position* sensor);
+
+/*
+ * Expect a list of sick_struct*
+ */
+int post_sick_poster(	POSTER_ID id,
+						POM_SENSOR_POS* pom_sensor_pos,
+						char* sick_data);
+
+int finalize ( void* id );
