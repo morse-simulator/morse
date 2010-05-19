588b20ff2eff339771b19f5c3d409fa6255194ca
Gilberto Echeverria <gechever@samus.laas.fr>
2010-05-19 20:26:25 +0200

    Make Morse_Object_Class an abstract class
    Make the class inherit from the Abstract Base Class metaclass in Python

--- scripts/helpers/Morse_Object.py
+++ scripts/helpers/Morse_Object.py
@@ -1,16 +1,22 @@
+from abc import ABCMeta, abstractmethod
 import GameLogic
+import MorseTransformation
 
 class Morse_Object_Class(object):
 	""" Basic Class for all 3D objects (components) used in the simulation.
 		Provides common attributes. """
 
+	# Make this an abstract class
+	__metaclass__ = ABCMeta
+
 	def __init__ (self, obj, parent=None):
 		""" Constructor method. """
 		# Fill in the data sent as parameters
 		self.blender_obj = obj
-		self.position = obj.position
-		self.rotation = [0.0, 0.0, 0.0]
 		self.robot_parent = parent
+
+		# Create an instance of the 3d transformation class
+		self.position_3d = MorseTransformation.Transformation3d(obj)
 
 		# Define lists of dynamically added functions
 		self.action_functions = []
@@ -34,6 +40,13 @@ class Morse_Object_Class(object):
 			#  and name as parameters
 			function(self.message_data, self.blender_obj.name)
 
+
+	def default_action():
+		""" Abstract model for the default action that should be
+			implemented by all subclasses of Morse_Object_Class. """
+		pass
+
+
 	def print_position(self):
 		""" Print the current position of the blender object. """
-		print ("%s is at %s" % (self.blender_obj.name, self.position))
+		print ("%s is at %s" % (self.blender_obj.name, self.position_3d))
