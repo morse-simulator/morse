f7a36877e9cb7f12190415e8ed4adcdb274aeff2
Séverin Lemaignan <severin.lemaignan@laas.fr>
2010-05-28 16:12:28 +0200

    Removed jORS bindings
    Java-ORS bindings is not directly related to the MORSE simulator. The bindings should be maintained in a dedicated branch or another project

--- bindings/jORS/src/laas/openrobots/jORS/states/FollowState.java
+++
@@ -1,7 +0,0 @@
-package laas.openrobots.jORS.states;
-
-public enum FollowState {
-	IN_PROGRESS,
-	BLOCKED_FOR_REASON,
-	ARRIVED
-}
--- bindings/jORS/src/laas/openrobots/jORS/states/MoveStatus.java
+++
@@ -1,7 +0,0 @@
-package laas.openrobots.jORS.states;
-
-public class MoveStatus {
-	public MoveState moveState;
-	public float speed;
-	public float angle;
-}
