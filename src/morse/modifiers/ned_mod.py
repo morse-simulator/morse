09d74346f145f13065ef9c5b12ffae7b73fd2426
Gilberto Echeverria <gechever@samus.laas.fr>
2010-07-06 16:25:12 +0200

    Corrections to modifiers

---
+++ src/morse/modifiers/ned_mod.py
@@ -0,0 +1,55 @@
+import GameLogic
+
+class MorseNEDClass(object):
+	""" Convert between ENU and NED coordinates. """
+	
+	def __init__(self, obj, parent=None):
+		self.blender_obj = obj
+		#self.init_components()
+
+
+	def __del__(self):
+		""" Destructor method. """
+		pass
+
+
+	def register_component(self, component_name, component_instance, mod_data):
+		""" Add the corresponding function to a component. """
+		# Extract the information for this modifier
+		# This will be tailored for each middleware according to its needs
+		function_name = mod_data[1]
+
+		try:
+			# Get the reference to the function
+			function = getattr(self, function_name)
+		except AttributeError as detail:
+			print ("ERROR: %s. Check the 'component_config.py' file for typos" % detail)
+			return
+
+		# Choose what to do, depending on the function being used
+		# Data read functions
+		if function_name == "ned_to_blender":
+			component_instance.input_modifiers.append(function)
+		# Data write functions
+		elif function_name == "blender_to_ned":
+			component_instance.output_modifiers.append(function)
+
+
+	def blender_to_ned(self, component_instance):
+		""" Convert the coordinates from Blender to UTM reference. """
+		tmp = component_instance.modified_data[0]
+		component_instance.modified_data[0] = component_instance.modified_data[1]
+		component_instance.modified_data[1] = tmp
+		component_instance.modified_data[2] = -component_instance.modified_data[2]
+
+		return component_instance.modified_data
+
+
+	def ned_to_blender(self, component_instance):
+		""" Convert the coordinates from UTM to Blender reference. """
+		tmp = component_instance.modified_data[0]
+		component_instance.modified_data[0] = component_instance.modified_data[1]
+		component_instance.modified_data[1] = tmp
+		component_instance.modified_data[2] = -component_instance.modified_data[2]
+
+		return component_instance.modified_data
