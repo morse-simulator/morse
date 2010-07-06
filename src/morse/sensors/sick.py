578a5bff694b46810437ed81cd23f9a51a8d85aa
Gilberto Echeverria <gechever@samus.laas.fr>
2010-07-06 15:53:25 +0200

    Add modified_data and data_keys arrays

--- src/morse/sensors/sick.py
+++ src/morse/sensors/sick.py
@@ -29,6 +29,12 @@ class SICKClass(morse.helpers.sensor.MorseSensorClass):
 		# Create an empty list to store the intersection points
 		self.local_data['point_list'] = []
 
+		self.data_keys = ['point_list']
+
+		# Initialise the copy of the data
+		for variable in self.data_keys:
+			self.modified_data.append(self.local_data[variable])
+
 		print ('######## SICK INITIALIZED ########')
 
 
