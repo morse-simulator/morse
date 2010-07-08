f9ea4c6e8fa79dddbed9534c6b58e5cb14048925
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-07-08 11:55:40 +0200

    Make all the viam poster computation in a background thread
    In this way, we spend less time in the output function of the component,
    so the GE can run a bit faster (~ 1 fps on this box). Still a lot of
    useless copy.

--- src/morse/middleware/pocolibs/sensors/Camera_Poster/ors_viam_poster.c
+++ src/morse/middleware/pocolibs/sensors/Camera_Poster/ors_viam_poster.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <pthread.h>
+
 #include "ors_viam_poster.h"
 
 static void create_camera_calibration(viam_cameracalibration_t*, 
@@ -14,8 +16,36 @@ static int create_pom_sensor_pos( int blender_date,
 		POM_SENSOR_POS* pos,
 		const struct pom_position* robot, 
 		const struct pom_position* sensor);
+static int real_post_viam_poster(	POSTER_ID id,
+						const struct pom_position* robot,
+						size_t nb_images,
+						const struct simu_image* img1,
+                        char* image_data1,
+						const struct simu_image* img2,
+						char* image_data2
+					);
+
+static void* thread_main(void* v);
 
 #define MAGIC_CALIBRATION_STUFF		200
+
+bool abort_thr = false;
+pthread_t thr;
+pthread_mutex_t data_mutex;
+pthread_mutex_t cond_mutex;
+pthread_cond_t cond;
+
+struct internal_args {
+	POSTER_ID id;
+	struct pom_position robot;
+	size_t nb_images;
+	struct simu_image img1;
+    char* image_data1;
+	struct simu_image img2;
+	char* image_data2;
+};
+
+struct internal_args args;
 
 static void 
 fill_static_data(size_t i, size_t nb_images, ViamImageHeader* header, 
@@ -70,6 +100,11 @@ POSTER_ID init_data (char*	poster_name, const char* bank_name, size_t nb_images,
 			assert(false);
 	}
 
+	args.image_data1 = malloc(init1->width * init1->height * 4);
+	assert(args.image_data1 != NULL);
+	args.image_data2 = malloc(init2->width * init2->height * 4);
+	assert(args.image_data2 != NULL);
+
 	STATUS s = posterCreate (poster_name, poster_size, &id);
 	if (s == ERROR)
 	{
@@ -110,6 +145,12 @@ POSTER_ID init_data (char*	poster_name, const char* bank_name, size_t nb_images,
 	posterGive(id);
 
     *ok = 1;
+
+	abort_thr = false;
+	pthread_create(&thr, NULL, &thread_main, NULL);
+	pthread_mutex_init(&data_mutex, NULL);
+	pthread_mutex_init(&cond_mutex, NULL);
+	pthread_cond_init(&cond, NULL);
 
 	return (id);
 }
@@ -248,13 +289,43 @@ fill_image(ViamImageHeader* image, const struct pom_position* robot,
 	return 0;
 }
 
+int post_viam_poster(	POSTER_ID id,
+						const struct pom_position* robot,
+						size_t nb_images,
+						const struct simu_image* img1,
+                        char* image_data1,
+						const struct simu_image* img2,
+						char* image_data2
+					)
+{
+	if (pthread_mutex_trylock(&data_mutex) == 0)
+	{
+		args.id = id;
+		memcpy(&args.robot, robot, sizeof(struct pom_position));
+		args.nb_images = nb_images;
+		memcpy(&args.img1, img1, sizeof(struct simu_image));
+		memcpy(&args.img2, img2, sizeof(struct simu_image));
+		memcpy(args.image_data1, image_data1, img1->height * img1->width * 4);
+		memcpy(args.image_data2, image_data2, img2->height * img2->width * 4);
+		pthread_mutex_unlock(&data_mutex);
+
+		pthread_mutex_lock(&cond_mutex);
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&cond_mutex);
+	} else {
+		// if we can't get the lock, drop it
+		fprintf(stderr, "Drop the information, we are too slow !!!\n");
+	}
+
+	return 0;
+}
 /*
  * Fill the complete poster with information computed by the simulator
  * We still have some issues with passing args from python to C, so the
  * prototyp of the function is not really correct (image_data must be in struct
  * simu_image, and it must be an array of struct ...). Needs investigation.
  */
-int post_viam_poster(	POSTER_ID id,
+int real_post_viam_poster(	POSTER_ID id,
 						const struct pom_position* robot,
 						size_t nb_images,
 						const struct simu_image* img1,
@@ -290,10 +361,46 @@ int post_viam_poster(	POSTER_ID id,
 	return 0;
 }
 
+void* thread_main(void * unused)
+{
+	(void) unused;
+	while (!abort_thr) 
+	{
+		pthread_mutex_lock(&cond_mutex);
+		pthread_cond_wait(&cond, &cond_mutex);
+		pthread_mutex_unlock(&cond_mutex);
+
+		if (abort_thr)
+			break;
+
+		pthread_mutex_lock(&data_mutex);
+		real_post_viam_poster(args.id, &args.robot, args.nb_images,
+									   &args.img1, args.image_data1,
+									   &args.img2, args.image_data2);
+		pthread_mutex_unlock(&data_mutex);
+	}
+	
+	pthread_exit(NULL);
+}
+
 
 int finalize (POSTER_ID id)
 {
 	posterDelete(id);
+
+	abort_thr = true;
+
+	pthread_mutex_lock(&cond_mutex);
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&cond_mutex);
+
+	pthread_join(thr, NULL);
+	pthread_cond_destroy(&cond);
+	pthread_mutex_destroy(&cond_mutex);
+	pthread_mutex_destroy(&data_mutex);
+
+	free(args.image_data1);
+	free(args.image_data2);
 
 	return 0;
 }
