19ab78816f90ee4d3b9a05d049a5491c06367964
Gilberto Echeverria <gilberto@FlareCarmine.gamer.com>
2010-10-18 23:01:28 +0200

    Create a loop for the waypoint client

--- examples/morse/clients/atrv/socket_waypoint_client.py
+++ examples/morse/clients/atrv/socket_waypoint_client.py
@@ -2,39 +2,73 @@
 from socket import *
 from cPickle import *
 
-ServerIP = "140.93.0.93"
+ServerIP = "localhost"
 
 Serverport = 60000
-Clientname = ''
-ClientPort = 10001
 sClient = socket(AF_INET,SOCK_DGRAM)
 host = (ServerIP,Serverport)	
 sClient.setblocking(0)
 
+px = 0
+py = 0
+pz = 0
+speed = 1
 
+while True:
+
+#--------- ASK FOR OPTIONS ----------------#
+
+	print ("Select an option:")
+	print ("a) Enter destination")
+	print ("b) Enter speed")
+	print ("c) Read coordinates")
+	print ("q) Quit client program")
+	op = raw_input("Enter option: ")
+	
+	if op == 'a':
 
 #------------ASK FOR NEW COORDINATES--------------#	
-px = raw_input("Enter X coordinate: ")
-py = raw_input("Enter Y coordinate: ")
-pz = raw_input("Enter Z coordinate: ")
-speed = raw_input("Enter speed: ")
-#locZ = raw_input("Enter Z coordinate: ")
+		px = raw_input("Enter X coordinate: ")
+		py = raw_input("Enter Y coordinate: ")
+		pz = raw_input("Enter Z coordinate: ")
 
-waypoint = [float(px), float(py), float(pz), float(speed)]
+		waypoint = [float(px), float(py), float(pz), float(speed)]
 
-print ("Sending the command: {0}".format(waypoint))
+		print ("Sending the command: {0}".format(waypoint))
 
-#------------RECEIVE/SEND--------------#	
-Data = dumps((waypoint))
-sent = sClient.sendto(Data,host)
+# Sending
+		Data = dumps((waypoint))
+		sent = sClient.sendto(Data,host)
 
-print ("Just sent %d bytes to server" % sent)
+		print ("Just sent %d bytes to server" % sent)
 
-try:
-	Data, SRIP = sClient.recvfrom(1024)
-	UPData = loads(Data)
-	PosServer = [UPData[0],UPData[1],UPData[2]]
-	print ("The angle of the robot: {0}".format(PosServer))
-except:
-	pass
+	elif op == 'b':
+		speed = raw_input("Enter speed: ")
+		waypoint = [float(px), float(py), float(pz), float(speed)]
+		print ("Sending the command: {0}".format(waypoint))
+
+# Sending
+		Data = dumps((waypoint))
+		sent = sClient.sendto(Data,host)
+
+		print ("Just sent %d bytes to server" % sent)
+
+
+	elif op == 'c':
+# Receiving
+		try:
+			print ("Trying to read data:")
+			Data, SRIP = sClient.recvfrom(1024)
+			#UPData = loads(Data)
+			#PosServer = [UPData[0],UPData[1],UPData[2]]
+			print ("Data read: {0}".format(Data))
+		except:
+			pass
+
+	elif op == 'q':
+		break
+
+	else:
+		print ("Unknown option. Try again.")
+
 #---------------THE-END----------------#
