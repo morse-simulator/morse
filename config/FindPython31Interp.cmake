ef0ebf2b93641e4a5c783a4fadc47c36bd1ce84a
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-11-16 16:38:12 +0100

    Update CMakeLists to search for python3.1 and its libs
    XXX : it is not backward compatible (it won't search
    anymore for python 2.6, so if you still need to build it,
    you need to fix by hand PYTHON_INTERP and its friends)

---
+++ config/FindPython31Interp.cmake
@@ -0,0 +1,34 @@
+# - Find python interpreter
+# This module finds if Python interpreter is installed and determines where the
+# executables are. This code sets the following variables:
+#
+#  PYTHONINTERP3_FOUND - Was the Python executable found
+#  PYTHON3_EXECUTABLE  - path to the Python interpreter
+#
+
+#=============================================================================
+# Copyright 2005-2009 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distributed this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+FIND_PROGRAM(PYTHON3_EXECUTABLE
+  NAMES python3.1 python3
+  PATHS
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.1\\InstallPath]
+  )
+
+# handle the QUIETLY and REQUIRED arguments and set PYTHONINTERP_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Python3Interp DEFAULT_MSG PYTHON3_EXECUTABLE)
+
+MARK_AS_ADVANCED(PYTHON3_EXECUTABLE)
+
