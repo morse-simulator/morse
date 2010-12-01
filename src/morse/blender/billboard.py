a15b6a1c67e7e625b7fef7053443283d4ad8226d
Gilberto Echeverria <gechever@samus.laas.fr>
2010-12-01 15:37:43 +0100

    Add script to stop Compass object from rotating

---
+++ src/morse/blender/billboard.py
@@ -0,0 +1,16 @@
+def reset_rotation (contr):
+    """ Cancell the global rotation of the object
+
+    This will make it remain still, as a "billboard",
+    with respect to the user controlled camera
+    """
+    obj = contr.owner
+    obj.worldOrientation = [0.0, 0.0, 0.0]
+
+
+def display (contr):
+    """ Toggle showing or hiding of component """
+    if contr.sensors[0].positive:
+        obj = contr.owner
+        obj['Display'] = not obj['Display']
+        obj.setVisible(obj['Display'])
