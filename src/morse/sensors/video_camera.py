7f37d605c162002f9ddaa04d464ae9125dabe749
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-12-07 14:08:56 +0100

    Compute and store the intrinsic camera in "video_camera" object
    While here, improve the file to pass most of the pylint test

--- src/morse/sensors/video_camera.py
+++ src/morse/sensors/video_camera.py
@@ -1,7 +1,8 @@
 import GameLogic
-import VideoTexture
+import mathutils
 import morse.sensors.camera
 
+BLENDER_HORIZONTAL_APERTURE = 32.0
 
 class VideoCameraClass(morse.sensors.camera.CameraClass):
     """ Video capture camera
@@ -17,10 +18,21 @@ class VideoCameraClass(morse.sensors.camera.CameraClass):
         """
         print ("######## VIDEO CAMERA '%s' INITIALIZING ########" % obj.name)
         # Call the constructor of the parent class
-        super(self.__class__,self).__init__(obj, parent)
+        super(self.__class__, self).__init__(obj, parent)
 
         # Prepare the exportable data of this sensor
         self.local_data['image'] = ''
+
+        # Prepare the intrinsic matrix for this camera.
+        # Note that the matrix is stored in column major
+        intrinsic = mathutils.Matrix().identity()
+        alpha_u = self.image_size_X  * \
+                  self.image_focal / BLENDER_HORIZONTAL_APERTURE
+        intrinsic[0][0] = alpha_u
+        intrinsic[1][1] = alpha_u
+        intrinsic[2][0] =  self.image_size_X / 2.0
+        intrinsic[2][1] =  self.image_size_Y / 2.0
+        self.local_data['intrinsic_matrix'] = intrinsic
 
         self.capturing = False
 
@@ -40,7 +52,7 @@ class VideoCameraClass(morse.sensors.camera.CameraClass):
     def default_action(self):
         """ Update the texture image. """
         # Call the action of the parent class
-        super(self.__class__,self).default_action()
+        super(self.__class__, self).default_action()
 
         # Grab an image from the texture
         if self.blender_obj['capturing']:
