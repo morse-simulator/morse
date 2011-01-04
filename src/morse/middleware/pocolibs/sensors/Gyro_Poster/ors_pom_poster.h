e15d8af29759b2c59f4b135d94153e4ed538a3e9
Gilberto Echeverria <gechever@samus.laas.fr>
2011-01-04 18:08:38 +0100

    Correction to the poster structure of POM
    Replaced the use of POM_ME_POS for POM_SENSOR_POS, and add the
    coordinates of the robot and the sensor, just as it is done for the VIAM
    poster.
    WARNING: Still needs testing

--- src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.h
+++ src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.h
@@ -1,7 +1,15 @@
 #include <posterLib.h>
 
+struct pom_position {
+    double yaw, pitch, roll;
+    double x, y, z;
+};
+
+
 POSTER_ID init_data ( const char* poster_name, const char* reference_frame, 
 					  float confidence, int* ok);
-int post_data( POSTER_ID id, double x, double y, double z,
-							double yaw, double pitch, double roll);
+//int post_data( POSTER_ID id, double x, double y, double z,
+							//double yaw, double pitch, double roll);
+int post_data( POSTER_ID id, const struct pom_position* robot, 
+                             const struct pom_position* sensor);
 int finalize ( POSTER_ID id );
--- src/morse/middleware/pocolibs/sensors/pom.py
+++ src/morse/middleware/pocolibs/sensors/pom.py
@@ -41,12 +41,42 @@ def write_pom(self, component_instance):
     """ Write the sensor position to a poster
 
     The argument must be the instance to a morse gyroscope class. """
-    position3d = component_instance.position_3d
-
     # Get the id of the poster already created
-    poster_id = self._poster_dict[component_instance.blender_obj.name]
+    try:
+        poster_id = self._poster_dict[component_instance.blender_obj.name]
+    except AttributeError as detail:
+        print ("Something BAD happened at POM poster: %s" % detail)
+        sys.exit()
+
+    main_to_origin = component_instance.robot_parent.position_3d
+
+    pom_robot_position =  ors_pom_poster.pom_position()
+    pom_robot_position.x = main_to_origin.x
+    pom_robot_position.y = main_to_origin.y
+    pom_robot_position.z = main_to_origin.z
+    pom_robot_position.yaw = main_to_origin.yaw
+    pom_robot_position.pitch = main_to_origin.pitch
+    pom_robot_position.roll = main_to_origin.roll
+
+    #print ("POM main_to_origin: %.4f, %.4f, %.4f" % (main_to_origin.x, main_to_origin.y, main_to_origin.z))
+
+    main_to_sensor = component_instance.sensor_to_robot_position_3d()
+
+    pom_sensor_position = ors_pom_poster.pom_position()
+    pom_sensor_position.x = main_to_sensor.x
+    pom_sensor_position.y = main_to_sensor.y
+    pom_sensor_position.z = main_to_sensor.z
+    pom_sensor_position.yaw = main_to_sensor.yaw
+    pom_sensor_position.pitch = main_to_sensor.pitch
+    pom_sensor_position.roll = main_to_sensor.roll
+
+    #print ("POM main_to_sensor: %.4f, %.4f, %.4f" % (main_to_sensor.x, main_to_sensor.y, main_to_sensor.z))
+
+    ors_pom_poster.write_pom_poster(poster_id, pom_robot_position, pom_sensor_position)
+
+    """
     ors_pom_poster.post_data(poster_id,
             position3d.x, position3d.y,
             position3d.z, position3d.yaw,
             position3d.pitch, position3d.roll)
-    #print ("POM position: %.4f, %.4f, %.4f" % (position3d.x, position3d.y, position3d.z))
+    """
