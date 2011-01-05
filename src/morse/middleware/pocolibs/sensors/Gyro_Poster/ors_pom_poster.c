9409357933cc0e96f582a17ba0af88f64abdc98f
Gilberto Echeverria <gechever@samus.laas.fr>
2011-01-05 15:47:50 +0100

    Revert previous changes and give robot coordinates
    Go back to using the POM_ME_POSTER data structure.
    Also now pass the coordinates of the robot, instead of those of the
    sensor.

--- src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.c
+++ src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.c
@@ -38,52 +38,11 @@ POSTER_ID init_data (const char* poster_name, const char* reference_frame,
 	return (id);
 }
 
-
-/*
- * Fill a POM_SENSOR_POS on the base of information returned by the simulator
- */
-int
-create_pom_sensor_pos( int blender_date, 
-							POM_SENSOR_POS* pos,			 
-							const struct pom_position* robot, 
-							const struct pom_position* sensor)
-{
-	// Fill in the Sensor to Main
-	POM_EULER* local_stm_euler = & (pos->sensorToMain.euler);
-	local_stm_euler->yaw = sensor->yaw;
-	local_stm_euler->pitch = sensor->pitch;
-	local_stm_euler->roll = sensor->roll;
-
-	local_stm_euler->x = sensor->x;
-	local_stm_euler->y = sensor->y;
-	local_stm_euler->z = sensor->z;
-
-	// Fill in the Main to Origin
-	POM_EULER* local_mto_euler = &(pos->mainToOrigin.euler);
-	local_mto_euler->yaw = robot->yaw;
-	local_mto_euler->pitch = robot->pitch;
-	local_mto_euler->roll = robot->roll;
-
-	local_mto_euler->x = robot->x;
-	local_mto_euler->y = robot->y;
-	local_mto_euler->z = robot->z;
-
-	memcpy(&pos->mainToBase.euler, &pos->mainToOrigin.euler, sizeof(POM_EULER));
-
-	pos->date = blender_date;
-	pos->pad = 0;
-
-	return 0;
-}
-
-
-
-
 /*
  * Yaw, pitch, roll are in degree in input
  */
-int post_data( POSTER_ID id, const struct pom_position* robot, 
-                             const struct pom_position* sensor) 
+int post_data( POSTER_ID id, double x, double y, double z, 
+						     double yaw, double pitch, double roll)
 {
 	// Variables to use for writing the poster
 	int offset = 0;
@@ -104,23 +63,21 @@ int post_data( POSTER_ID id, const struct pom_position* robot,
 
 	posterRead(ref_id, 0, &framePos, sizeof(POM_SENSOR_POS));
 
-	//POM_ME_POS* pos = posterAddr(id);
-	POM_SENSOR_POS* pos = posterAddr(id);
+	POM_ME_POS* pos = posterAddr(id);
 	posterTake(id, POSTER_WRITE);
 	// Fill in the POM_POS_EULER
 	// yaw, pitch, roll are expected in radian
 
-	create_pom_sensor_pos(framePos.date, pos, robot, sensor);
 
-	//pos->main.euler.yaw = yaw;
-	//pos->main.euler.pitch = pitch;
-	//pos->main.euler.roll = roll;
+	pos->main.euler.yaw = yaw;
+	pos->main.euler.pitch = pitch;
+	pos->main.euler.roll = roll;
 
-	//pos->main.euler.x = x;
-	//pos->main.euler.y = y;
-	//pos->main.euler.z = z;
+	pos->main.euler.x = x;
+	pos->main.euler.y = y;
+	pos->main.euler.z = z;
 
-    //pos->date1 = framePos.date;
+	pos->date1 = framePos.date;
 	posterGive(id);
 	return 0;
 }
--- src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.h
+++ src/morse/middleware/pocolibs/sensors/Gyro_Poster/ors_pom_poster.h
@@ -1,15 +1,8 @@
 #include <posterLib.h>
 
-struct pom_position {
-    double yaw, pitch, roll;
-    double x, y, z;
-};
-
 
 POSTER_ID init_data ( const char* poster_name, const char* reference_frame, 
 					  float confidence, int* ok);
-//int post_data( POSTER_ID id, double x, double y, double z,
-							//double yaw, double pitch, double roll);
-int post_data( POSTER_ID id, const struct pom_position* robot, 
-                             const struct pom_position* sensor);
+int post_data( POSTER_ID id, double x, double y, double z,
+							double yaw, double pitch, double roll);
 int finalize ( POSTER_ID id );
