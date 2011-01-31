0b56a2dc65570730683451d31101410b6d35cfde
Gilberto Echeverria <gechever@samus.laas.fr>
2011-01-31 18:05:37 +0100

    Implement the healing of the victim from the robot

--- src/morse/blender/victim.py
+++ src/morse/blender/victim.py
@@ -1,13 +1,26 @@
 import GameLogic
 
+def init(contr):
+    """ Set the mesh color to red """
+    obj = contr.owner
+    obj.color = [1.0, 0.5, 0.5, 1.0]
+
+
 def heal(contr):
     """ Change the status of the victim
     
     Change the material to a green color,
     and the status to healed.
     """
-
     obj = contr.owner
 
-    obj['Injured'] = False
-    obj['Gravity'] = 'small'
+    if obj['Severity'] > 0:
+        obj['Severity'] = obj['Severity'] -1
+        # Set the colors depending on the severity of the injuries
+        red = 1 - obj['Severity'] * 0.05
+        green = 0.5 + red
+        obj.color = [red, green, 0.5, 1.0]
+
+    # When fully healed, mark as not injured
+    if obj['Severity'] == 0:
+        obj['Injured'] = False
