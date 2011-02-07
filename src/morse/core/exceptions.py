9c4b1287a83f0b91729966fe55fac830f2b15ba9
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-02-07 15:18:55 +0100

    Added a specific file to store MORSE exceptions

---
+++ src/morse/core/exceptions.py
@@ -0,0 +1,12 @@
+
+class MorseServiceError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class MorseRPCInvokationError(MorseServiceError):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
