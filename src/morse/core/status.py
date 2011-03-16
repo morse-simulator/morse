d0bb819ab1db477c4fa67401e140acc60c05eef9
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-03-16 18:26:33 +0100

    Added status constants

---
+++ src/morse/core/status.py
@@ -0,0 +1,8 @@
+
+#Request status constants
+
+SUCCESS = "SUCCESS" #The request was successfully achieved
+PREEMPTED = "PREEMPTED" #The request was interupted and replaced by another one
+FAILED = "FAILED" #The request failed.
+
+
