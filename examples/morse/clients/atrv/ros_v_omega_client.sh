743c09aa7d48585dbadc816e0d58405ee6eb2c84
pierriko <pierrick.koch@gmail.com>
2011-04-04 14:33:51 +0700

    add ros scripts

---
+++ examples/morse/clients/atrv/ros_v_omega_client.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+while :
+do
+  echo "Select an option:"
+  echo "a) Enter speed"
+  echo "b) Read coordinates"
+  echo "q) Quit client program"
+  read -p "Enter option: " OPTION
+  case $OPTION in
+  a) 
+    read -p "Enter V speed: " SPEEDV
+    read -p "Enter W speed: " SPEEDW
+    rostopic pub /ATRV/Motion_Controller geometry_msgs/Twist -1 [$SPEEDV,0,0] [0,0,$SPEEDW];;
+  b) 
+    rostopic echo -n 1 /ATRV/Gyroscope;;
+  q) 
+    break;;
+  *) 
+    echo "Unknown option. Try again."
+  esac
+done
+
