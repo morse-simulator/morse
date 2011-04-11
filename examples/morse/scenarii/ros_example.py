a33b7f065acd62d67e2537394a5f3d04557da12b
pierriko <pierrick.koch@gmail.com>
2011-04-11 14:23:22 +0700

    add middleware configuration

--- examples/morse/scenarii/ros_example.py
+++ examples/morse/scenarii/ros_example.py
@@ -1,39 +1,24 @@
-import morse.builder.morsebuilder as morse
+from morse.builder.morsebuilder import *
 
-# Test the API
 # http://www.openrobots.org/morse/doc/latest/user/tutorial.html
 
-# Add ATRV robot to the scene
-atrv = morse.Robot('atrv')
+# Append ATRV robot to the scene
+atrv = Robot('atrv')
 
-# Link an actuator
-motion = morse.Controller('morse_vw_control')
-motion.location = (0, 0, 0.3)
+# Append an actuator
+motion = Controller('morse_vw_control')
+motion.translate(z=0.3)
 atrv.append(motion)
 
-# Link a Gyroscope sensor
-gyroscope = morse.Sensor('morse_gyroscope')
-gyroscope.location = (0, 0, 0.83)
+# Append a Gyroscope sensor
+gyroscope = Sensor('morse_gyroscope')
+gyroscope.translate(z=0.83)
 atrv.append(gyroscope)
 
 # Insert the middleware object
-socket = morse.Middleware('socket_empty')
-ros = morse.Middleware('ros_empty')
+ros = Middleware('ros_empty')
 
-conf = morse.Config()
-# Associate each component to a middleware method
-conf.link(gyroscope, ['ROS', 'post_message'])
-conf.link(motion, ['ROS', 'read_twist', 'morse/middleware/ros/read_vw_twist'])
-
-conf.init()
-
-
-"""
-TODO easier way to link component to middleware, like:
-  conf.linkV2(gyroscope, morse.middleware.ros.post_message)
-  conf.linkV2(motion, morse.middleware.ros.read_vw_twist)
-
-TIPS use Blender in debug mode (-d) to watch bpy calls
-  blender -d 2>/dev/null | grep bpy
-"""
+# Configuring the middlewares
+ros.configure(gyroscope)
+ros.configure(motion)
 
