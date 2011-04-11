bab3adc61544618eb1938ece8e6288ece7f76de0
Gilberto Echeverria <gechever@samus.laas.fr>
2011-04-11 16:46:47 +0200

    Add request to reset objects in the scene

--- src/morse/core/supervision_services.py
+++ src/morse/core/supervision_services.py
@@ -1,8 +1,23 @@
 import GameLogic
 from morse.core.services import service
 from morse.core import status
+from morse.blender.main import reset_objects as main_reset
 
 @service(component = "simulation")
 def list_robots():
+    """ Return a list of the robots in the current scenario
+
+    Uses the list generated during the initialisation of the scenario
+    """
     return (status.SUCCESS, [obj.name for obj in GameLogic.robotDict.keys()])
 
+@service(component = "simulation")
+def reset_objects():
+    """ Restore all simulation objects to their original position
+
+    Upon receiving the request using sockets, call the
+    'reset_objects' function located in morse/blender/main.py
+    """
+    contr = GameLogic.getCurrentController()
+    main_reset(contr)
+    return (status.SUCCESS, "Objects restored to initial position")
