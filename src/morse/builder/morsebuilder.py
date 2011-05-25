c6277db146a84dbb8e4bbe2d6034d12394b78150
pierriko <pierrick.koch@gmail.com>
2011-05-25 15:55:42 +0700

    modify the morse builder API
    for futur use of Blender 2.57, we wont need the
    MORSE_COMPONENTS_DICT, thanks to bpy.data.libraries.load,
    but we should have some constraint about the components data.
    ie. only 1 parent component per file with any child.

--- src/morse/builder/morsebuilder.py
+++ src/morse/builder/morsebuilder.py
@@ -25,17 +25,25 @@ class Component(object):
   """
   _config = Configuration()
   def __init__(self, category, name):
-    objlist = [{'name':obj} for obj in MORSE_COMPONENTS_DICT[category][name]]
-    objpath = os.path.join(MORSE_COMPONENTS, category, name + '.blend/Object/')
+    filepath = os.path.join(MORSE_COMPONENTS, category, name + '.blend')
+    if bpy.app.version[1] > 56:
+      with bpy.data.libraries.load(filepath) as (src, _):
+        objlist = [{'name':obj} for obj in src.objects]
+    else: # Blender 2.56 does not support bpy.data.libraries.load
+      objlist = [{'name':obj} for obj in MORSE_COMPONENTS_DICT[category][name]]
     bpy.ops.object.select_all(action='DESELECT')
-    bpy.ops.wm.link_append(directory=objpath, link=False, files=objlist)
+    bpy.ops.wm.link_append(directory=filepath + '/Object/', link=False, files=objlist)
     # is there a way to get back the object, or its imported name, once we did?
     self._blendname = name # for middleware dictionary
+    # here we use the fact that after appending, Blender select the objects 
+    # and the root (parent) object first ( [0] )
     self._blendobj = bpy.context.selected_objects[0]
   def append(self, obj):
     """ Add a child to the current object,
     eg: robot.append(sensor), will set the robot parent of the sensor.
     cf: bpy.ops.object.parent_set()
+    obj._blendobj.parent = self._blendobj
+    self._blendobj.children += obj._blendobj
     """
     opsobj = bpy.ops.object
     opsobj.select_all(action = 'DESELECT')
@@ -69,15 +77,41 @@ class Component(object):
     old = self._blendobj.rotation_euler
     self._blendobj.rotation_euler = (old[0]+x, old[1]+y, old[2]+z)
   def properties(self, **kwargs):
-    """ modify the game properties of the Blender object
+    """ add/modify the game properties of the Blender object
     http://www.blender.org/documentation/blender_python_api_2_57_release/bpy.types.Object.html#bpy.types.Object.game
     http://www.blender.org/documentation/blender_python_api_2_57_release/bpy.types.GameObjectSettings.html#bpy.types.GameObjectSettings.properties
     http://www.blender.org/documentation/blender_python_api_2_57_release/bpy.types.GameProperty.html#bpy.types.GameProperty
     """
     prop = self._blendobj.game.properties
     for k in kwargs.keys():
-      prop[k].value = kwargs[k]
+      if k in prop.keys():
+        prop[k].value = kwargs[k]
+      else:
+        self.property_new(k, kwargs[k])
 
+  def property_new(self, n, v, t=None):
+    """ add a new game property for the Blender object
+    n: property name (string)
+    v: property value
+    t: property type (enum in ['BOOL', 'INT', 'FLOAT', 'STRING', 'TIMER'], 
+        optional, auto-detect, default=None)
+    """
+    o = self._blendobj
+    bpy.ops.object.select_all(action = 'DESELECT')
+    bpy.ops.object.select_name(name = o.name)
+    bpy.ops.object.game_property_new()
+    # select the last property in the list
+    x = o.game.properties.items()[-1][0]
+    o.game.properties[x].name = n
+    if t == None:
+      t = v.__class__.__name__.upper()
+    if t == 'STR':
+      t = 'STRING'
+    o.game.properties[n].type = t
+    o.game.properties[n].value = v
+
+class timer(float):
+  __doc__ = "this class extends float for the game properties configuration"
 
 class Robot(Component):
   def __init__(self, name):
