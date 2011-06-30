62feba194dd2c1eaa8eea54e67625da7d89e1905
Pierrick Koch <pierrick.koch@gmail.com>
2011-06-30 16:05:44 +0700

    add charging function
    TODO define the isInChargingZone for 'charging_zone' in scene:
    if the robot is near the zone, return true

--- src/morse/sensors/baterry.py
+++ src/morse/sensors/baterry.py
@@ -25,12 +25,25 @@ class BatteryClass(morse.core.sensor.MorseSensorClass):
     def default_action(self):
         """ Main function of this component. """
         newtime = time.clock()
-        c = self.local_data['charge']
+        charge = self.local_data['charge']
         dt = newtime - self._time
-        v = c - (dt * self.blender_obj['DischargingRate'])
-        self._time = newtime
+
+        if self.isInChargingZone() and charge < 100:
+            charge = charge + (dt * self.blender_obj['DischargingRate'])
+        elif charge > 0:
+            charge = charge - (dt * self.blender_obj['DischargingRate'])
 
         # Store the data acquired by this sensor that could be sent
         #  via a middleware.
-        self.local_data['charge'] = float(v)
+        self.local_data['charge'] = float(charge)
+        # update the current time
+        self._time = newtime
+
+    def isInChargingZone(self):
+        # Test if the robot (parent) is in a charging zone
+        pose = self.position_3d
+        # look for a charging zon in the scene
+        # TODO for 'charging_zone' in scene:
+        # if the robot is near the zone, return true
+        return False
 
