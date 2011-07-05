38812343dcc5c437d77f3ed407398dc386148846
Gilberto Echeverria <gechever@samus.laas.fr>
2011-07-05 20:11:55 +0200

    Add new dedicated sensor for ROSACE
    This will replace the 'healer_beam', and provides functionality specific
    to the ROSACE scenario.
    The sensor is capable of detecting victims, identifying their status,
    reporting the equipment of the robot and healing the victims.

---
+++ doc/morse/user/sensors/rosace.rst
@@ -0,0 +1,72 @@
+Rosace sensor
+=============
+
+This is a multi functional component specific for the ROSACE scenario,
+where the robot must be able to aid human victims.
+The sensor is capable of detecting any victim located within a cone in front of
+the robot, with a range delimited in the properties of the Blender object.
+The output of the sensor is a list of the robots and their positions in the
+simulated world.
+This sensor works only with the `human victim <../others/victim>` object.
+
+Additionally, the sensor provides a number of services related to the
+capabilities of the robot to help the nearest victim:
+    - Report on the condition of a victim
+    - Report the capabilities of the robot
+    - Heal a victim (if compatible capabilities and requirements)
+
+
+In the test scenarios, human victims are shown in *red*. When a robot approaches,
+if it has the adequate capabilities, it will be able to help the victims.
+When issued the command, the sensor will gradually change the colour of the
+victim to *green*, and its status to healthy.
+It will detect if a victim is in front of the robot. When instructed to heal the victim,
+it will change the Game Properties of the object to reduce its ``injured`` value.
+
+Files
+-----
+
+  - Blender: ``$MORSE_ROOT/data/morse/components/sensors/morse_rosace.blend``
+  - Python: ``$MORSE_ROOT/src/morse/sensors/rosace.py``
+
+Local data 
+----------
+
+  - **victim_dict**: (dictionary) Contains an entry for each of the victims that is
+     located inside the cone of the sensor. The value of each entry is a list containing
+     the coordinates of the victim object.
+
+Configurable parameters
+-----------------------
+
+-  **Heal_range**: (float) maximum distance from which it is possible to heal a victim. Even if
+    the victim can be detected by the sensor, it can't be healed unless its distance from the robot
+    is less than this value.
+-  **Abilities**: (string) encoded string with a list of numbers, separated by comas, that represent
+    the equipment capabilities of the robot. This information should be used by the operator of the robot
+    to determine if it is capable of helping a victim or not.
+
+The following parameters can be adjusted within the **Logic Bricks** of the ``Rosace_Sensor`` object in
+Blender, in the properties of the ``Radar`` sensor.
+
+- **Angle**: (float) aperture angle of the radar capable of detecting the victims.
+- **Distance**: (float) detection distance of the radar. Victims further away from the robot than this
+    value will not be detected.
+
+
+Services
+--------
+
+- **heal**: (Asynchronous service) Reduce the ``Severity`` value of the victim, and when the value reaches '0', change
+    the ``Injured`` status of the victim to False.
+    When the victim is healed, the sensor sends out a reply, indicating the new status of the victim.
+
+- **get_victim_requirements**: (Synchronous service) This method will return the string describing the type of injury sustained by the victim.
+
+- **get_robot_abilities**: (Synchronous service) This method will return the string describing the abilities with which the robot is equipped, and which must match the requirements of the victim for the robot to be able to heal it.
+
+
+Applicable modifiers
+--------------------
+
+No available modifiers
---
+++ src/morse/sensors/rosace.py
@@ -0,0 +1,127 @@
+######################################################
+#
+#    rosace.py        Blender 2.5x
+#
+#    Multiple sensor capable of detectin victims
+#    and helping them, according to the requirements
+#    and capabilities of the robot.
+#    Used for the Rosace scenario
+#
+#
+#    Gilberto Echeverria
+#    16 / 06 / 2011
+#
+######################################################
+
+
+import math
+import GameLogic
+import mathutils
+import morse.core.sensor
+from morse.core.services import service
+from morse.core.services import async_service
+from morse.core import status
+
+
+class RosaceSensorClass(morse.core.sensor.MorseSensorClass):
+    """ Multi function sensor/actuator for Rosace scenario
+
+    This sensor will be able to detect victims within a certain
+    distance of the robot. Additionally, it provides the following services:
+    - Report on the condition of a victim
+    - Report the capabilities of the robot
+    - Heal a victim (if compatible capabilities and requirements)
+    """
+
+    def __init__(self, obj, parent=None):
+
+        print ('######## ROSACE SENSOR INITIALIZATION ########')
+        # Call the constructor of the parent class
+        super(self.__class__,self).__init__(obj, parent)
+
+        # Flag to indicate when the component is trying to heal a victim
+        self._healing = False
+
+        # Variables to store information about the victim nearest to the robot
+        self._nearest_victim = None
+        self._nearest_distance = 999999
+
+        # List of victims visible from this sensor
+        self.local_data['victim_dict'] = {}
+
+        print ('######## ROSACE SENSOR INITIALIZED ########')
+
+
+
+    @async_service
+    def heal(self):
+        self._healing = True
+
+    @service
+    def get_robot_abilities(self):
+        return (status.SUCCESS, self.blender_obj['Abilities'])
+
+    @service
+    def get_victim_requirements(self):
+        if self._nearest_victim:
+            return (status.SUCCESS, self._nearest_victim['Requirements'])
+        else:
+            message = "No victim within range (%.2f m)" % self.blender_obj['Heal_range']
+            return (status.FAILED, message)
+
+    def _heal_victim(self):
+        """ Change the 'Severity' property of a nearby victim
+
+        When the victim is fully healed, set its status as not Injured
+        """
+        victim = self._nearest_victim
+        #print ("Healing victim %s at distance %d" % (victim.name, self._nearest_distance))
+        # Check that the victim is whithing the valid range and that
+        #  the robot is equiped to help the victim
+        if self._nearest_distance < self.blender_obj['Heal_range']:
+            # Restore the health to the victim
+            if victim['Severity'] > 0:
+                victim['Severity'] = victim['Severity'] - 1
+                # Set the colors depending on the severity of the injuries
+                red = 1 - victim['Severity'] * 0.05
+                green = 0.5 + red
+                victim.color = [red, green, 0.5, 1.0]
+
+            # Change the status
+            if victim['Severity'] == 0:
+                victim['Injured'] = False
+                # Reset the healing flag
+                self._healing = False
+                message = "Victim '%s' healed" % victim.name
+                self.completed(status.SUCCESS, message)
+        else:
+            self._healing = False
+            message = "No victim within range (%.2f m)" % self.blender_obj['Heal_range']
+            self.completed(status.SUCCESS, message)
+
+
+    def default_action(self):
+        """ Look for nearby victims, and heal when instructed to """
+        # Look for victims in the cone of the sensor
+        contr = GameLogic.getCurrentController()
+        radar = contr.sensors['Radar']
+        if radar.triggered and radar.positive:
+            for victim_obj in radar.hitObjectList:
+                victim_position = victim_obj.worldPosition
+                self.local_data['victim_dict'][victim_obj.name] = [victim_position[0], victim_position[1], victim_position[2]]
+
+                # Find the closest victim and its distance
+                new_distance = self.blender_obj.getDistanceTo(victim_obj)
+                if new_distance < self._nearest_distance:
+                    self._nearest_victim = victim_obj
+                    self._nearest_distance = new_distance
+
+            # When instructed to do so, help a victim
+            if self._healing:
+                self._heal_victim()
+
+        if radar.triggered and not radar.positive:
+            # Clear the variables for the victims
+            self.local_data['victim_dict'] = {}
+            self._nearest_victim = None
+            self._nearest_distance = 999999
