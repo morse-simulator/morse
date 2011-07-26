779432a4a4bee976bfa911422acd4624f213b27b
Michael Karg <kargm@in.tum.de>
2011-07-26 14:02:01 +0200

    Adapted middleware output to logger framework

--- src/morse/middleware/ros/odometry_sensor.py
+++ src/morse/middleware/ros/odometry_sensor.py
@@ -25,7 +25,7 @@ def init_extra_module(self, component_instance, function, mw_data):
     else:
         self._topics.append(rospy.Publisher(parent_name + "/" + component_name, Twist)) 
     
-    print('######## ROS POSE PUBLISHER INITIALIZED ########')
+    logger.info('######## ROS POSE PUBLISHER INITIALIZED ########')
 
 def post_pose(self, component_instance):
     """ Publish the data of the Odometry-sensor as a ROS-Pose message
