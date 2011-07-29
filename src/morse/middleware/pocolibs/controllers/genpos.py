2506f999ce709f0eff8f869f3181061b0e0a66ab
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-07-29 15:11:41 +0200

    Encapsulate some pocolibs middleware into classes

--- src/morse/middleware/pocolibs/controllers/genpos.py
+++ src/morse/middleware/pocolibs/controllers/genpos.py
@@ -19,10 +19,7 @@ def init_extra_module(self, component_instance, function, mw_data):
     self._poster_in_dict[component_name] = poster_id
     component_instance.input_functions.append(function)
 
-def read_genpos(self, component_instance):
-    """ Read v,w from a genPos poster """
-    # Read from the poster specified
-    poster_id = self._poster_in_dict[component_instance.blender_obj.name]
+def read_genpos_(poster_id, component_instance):
     genpos_speed, ok = ors_genpos_poster.read_genPos_data(poster_id)
     #print ("Tuple type ({0}) returned".format(type(genpos_speed)))
     #print ("Tuple data: (%.4f, %.4f)" % (genpos_speed.v, genpos_speed.w))
@@ -33,3 +30,27 @@ def read_genpos(self, component_instance):
         return True
     else:
         return False
+
+def read_genpos(self, component_instance):
+    """ Read v,w from a genPos poster """
+    # Read from the poster specified
+    poster_id = self._poster_in_dict[component_instance.blender_obj.name]
+    return read_genpos_(poster_id, component_instance)
+
+class PosterNotFound(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class GenPosPoster:
+    def __init__(self, poster_name):
+        self.poster_id = ors_genpos_poster.createPosterHandler(poster_name)
+        if not self.poster_id.found:
+            raise PosterNotFound(poster_name)
+
+    def read(self,  component_instance):
+        return read_genpos_(self.poster_id, component_instance)
+
+    def __del__(self):
+        pass
