f5b458318f08f2a1f122bbb767e236e95f1cbb61
Pierrick Koch <pierrick.koch@gmail.com>
2011-08-25 13:05:01 +0700

    [sensor.infrared] fix robot in hitObjectList

--- src/morse/sensors/infrared.py
+++ src/morse/sensors/infrared.py
@@ -1,18 +1,9 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import bge
 import morse.core.sensor
-import morse.helpers.math
 
-"""
-Morse is MW agnostic, but as help, I base this sensor on:
-http://www.ros.org/doc/api/sensor_msgs/html/msg/Range.html
-the Range message of ROS middleware
-
-based on sick laser, with a range of 20 deg.
-"""
-
-class InfraRedClass(morse.core.sensor.MorseSensorClass):
-    """ SICK laser range sensor """
+class InfraredClass(morse.core.sensor.MorseSensorClass):
+    """ Infrared radar sensor """
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -30,22 +21,26 @@ class InfraRedClass(morse.core.sensor.MorseSensorClass):
 
 
     def default_action(self):
-        """ get the object-list scanned by the radar, and get their distance
+        """ get the object-list scanned by the radar, and get their distance.
+        In order to be detected by the radar, 
+        the object must be Actor (checkbox in Physics properties)
         
-        http://www.blender.org/documentation/blender_python_api_2_58_release/bge.types.html#bge.types.KX_GameObject.getDistanceTo
+        http://www.blender.org/documentation/blender_python_api_2_59_release/bge.types.html#bge.types.KX_GameObject.getDistanceTo
+        http://www.blender.org/documentation/blender_python_api_2_59_release/bge.types.html#bge.types.KX_TouchSensor.hitObjectList
         """
         # get the controller thats running this python script
         cont = bge.logic.getCurrentController()
         # get the game object this controller is on
         infrared = cont.owner
         # get the radar sensor
-        #radar = infrared.sensors['Radar']
         radar = cont.sensors['Radar']
         tmp = self.blender_obj['ir_range']
-        min_dist = .1
+
         for obj in radar.hitObjectList:
-            dist = infrared.getDistanceTo(obj)
-            if dist > min_dist and dist < tmp:
-                tmp = dist
+            # the robot is always in the list, even if the sensor is in front
+            if obj.name != self.robot_parent.name():
+                dist = infrared.getDistanceTo(obj)
+                if dist < tmp:
+                    tmp = dist
         self.local_data['range'] = tmp
 
