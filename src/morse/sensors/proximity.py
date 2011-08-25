6e4b93b6145fa6bc8a577d6b45613fcae579fd22
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2011-08-25 11:07:38 +0200

    Add services to Proximity sensor, change 'robot' into 'object' and update doc

--- src/morse/sensors/proximity.py
+++ src/morse/sensors/proximity.py
@@ -1,6 +1,8 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import GameLogic
+
 import morse.core.sensor
+from morse.core.services import service
 
 class ProximitySensorClass(morse.core.sensor.MorseSensorClass):
     """ Distance sensor to detect nearby objects. """
@@ -15,6 +17,7 @@ class ProximitySensorClass(morse.core.sensor.MorseSensorClass):
         # Call the constructor of the parent class
         super(self.__class__,self).__init__(obj, parent)
 
+        self.local_data['near_objects'] = {}
         self.local_data['near_robots'] = {}
         
         try:
@@ -31,32 +34,39 @@ class ProximitySensorClass(morse.core.sensor.MorseSensorClass):
         
         logger.info('Component initialized')
 
+    @service
+    def set_range(self, range):
+        self._range = float(range)
+
+    @service
+    def set_tracked_tag(self, tag):
+        self._tag = tag
 
     def default_action(self):
-        """ Create a list of robots within a certain radius of the sensor. """
+        """ Create a list of tagged objects within a certain radius of the sensor. """
 
-        self.local_data['near_robots'] = {}
+        self.local_data['near_objects'] = {}
+        self.local_data['near_robots'] = self.local_data['near_objects']
 
         parent = self.robot_parent.blender_obj
 
-        # Get the fire sources
-        #for robot in GameLogic.robotDict.keys():
-        for robot in GameLogic.getCurrentScene().objects:
+        # Get the tracked sources
+        for obj in GameLogic.getCurrentScene().objects:
             try:
-                robot[self._tag]
+                obj[self._tag]
                 # Skip distance to self
-                if parent != robot:
-                    distance = self._measure_distance_to_robot (parent, robot)
+                if parent != obj:
+                    distance = self._measure_distance_to_object (parent, obj)
                     if distance <= self._range:
-                        self.local_data['near_robots'][robot.name] = distance
+                        self.local_data['near_objects'][obj.name] = distance
             except KeyError:
                 pass
 
-    def _measure_distance_to_robot(self, own_robot, target_robot):
-        """ Compute the distance between two robots
+    def _measure_distance_to_object(self, own_robot, target_object):
+        """ Compute the distance between two objects
 
         Parameters are two blender objects
         """
-        distance, globalVector, localVector = own_robot.getVectTo(target_robot)
-        logger.debug("Distance from robot {0} to robot {1} = {2}".format(own_robot, target_robot, distance))
+        distance, globalVector, localVector = own_robot.getVectTo(target_object)
+        logger.debug("Distance from robot {0} to object {1} = {2}".format(own_robot, target_object, distance))
         return distance
