4d76e023af2f3e689f7c17b816d0da5a8301dd86
Gilberto Echeverria <gechever@laas.fr>
2011-09-09 18:03:18 +0200

    Change print for logging

--- src/morse/actuators/steer_force.py
+++ src/morse/actuators/steer_force.py
@@ -1,3 +1,4 @@
+import logging; logger = logging.getLogger("morse." + __name__)
 import GameLogic
 import morse.core.actuator
 
@@ -10,7 +11,7 @@ class SteerForceActuatorClass(morse.core.actuator.MorseActuatorClass):
     """
 
     def __init__(self, obj, parent=None):
-        print ('######## STEER_FORCE CONTROL INITIALIZATION ########')
+        logger.info('%s initialization' % obj.name)
         # Call the constructor of the parent class
         super(self.__class__,self).__init__(obj, parent)
 
@@ -24,8 +25,7 @@ class SteerForceActuatorClass(morse.core.actuator.MorseActuatorClass):
         #self._type = 'Velocity'
         #self._type = 'Position'
 
-        print ('######## CONTROL INITIALIZED ########')
-
+        logger.info('Component initialized')
 
 
     def default_action(self):
