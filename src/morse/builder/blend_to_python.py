aeaf7a4eb417b06e72a6ba7789c507b2adb07c2d
Gilberto Echeverria <gechever@laas.fr>
2011-09-09 18:02:51 +0200

    Allow saving of component orientations

--- src/morse/builder/blend_to_python.py
+++ src/morse/builder/blend_to_python.py
@@ -38,9 +38,61 @@ in the folder ``MORSE_COMPONENTS/robots/``.
 morse_types = {
     "robots": "Robot",
     "sensors": "Sensor",
-    "actuators": "Actuators",
+    "actuators": "Actuator",
     "middleware": "Middleware",
+    "modifiers": "Modifier",
 }
+
+def save_translation(obj, obj_name, out_file):
+    # Set its position
+    position_string = ''
+    component_position = obj.location
+    if component_position[0] != 0:
+        position_string += 'x=%.4f' % component_position[0]
+    if component_position[1] != 0:
+        if position_string != '':
+            position_string += ', '
+        position_string += 'y=%.4f' % component_position[1]
+    if component_position[2] != 0:
+        if position_string != '':
+            position_string += ', '
+        position_string += 'z=%.4f' % component_position[2]
+    # Register a translation only if necessary
+    if position_string != '':
+        out_file.write("%s.translate(%s)\n" % (obj_name, position_string))
+
+
+def save_rotation(obj, obj_name, out_file):
+    # Set its rotation
+    rotation_string = ''
+    component_rotation = obj.rotation_euler
+    if component_rotation[0] != 0:
+        rotation_string += 'x=%.4f' % component_rotation[0]
+    if component_rotation[1] != 0:
+        if rotation_string != '':
+            rotation_string += ', '
+        rotation_string += 'y=%.4f' % component_rotation[1]
+    if component_rotation[2] != 0:
+        if rotation_string != '':
+            rotation_string += ', '
+        rotation_string += 'z=%.4f' % component_rotation[2]
+    # Register a translation only if necessary
+    if rotation_string != '':
+        out_file.write("%s.rotate(%s)\n" % (obj_name, rotation_string))
+
+
+def save_properties(obj, obj_name, out_file):
+    # Store the properties of the component
+    for key,prop in obj.game.properties.items():
+        if key not in ['Robot_Tag', 'Component_Tag', 'Middleware_Tag', 'Modifier_Tag', 'Class', 'Path']:
+            if prop.value != '':
+                if prop.type == 'STRING':
+                    out_file.write("%s.properties(%s = '%s')\n" % (obj_name, key, prop.value))
+                elif prop.type == 'FLOAT' or prop.type == 'TIMER':
+                    out_file.write("%s.properties(%s = %.4f)\n" % (obj_name, key, prop.value))
+                else:
+                    out_file.write("%s.properties(%s = %s)\n" % (obj_name, key, prop.value))
+
 
 def scan_scene (out_file):
     """ Read all the MORSE components from a Blender file
@@ -66,33 +118,15 @@ def scan_scene (out_file):
         obj_name = re.sub('\.', '_', obj.name)
         # Create the object instance
         out_file.write("%s = %s('%s')\n" % (obj_name, builder_type, component_name))
-        # Set its position
-        position_string = ''
-        component_position = obj.location
-        if component_position[0] != 0:
-            position_string += 'x=' + str(component_position[0])
-        if component_position[1] != 0:
-            if position_string != '':
-                position_string += ', '
-            position_string += 'y=' + str(component_position[1])
-        if component_position[2] != 0:
-            if position_string != '':
-                position_string += ', '
-            position_string += 'z=' + str(component_position[2])
-        # Register a translation only if necessary
-        if position_string != '':
-            out_file.write("%s.translate(%s)\n" % (obj_name, position_string))
+        save_translation(obj, obj_name, out_file)
+        save_rotation(obj, obj_name, out_file)
 
         # Assign component to the parent
         if component_type == 'sensors' or component_type == 'actuators':
             parent_name = re.sub('\.', '_', obj.parent.name)
             out_file.write("%s.append(%s)\n" % (parent_name, obj_name))
 
-            # Store the properties of the component
-            for key,value in obj.game.properties.items():
-                if key not in ['Robot_Tag', 'Component_Tag', 'Middleware_Tag', 'Modifier_Tag', 'Class', 'Path']:
-                    if value.value != '':
-                        out_file.write("%s.properties(%s = %s)\n" % (obj_name, key, value.value))
+            save_properties(obj, obj_name, out_file)
 
         out_file.write("\n")
 
@@ -108,18 +142,38 @@ def scan_config(file_out):
         mw = value[0]
         file_out.write("%s.configure_mw(%s, %s)\n" % (mw, component, value))
 
-    for key,value in component_config.component_service.items():
-        component = re.sub('\.', '_', key)
-        mw = re.search('(\w+)_request_manager', value[0])
-        file_out.write("%s.configure_service('%s')\n" % (component, mw.group(1)))
+    try:
+        component_config.component_service
+        file_out.write("\n")
+        for key,value in component_config.component_service.items():
+            component = re.sub('\.', '_', key)
+            mw = re.search('(\w+)_request_manager', value[0])
+            file_out.write("%s.configure_service('%s')\n" % (component, mw.group(1)))
+    except AttributeError as detail:
+        print ("\tNo services configured")
+
+    try:
+        component_config.component_modifier
+        file_out.write("\n")
+        for key,value in component_config.component_modifier.items():
+            component = re.sub('\.', '_', key)
+            mod = value[0]
+            file_out.write("%s.configure_modifier(%s)\n" % (component, mod))
+    except AttributeError as detail:
+        print ("\tNo modifiers configured")
+
 
 
 def main():
-    file_out = open("backup.py", "w")
+    print ("\nRunning from %s" % bpy.data.filepath)
+    filename = bpy.path.display_name_from_filepath(bpy.data.filepath) + ".py"
+    file_out = open(filename, "w")
+    print ("Saving scene robot configuration to file '%s'" % filename)
     scan_scene(file_out)
     scan_config(file_out)
     file_out.write("\nConfiguration.write()")
     file_out.close()
+    print ("Configuration saved")
 
 
 #--- ### Operator
