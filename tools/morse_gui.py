05f0f9cd55c812a0cf065e54f01b4073c223e780
Pierrick Koch <pierrick.koch@gmail.com>
2011-09-21 23:45:16 +0200

    [tools] morse GUI progress

--- tools/morse_gui.py
+++ tools/morse_gui.py
@@ -9,28 +9,28 @@ def init():
     for category in os.listdir(path):
         pathc = os.path.join(path, category)
         if os.path.isdir(pathc):
-            data[category] = []
-            for blend in os.listdir(pathc):
-                pathb = os.path.join(pathc, blend)
-                if os.path.isfile(pathb) & blend.endswith('.blend'):
-                    data[category].append(blend[:-6])
-                # go to 2nd level (for scenes)
-                elif os.path.isdir(pathb):
-                    pathb = os.path.join(pathc, blend)
-                    if os.path.isfile(pathb) and blend.endswith('.blend'):
-                        data[category].append(blend[:-6])
-    #print(data)
-    for comp in data:
-        if len(data[comp]) > 0:
-            init_prop(comp, data[comp])
+            data[category] = list_components(pathc)
+            if len(data[category]) > 0:
+                init_prop(category, data[category])
 
-def init_prop(comp, complist):
+def list_components(path, subdir=''):
+    components = []
+    for blend in os.listdir(path):
+        pathb = os.path.join(path, blend)
+        if os.path.isfile(pathb) and blend.endswith('.blend'):
+            components.append(subdir + blend[:-6])
+        # go to 2nd level (for scenes)
+        elif os.path.isdir(pathb):
+            components.extend(list_components(pathb, subdir='%s%s/'%(subdir, blend)))
+    return components
+
+def init_prop(category, components):
     objects = []
-    for index, name in enumerate(complist):
-        objects.append((str(index), name, str(index)))
+    for index, name in enumerate(components):
+        objects.append((str(index), name, category))
 
-    enum = bpy.props.EnumProperty(name=comp, description="Choose %s"%comp, items=objects, default='0')
-    setattr(bpy.types.Scene, 'enum_%s'%comp, enum)
+    enum = bpy.props.EnumProperty(name=category, description="Choose %s"%category, items=objects, default='0')
+    setattr(bpy.types.Scene, 'enum_%s'%category, enum)
 
 class MorsePanel(bpy.types.Panel):
     bl_label = "MORSE Panel"
@@ -43,43 +43,28 @@ class MorsePanel(bpy.types.Panel):
         layout = self.layout
         scene = context.scene
         
-        for comp in data:
-            if len(data[comp]) > 0:
-                #row = layout.row()
-                #row.label(text="%s: "%comp)
+        for category in data:
+            if len(data[category]) > 0:
                 row = layout.row()
-                row.prop(scene, 'enum_%s'%comp)
+                row.prop(scene, 'enum_%s'%category)
                 row = layout.row()
-                row.operator('bpt.add', text="Add MORSE %s"%comp)
-                #row.operator('bpt.add_%s'%comp)
+                btn = row.operator('bpt.add', text="Add MORSE %s"%category)
+                btn.category = category
 
 class MorseOperator(bpy.types.Operator):
     bl_idname = "bpt.add"
     bl_label = "Add MORSE Component"
+    
+    category = bpy.props.StringProperty()
 
     def execute(self, context):
-        for i in dir(self):
-            try:
-                print(i)
-                print(getattr(self,i))
-            except Exception:
-                pass
-        #print(self.properties)
-        #print(self.name)
-        #print(dir(context))
-        """
-        # TEST
-        cat = "robots"
-        select = getattr(bpy.context.scene, 'enum_%s'%cat)
-        enum = getattr(bpy.types.Scene, 'enum_%s'%cat)
-        comp = enum[1]['items'][int(select)][1]
-        print(comp)
-        Component(cat, comp)
-        """
-        return{"FINISHED"}
+        select = getattr(bpy.context.scene, 'enum_%s'%self.category)
+        enum = getattr(bpy.types.Scene, 'enum_%s'%self.category)
+        component = enum[1]['items'][int(select)][1]
+        # import the MORSE component
+        Component(self.category, component)
 
-"""    def invoke(self, context, event):
-        return self.execute(context)"""
+        return{"FINISHED"}
 
 def register():
     init()
