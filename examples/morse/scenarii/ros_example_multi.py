3a15c9f47a050dfc9c80f49a5b68c67e82ad6635
Pierrick Koch <pierrick.koch@gmail.com>
2011-09-22 11:29:14 +0200

    [examples] clean builder example

--- examples/morse/scenarii/ros_example_multi.py
+++ examples/morse/scenarii/ros_example_multi.py
@@ -1,33 +1,37 @@
 from morse.builder.morsebuilder import *
+
 # http://www.openrobots.org/morse/doc/latest/user/tutorial.html
 
-# Insert the middleware object
-ros = Middleware('ros_empty')
-
 def import_atrv(mw):
-  # Append ATRV robot to the scene
-  atrv = Robot('atrv')
-  # Append an actuator
-  motion = Controller('morse_vw_control')
-  motion.translate(z=0.3)
-  atrv.append(motion)
-  # Append a Gyroscope sensor
-  gyroscope = Sensor('morse_gyroscope')
-  gyroscope.translate(z=0.83)
-  atrv.append(gyroscope)
-  # Configuring the middlewares
-  mw.configure(gyroscope)
-  mw.configure(motion)
-  return atrv
+    # Append ATRV robot to the scene
+    atrv = Robot('atrv')
 
-atrv1 = import_atrv(ros)
+    # Append an actuator
+    motion = Actuator('v_omega')
+    motion.translate(z=0.3)
+    atrv.append(motion)
+
+    # Append a Gyroscope sensor
+    gyroscope = Sensor('gyroscope')
+    gyroscope.translate(z=0.83)
+    atrv.append(gyroscope)
+
+    # Configuring the middlewares
+    gyroscope.configure_mw(mw)
+    motion.configure_mw(mw)
+    return atrv
+
+atrv1 = import_atrv('ros')
 atrv1.translate(x=-6)
 
-atrv2 = import_atrv(ros)
+atrv2 = import_atrv('ros')
 atrv2.translate(x=-4)
 
-atrv3 = import_atrv(ros)
+atrv3 = import_atrv('ros')
 atrv3.translate(x=-2)
+
+env = Environment('indoors-1/indoor-1')
+env.aim_camera([1.0470, 0, 0.7854])
 
 # rostopic pub /ATRV/Motion_Controller geometry_msgs/Twist -1 [1,0,0] [0,0,1]
 # rostopic pub /ATRV.001/Motion_Controller.001 geometry_msgs/Twist -1 [1,0,0] [0,0,1]
