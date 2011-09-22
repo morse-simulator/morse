51624a6b0a0f8ae72f3982a33d182dd6ec04f545
Pierrick Koch <pierrick.koch@gmail.com>
2011-09-22 12:15:07 +0200

    [builder] creator: changing blendname

--- src/morse/builder/middlewares.py
+++
@@ -1,8 +0,0 @@
-import morse.builder.creator
-import morse.builder.blenderobjects
-
-class ROS(morse.builder.creator.MiddlewareCreator):
-    def __init__(self, name="ROS_Empty"):
-        morse.builder.creator.MiddlewareCreator.__init__(self, name, 
-            "morse/middleware/ros_mw", "ROSClass", "ros_empty")
-
