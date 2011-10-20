fc4da9444ea9c72bf5248809ba75cc9813da6e9c
Gilberto Echeverria <gechever@laas.fr>
2011-10-20 17:25:38 +0200

    Add a sound effect when the arm moves
    This is mostly only a test, since this arm is no longer used.

--- src/morse/actuators/pa_10.py
+++ src/morse/actuators/pa_10.py
@@ -1,5 +1,5 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-import GameLogic
+import bge
 import math
 import mathutils
 import morse.core.actuator
@@ -37,12 +37,25 @@ class PA10ActuatorClass(morse.core.actuator.MorseActuatorClass):
             self._segments.append(segment)
             segment = segment.children[0]
 
+        # Variable to store the reference to the Sound actuator
+        self._sound = None
+
+        self._moving = False
+
         logger.info('Component initialized')
+        #logger.setLevel(logging.DEBUG)
 
 
 
     def default_action(self):
         """ Apply rotation to the arm segments """
+        # Get the reference to the Sound actuator
+        if self._sound == None:
+            logger.debug ("ACTIVATING THE SOUND ACTUATOR")
+            contr = bge.logic.getCurrentController()
+            self._sound = contr.actuators['Sound']
+            contr.activate(self._sound)
+            self._sound.stopSound()
 
         # Reset movement variables
         rx, ry, rz = 0.0, 0.0, 0.0
@@ -50,7 +63,7 @@ class PA10ActuatorClass(morse.core.actuator.MorseActuatorClass):
         # Tick rate is the real measure of time in Blender.
         # By default it is set to 60, regardles of the FPS
         # If logic tick rate is 60, then: 1 second = 60 ticks
-        ticks = GameLogic.getLogicTicRate()
+        ticks = bge.logic.getLogicTicRate()
         # Scale the speeds to the time used by Blender
         try:
             rotation = self._speed / ticks
@@ -58,6 +71,8 @@ class PA10ActuatorClass(morse.core.actuator.MorseActuatorClass):
         # It happens apparently when the simulation starts
         except ZeroDivisionError:
             pass
+
+        self._moving = False
 
         for i in range(6):
             key = ('seg%d' % i)
@@ -74,13 +89,26 @@ class PA10ActuatorClass(morse.core.actuator.MorseActuatorClass):
             # Use the corresponding direction for each rotation
             if self._dofs[i] == 'y':
                 ry = morse_math.rotation_direction(segment_euler[1], target_angle, self._tolerance, rotation)
+                #logger.debug("PARAMETERS Y: %.4f, %.4f, %.4f, %.4f = %.4f" % (segment_euler[1], target_angle, self._tolerance, rotation, ry))
             elif self._dofs[i] == 'z':
                 rz = morse_math.rotation_direction(segment_euler[2], target_angle, self._tolerance, rotation)
-                logger.debug("PARAMETERS: %.4f, %.4f, %.4f, %.4f = %.4f" % (segment_euler[2], target_angle, self._tolerance, rotation, rz))
+                #logger.debug("PARAMETERS Z: %.4f, %.4f, %.4f, %.4f = %.4f" % (segment_euler[2], target_angle, self._tolerance, rotation, rz))
+
+            logger.debug("ry = %.4f, rz = %.4f" % (ry, rz))
 
             # Give the movement instructions directly to the parent
             # The second parameter specifies a "local" movement
             segment.applyRotation([rx, ry, rz], True)
 
+            if ry != 0.0 or rz != 0.0:
+                self._moving = True
+
             # Reset the rotations for the next segment
             ry = rz = 0
+
+        if self._moving:
+            self._sound.startSound()
+            logger.debug("STARTING SOUND")
+        else:
+            self._sound.stopSound()
+            logger.debug("STOPPING SOUND")
