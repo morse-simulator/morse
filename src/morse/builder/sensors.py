507839c382ca8d520e72505faaa475be93cd5298
Pierrick Koch <pierrick.koch@gmail.com>
2011-11-01 10:38:15 +0700

    [builder] creator add append mesh method

--- src/morse/builder/sensors.py
+++ src/morse/builder/sensors.py
@@ -43,36 +43,33 @@ class Sick(morse.builder.creator.SensorCreator):
     def __init__(self, name="Sick"):
         morse.builder.creator.SensorCreator.__init__(self, name, 
             "morse/sensors/sick", "SICKClass", "sick")
-        # append sick mesh, from MORSE_COMPONENTS/meshes/sensors/sick.blend
-        mesh = morse.builder.morsebuilder.Component("meshes/sensors", "sick")
-        # TODO still need only 1 root object + children
-        #      see which of the Arc_ / Cube should be the parent
-        self.append(mesh)
         # set components-specific properties
         self.properties(Visible_arc = True, laser_range = 30.0, 
                 scan_window = 180.0, resolution = 0.25)
-        # logic Always sensor frequency: 10
-        sensor = self._blendobj.game.sensors[-1]
-        sensor.frequency = 10
+        # set the frequency to 10 (6 scan/s for ticrate = 60Hz)
+        self.frequency(10)
+        # append sick mesh, from MORSE_COMPONENTS/sensors/sick.blend
+        self.append_meshes(['Arc_180', 'Sick_Model'])
 
-"""
-d = {}
-o = bpy.data.objects['Sick']
-p = o.game.properties
-for k in p.keys():
-    d[k] = p[k].value
-    if p[k].type == 'TIMER':
-        d[k] = "timer(%f)"%d[k]
-    elif type(d[k]) is str:
-        d[k] = "'%s'"%d[k]
+def get_properties_str(name):
+    """ Returns the Game properties of the Blender object represented by the name
+    ie. get_properties_str('Sick')
+    # laser_range = 30.0, Component_Tag = True, scan_window = 180.0, Visible_arc = True, Path = 'morse/sensors/sick', resolution = 0.25, Class = 'SICKClass'
+    """
+    o = bpy.data.objects[name]
+    d = get_properties(o)
+    return ", ".join(["%s = %s"%(k,d[k]) for k in d])
 
-print(d)
-# d = {'laser_range': 30.0, 'Component_Tag': True, 'scan_window': 180.0, 'Visible_arc': True, 'Path': 'morse/sensors/sick', 'resolution': 0.25, 'Class': 'SICKClass'}
-
-s = ", ".join(["%s = %s"%(k,d[k]) for k in d])
-print(s)
-# laser_range = 30.0, Component_Tag = True, scan_window = 180.0, Visible_arc = True, Path = 'morse/sensors/sick', resolution = 0.25, Class = 'SICKClass'
-"""
+def get_properties(o):
+    d = {}
+    p = o.game.properties
+    for k in p.keys():
+        d[k] = p[k].value
+        if p[k].type == 'TIMER':
+            d[k] = "timer(%f)"%d[k]
+        elif type(d[k]) is str:
+            d[k] = "'%s'"%d[k]
+    return d
 
 class Camera(morse.builder.creator.SensorCreator):
     def __init__(self, name="CameraMain"):
@@ -80,55 +77,25 @@ class Camera(morse.builder.creator.SensorCreator):
             "morse/sensors/video_camera", "VideoCameraClass", "video_camera")
         camera = morse.builder.blenderobjects.Camera("CameraRobot")
         self.append(camera)
-        self.properties(cam_width = 512, cam_height = 512, cam_focal = 35.0, 
-                flip = True, capturing = False)
+        self.properties(cam_width = 256, cam_height = 256, cam_focal = 35.0, 
+                capturing = True, Vertical_Flip = True)
+        # set the frequency to 3 (20ips for ticrate = 60Hz)
+        self.frequency(3)
         # add toggle capture action (`Space` key)
         bpy.ops.object.select_all(action = 'DESELECT')
         bpy.ops.object.select_name(name = self._blendobj.name)
         bpy.ops.logic.sensor_add(type="KEYBOARD")
-        sensor = self._blendobj.game.sensors.keys()[-1]
-        self._blendobj.game.sensors[sensor].key = 'SPACE'
+        sensor = self._blendobj.game.sensors[-1]
+        sensor.key = 'SPACE'
         bpy.ops.logic.controller_add(type='LOGIC_AND')
-        controller = self._blendobj.game.controllers.keys()[-1]
+        controller = self._blendobj.game.controllers[-1]
         bpy.ops.logic.actuator_add(type='PROPERTY')
-        actuator = self._blendobj.game.actuators.keys()[-1]
-        self._blendobj.game.actuators[actuator].mode = 'TOGGLE'
-        self._blendobj.game.actuators[actuator].property = 'capturing'
-        self._blendobj.game.controllers[controller].link( 
-                sensor = self._blendobj.game.sensors[sensor], 
-                actuator = self._blendobj.game.actuators[actuator])
-        # cf. morse.sensors.camera.VideoCameraClass._setup_video_texture
-        mesh = morse.builder.blenderobjects.Cube("CameraMesh")
-        mesh.scale = (.1, .1, .05)
-        self.append(mesh)
-        #def text(self):
-        # XXX add "MAScreenMat" to "CameraCube" (!) or "ScreenMat" (?)
-        cam = bpy.data.objects['CameraMesh']
-        uvtex = cam.data.uv_textures.new()
-        # ScreenMat
-        mat = bpy.data.materials.new("ScreenMat")
-        mat.use_shadeless = True
-        mat.use_face_texture = True
-        mtex = mat.texture_slots.add()
-        mtex.texture = bpy.data.textures.new(name='ScreenTex', type='IMAGE')
-        mtex.texture_coords = 'UV'
-        mtex.uv_layer = uvtex.name
-        cam.data.materials.append(mat)
-        # CameraMat
-        mat = bpy.data.materials.new("CameraMat")
-        mat.diffuse_color = (.14, .14, .14)
-        mat.specular_color = (.28, .28, .28)
-        mtex = mat.texture_slots.add()
-        mtex.texture = bpy.data.textures.new(name='CameraTex', type='IMAGE')
-        mtex.texture_coords = 'UV'
-        mtex.uv_layer = uvtex.name
-        cam.data.materials.append(mat)
-        print(">>>>>>>>>>>>>>  Camera done!  <<<<<<<<<<<<<<")
-"""
-                for material in screen.data.materials.keys():
-                    if 'MAScreenMat' in material:
-                        material_name = material
-"""
+        actuator = self._blendobj.game.actuators[-1]
+        actuator.mode = 'TOGGLE'
+        actuator.property = 'capturing'
+        controller.link(sensor = sensor, actuator = actuator)
+        # append CameraMesh with its textures
+        self.append_meshes(['CameraMesh'])
 
 class Battery(morse.builder.creator.SensorCreator):
     def __init__(self, name="Battery"):
