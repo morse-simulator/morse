c25bf84e856b76d17947da3315bdaa805f4ebce4
Gilberto Echeverria <gechever@laas.fr>
2011-11-14 19:44:29 +0100

    Add new tutorial examples

---
+++ examples/morse/tutorials/tutorial-1-sockets.py
@@ -0,0 +1,19 @@
+from morse.builder.morsebuilder import *
+
+# Land robot
+ATRV = Robot('atrv')
+
+Pose = Sensor('pose')
+Pose.translate(x=-0.2000, z=0.9000)
+ATRV.append(Pose)
+
+Motion_Controller = Actuator('v_omega')
+ATRV.append(Motion_Controller)
+
+
+# Scene configuration
+Motion_Controller.configure_service('socket')
+Pose.configure_service('socket')
+
+env = Environment('indoors-1/indoor-1')
+env.aim_camera([1.0470, 0, 0.7854])
