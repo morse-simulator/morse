07ac80117a162ce68a8a941ef3672d20e8a5b9f7
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-11-17 13:58:08 +0100

    Second pass on ROS services - still not complete
    The following designed has been adopted:
    
    - services declared via the @service decorator are
    created as ROS services of type 'MorseAnyService'.
    This special service definition takes rospy.msg.AnyMsg as request
    and response message.
    
    AnyMsg does not do any serialization/deserialization: it directly
    copies a (serialized) binary buffer.
    
    In short: those services are valid ROS services, but almost not usable
    by other clients (I may provide later Python examples to use anyway
    those un-typed services).
    
    - 2 new decorators (defined in middleware/ros_request_manager.py) allows
    for declaration of ROS types for services/actions: @ros_service and
    @ros_action (note that currently only ROS services are implemented).
    
    They are meant to be used in ROS specific overlays.
    
    Dummy ROS overlay example:
    
    from my_ros_node.srv import QueryPTUService
    
    class MyROSOverlay(MorseOverlay):
    
        def __init__(self, overlaid_object):
    		super(self.__class__,self).__init__(overlaid_object)
    
    	@ros_service(type = QueryPTUService)
    	def QueryPTU():
    		return (1.2, 3.4)
    
    Here, QueryPTU is a standard MORSE service (it get implicitely
    decorated with a @service), but it also stores the right ROS type.
    
    'rosservice list' would display:
    /QueryPTU
    
    and 'rosservice type /QueryPTU' would return:
    my_ros_node/QueryPTUService

--- src/morse/middleware/ros_request_manager.py
+++ src/morse/middleware/ros_request_manager.py
@@ -1,14 +1,16 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-try:
-    import roslib; roslib.load_manifest('rospy'); roslib.load_manifest('rosmorse')
-    import rospy
-    from rosmorse.srv import *
-except ImportError as ie:
-    raise ImportError("Could not import the 'rosmorse' ROS node. Check" +\
-            "your ROS configuration is ok. Details:\n" + str(ie))
 
-from morse.core import status
+from functools import partial
+
+from morse.core import status, services
 from morse.core.request_manager import RequestManager
+
+try:
+    import roslib; roslib.load_manifest('rospy')
+    import rospy
+except ImportError as ie:
+    raise ImportError("Could not import the 'rospy' ROS module. Check" +\
+            "your ROS configuration is ok. Details:\n" + str(ie))
 
 class RosRequestManager(RequestManager):
 
@@ -41,12 +43,26 @@ class RosRequestManager(RequestManager):
         In order not to interfere with neither ROS own service wrapping mechanism or
         MORSE service invokation mechanisms, we create here, 'on the fly', handlers 
         for each service exposed through ROS.
+        
+        ROS requires type for the services/actions. Those can be set with the
+        :py:method:`ros_action` and :py:method:`ros_service` decorators.
+        If none is set, a default type is used (:py:class:`MorseAnyService`).
         """
-
-        service_name = service_name.split("#")[-1]
-
+        
+        # Default service type
+        rostype = MorseAnyService
+        
+        # TODO: I access here an internal member of the parent class to 
+        # retrieve the ROS type, if set. _services should probably be 
+        # 'officially' exposed
+        method, is_async = self._services[(component_name, service_name)]
+        try:
+            rostype = method._ros_service_type
+            logger.info(component_name + "." + service_name + " is a ROS service of type " + str(rostype))
+        except AttributeError:
+            logger.info(component_name + "." + service_name + " has no ROS-specific service type. Using default one.")
+        
         cb = self.add_ros_handler(component_name, service_name)
-        rostype = self.retrieve_type(component_name, service_name)
 
         s = rospy.Service("morse/" + component_name + "/" + service_name, rostype, cb)
         logger.info("Created new ROS service for {}.{}".format(
@@ -92,20 +108,89 @@ class RosRequestManager(RequestManager):
         "return a ROS ServiceResponse"
 
         innermethod.__name__ = "_" + service_name + "_ros_handler"
-
-        #setattr(self, innermethod.__name__, innermethod)
+        
         logger.debug("Created new ROS->MORSE dispatcher for service {}.{}".format(
                                                     component_name,
                                                     service_name))
         return innermethod
-
-    def retrieve_type(self, component_name, service_name):
-        # TODO: store a dictionary somewhere with service->type
-        # matching?
-        return ListRobots
 
     def on_service_completion(self, request_id, result):
         pass
 
     def main(self):
         pass
+
+class MorseAnyService(roslib.message.ServiceDefinition):
+    _type = 'morse/AnonymousService'
+    _md5sum = ''
+    _request_class = rospy.msg.AnyMsg
+    _response_class = rospy.msg.AnyMsg
+
+
+def ros_action(fn = None, type = None, name = None):
+    """ The @ros_action decorator.
+    
+    This decorator is very similar to the standard 
+    :py:meth:`morse.core.services.async_service` decorator. It sets a 
+    class method to be a asynchronous service, exposed as a ROS action of
+    type `type`.
+    
+    This decorator can only be used on methods in classes inheriting from 
+    :py:class:`morse.core.object.MorseObjectClass`.
+    
+    :param callable fn: [automatically set by Python to point to the
+      decorated function] 
+    :param class type: you MUST set this parameter to define the
+      type of the ROS action.
+    :param string name: by default, the name of the service is the name
+      of the method. You can override it by setting the 'name' argument.
+      Your ROS action will appear as /<name>/...
+    """
+    if not type:
+        logger.error("You must provide a valid ROS action type when using the " + \
+        "@ros_action decorator, e.g. @ros_action(type=MyRosAction). Action ignored.")
+        return
+        
+    if not hasattr(fn, "__call__"):
+        return partial(ros_action, type = type, name = name)
+        
+    fn._ros_action_type = type
+    return services.service(fn, component = None, name = name, async = True)
+        
+
+def ros_service(fn = None, type = None, component = None, name = None):
+    """ The @ros_service decorator.
+    
+    This decorator is very similar to the standard 
+    :py:meth:`morse.core.services.service` decorator. It sets a free function or
+    class method to be a (synchronous) service, exposed as a ROS service of
+    type `type`.
+
+    This decorator works both with free function and for methods in
+    classes inheriting from
+    :py:class:`morse.core.object.MorseObjectClass`. In the former case,
+    you must specify a component (your service will belong to this
+    namespace), in the later case, it is automatically set to the name
+    of the corresponding MORSE component.
+
+    :param callable fn: [automatically set by Python to point to the
+      decorated function] 
+    :param class type: you MUST set this parameter to define the
+      type of the ROS action.
+    :param string component: you MUST set this parameter to define the
+      name of the component which export the service ONLY for free
+      functions. Cf explanation above.
+    :param string name: by default, the name of the service is the name
+      of the method. You can override it by setting the 'name' argument.
+      Your ROS service will appear as /<name>
+    """    
+    if not type:
+        logger.error("You must provide a valid ROS action type when using the " + \
+        "@ros_action decorator, e.g. @ros_action(type=MyRosAction). Service ignored.")
+        return
+        
+    if not hasattr(fn, "__call__"):
+        return partial(ros_service, type = type, component = component, name = name)
+
+    fn._ros_service_type = type
+    return services.service(fn, component = component, name = name, async = False)
