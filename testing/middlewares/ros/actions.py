f928e71246a6d656f78f25911e0c07973de5f2e9
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-12-05 13:48:41 +0100

    [ros] Added unit-test for ROS actions

---
+++ testing/middlewares/ros/actions.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+"""
+This script tests ROS actions within MORSE.
+"""
+
+from morse.testing.testing import MorseTestCase
+
+# Include this import to be able to use your test file as a regular 
+# builder script, ie, usable with: 'morse [run|exec] base_testing.py
+try:
+    from morse.builder.morsebuilder import *
+except ImportError:
+    pass
+
+import os
+import sys
+try:
+    os.environ['MORSE_SRC_ROOT']
+except KeyError:
+    print("You must define the environment variable MORSE_SRC_ROOT"
+          " to point to the MORSE source before running ROS tests.")
+    sys.exit(1)
+
+os.environ['ROS_PACKAGE_PATH'] += os.path.dirname(
+        os.path.join(os.environ['MORSE_SRC_ROOT'], "testing", "middlewares", "ros"))
+
+import roslib; roslib.load_manifest("morsetesting")
+import rospy
+import actionlib
+from morsetesting.msg import *
+from geometry_msgs.msg import *
+
+class RosActionsTest(MorseTestCase):
+
+    def setUpEnv(self):
+        # Identical to ROS service testing
+        
+        print("Adding a robot...")
+        robot = Robot('atrv')
+        
+        waypoint = Actuator('waypoint')
+        robot.append(waypoint)
+        
+        waypoint.configure_service('ros')
+        
+        waypoint.configure_overlay('ros', 'morse.middleware.ros.overlays.actuator.WayPoint')
+        
+        env = Environment('indoors-1/indoor-1')
+        env.configure_service('ros')
+
+    def test_no_action(self):
+            rospy.init_node('move_base_client')
+            client = actionlib.SimpleActionClient('phantom_action', MoveBaseAction)
+            self.assertFalse(client.wait_for_server(rospy.Duration(1)))
+
+    def test_move_base(self):
+
+            rospy.init_node('move_base_client')
+            client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+            self.assertTrue(client.wait_for_server(rospy.Duration(1)))
+
+            goal = MoveBaseGoal(Pose(Point(0.1,3.0,0.0), Quaternion(0.0,0.0,0.0,1.0)))
+            client.send_goal(goal)
+            client.wait_for_result(rospy.Duration.from_sec(5.0))
+
+
+
+########################## Run these tests ##########################
+if __name__ == "__main__":
+    import unittest
+    from morse.testing.testing import MorseTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(RosActionsTest)
+    sys.exit(not MorseTestRunner().run(suite).wasSuccessful())
+
