1ef8ae0ea22ba5eb0589800039afd2de5186192d
Michael Karg <kargm@in.tum.de>
2011-12-06 16:51:58 +0100

    Added services to tuck arm of PR2

--- src/morse/middleware/ros/overlays/pr2.py
+++ src/morse/middleware/ros/overlays/pr2.py
@@ -14,12 +14,34 @@ class PR2(MorseOverlay):
     @ros_service(type = GetHead)
     def get_head(self):
         #print("Head rotations: %s"%self.overlaid_object.get_rotations())
-        return self.overlaid_object.get_rotations()w
+        return self.overlaid_object.get_rotations()
     
     @ros_service(type = SetHead)
     def set_head(self, pan, tilt):
         #print("Setting head to: %s"%pan, tilt)
+        print("Channels are %s"%self.overlaid_object.get_channels())
         self.overlaid_object.set_rotation("head_pan", [0, pan, 0])
         self.overlaid_object.set_rotation("head_tilt", [0, 0, tilt])
         return True
         
+    @ros_service(type = TuckLeftArm)    
+    def tuck_left_arm(self):
+        #print("Channels are %s"%self.overlaid_object.get_channels())
+        self.overlaid_object.set_rotation("l_shoulder_pan", [0, 0, -0.06024])
+        self.overlaid_object.set_rotation("l_shoulder_lift", [-1.248526, 0, 0])
+        self.overlaid_object.set_rotation("l_upper_arm", [0, 1.78907, 0])
+        self.overlaid_object.set_rotation("l_elbow", [1.683386, 0, 0])
+        self.overlaid_object.set_rotation("l_forearm", [0, 1.7343417, 0])
+        self.overlaid_object.set_rotation("l_wrist_flex", [0.0962141, 0, 0])
+        self.overlaid_object.set_rotation("l_wrist_roll", [0, 0.0864407, 0])
+        
+    @ros_service(type = TuckRightArm)    
+    def tuck_right_arm(self):
+        #print("Channels are %s"%self.overlaid_object.get_channels())
+        self.overlaid_object.set_rotation("r_shoulder_pan", [0, 0, 0.023593])
+        self.overlaid_object.set_rotation("r_shoulder_lift", [-1.1072800, 0, 0])
+        self.overlaid_object.set_rotation("r_upper_arm", [0, 1.5566882, 0])
+        self.overlaid_object.set_rotation("r_elbow", [-2.124408, 0, 0])
+        self.overlaid_object.set_rotation("r_forearm", [0, 1.4175, 0])
+        self.overlaid_object.set_rotation("r_wrist_flex", [1.8417, 0, 0])
+        self.overlaid_object.set_rotation("r_wrist_roll", [0, -0.21436, 0])
