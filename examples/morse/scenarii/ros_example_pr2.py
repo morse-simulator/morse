481750838639a74ae5d1a7c0dcece161ab177431
Michael Karg <kargm@in.tum.de>
2011-12-13 15:12:04 +0100

    Added objects to kitchen scenario

--- examples/morse/scenarii/ros_example_pr2.py
+++ examples/morse/scenarii/ros_example_pr2.py
@@ -54,7 +54,29 @@ Motion_Controller.configure_mw('ros')
 pr2_posture.configure_mw('ros', ['ROS', 'post_jointState', 'morse/middleware/ros/pr2_posture'])
 #pr2_posture.configure_mw('ros')
 
+# Add passive objects
+cornflakes = PassiveObject('props/kitchen_objects.blend', 'Cornflakes')
+cornflakes.make_graspable(human_readable_label = 'TUM_Cornflakes')
+cornflakes.translate(x=0.5, y=1.67, z=0.9)
 
+fork = PassiveObject('props/kitchen_objects.blend', 'Fork')
+fork.make_graspable()
+fork.translate(x=0.5, y=1.87, z=0.86)
+fork.rotate(z=1.45)
 
+knife = PassiveObject('props/kitchen_objects.blend', 'Knife')
+knife.make_graspable()
+knife.translate(x=0.5, y=1.97, z=0.86)
+knife.rotate(z=1.45)
+
+plate = PassiveObject('props/kitchen_objects.blend', 'Plate')
+plate.make_graspable()
+plate.translate(x=0.5, y=1.97, z=0.86)
+plate.rotate(z=1.45)
+
+# Set scenario
 env = Environment('tum_kitchen/tum_kitchen')
 env.aim_camera([1.0470, 0, 0.7854])
+
+
+
