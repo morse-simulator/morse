099ae95f47f5c8058534d1069ce63ca05ff342c3
Michael Karg <kargm@in.tum.de>
2011-12-20 16:15:52 +0100

    Added more objects, removed bread that has wrong mesh,
    pr2_posture middleware now using updated builder dictionary

--- examples/scenarii/ros_example_pr2.py
+++ examples/scenarii/ros_example_pr2.py
@@ -51,8 +51,8 @@ Pose_sensor.configure_mw('ros')
 IMU.configure_mw('ros', ['ROS', 'post_velocity_twist', 'morse/middleware/ros/imu'])
 Sick.configure_mw('ros')
 Motion_Controller.configure_mw('ros')
-pr2_posture.configure_mw('ros', ['ROS', 'post_jointState', 'morse/middleware/ros/pr2_posture'])
-#pr2_posture.configure_mw('ros')
+#pr2_posture.configure_mw('ros', ['ROS', 'post_jointState', 'morse/middleware/ros/pr2_posture'])
+pr2_posture.configure_mw('ros')
 
 # Add passive objects
 cornflakes = PassiveObject('props/kitchen_objects.blend', 'Cornflakes')
@@ -73,6 +73,26 @@ plate = PassiveObject('props/kitchen_objects.blend', 'Plate')
 plate.make_graspable()
 plate.translate(x=0.5, y=1.97, z=0.86)
 plate.rotate(z=1.45)
+
+#bread = PassiveObject('props/kitchen_objects.blend', 'Bread')
+#bread.make_graspable()
+#bread.translate(x=0.5, y=1.97, z=0.86)
+#bread.rotate(z=1.45)
+
+bowl = PassiveObject('props/kitchen_objects.blend', 'Bowl')
+bowl.make_graspable()
+bowl.translate(x=0.5, y=1.97, z=0.86)
+bowl.rotate(z=1.45)
+
+jam = PassiveObject('props/kitchen_objects.blend', 'Jam')
+jam.make_graspable()
+jam.translate(x=0.5, y=1.97, z=0.86)
+jam.rotate(z=1.45)
+
+nutella = PassiveObject('props/kitchen_objects.blend', 'Nutella')
+nutella.make_graspable()
+nutella.translate(x=0.5, y=1.97, z=0.86)
+nutella.rotate(z=1.45)
 
 # Set scenario
 env = Environment('tum_kitchen/tum_kitchen')
