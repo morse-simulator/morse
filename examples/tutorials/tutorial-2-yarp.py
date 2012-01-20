698144e0a55e16e859f039d9fca8d2902426badd
Gilberto Echeverria <gechever@laas.fr>
2012-01-20 15:37:17 +0100

    Update example file and add camera

---
+++ examples/tutorials/tutorial-2-yarp.py
@@ -0,0 +1,24 @@
+from morse.builder.morsebuilder import *
+
+# Land robot
+ATRV = Robot('atrv')
+
+Pose = Sensor('pose')
+Pose.translate(x=-0.2000, z=0.9000)
+ATRV.append(Pose)
+
+Camera = Sensor('video_camera')
+Camera.translate(x=0.2000, z=0.9000)
+ATRV.append(Camera)
+
+Motion_Controller = Actuator('waypoint')
+ATRV.append(Motion_Controller)
+
+
+# Scene configuration
+Motion_Controller.configure_mw('yarp')
+Pose.configure_mw('yarp')
+Camera.configure_mw('yarp')
+
+env = Environment('indoors-1/indoor-1')
+env.aim_camera([1.0470, 0, 0.7854])
