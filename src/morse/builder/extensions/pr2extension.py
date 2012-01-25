cf01130d12ed4f4dd5b4bab6e5f8c5096d342c98
Sebastian Schmidt <sb_schmidt_r@mytum.de>
2012-01-25 15:40:48 +0100

    Set PR2 color through builder script

--- src/morse/builder/extensions/pr2extension.py
+++ src/morse/builder/extensions/pr2extension.py
@@ -5,11 +5,11 @@ from morse.builder.morsebuilder import *
 class PR2(Robot):
     def __init__(self):
         Component.__init__(self, 'robots', 'pr2')
-        
+
         self.head = None
         self.l_arm = None
         self.r_arm = None
-        
+
         try:
             self._blendobj = bpy.context.selected_objects[0]            
             # !!! WARNING (TODO)!!!! If we import the armature like this, it will NOT work if there are more than one PR2!
@@ -21,9 +21,17 @@ class PR2(Robot):
             self.head = AbstractComponent(head_obj, "head_armature")
             self.l_arm = AbstractComponent(l_arm_obj, "l_arm_armature")
             self.r_arm = AbstractComponent(r_arm_obj, "r_arm_armature")
+
+            
             
         except KeyError:
             logger.error("Could not find the PR2 head armature! (I was looking " +\
                          "for an object called 'head_armature' in 'pr2'" +\
                          " children). I won't be able to export the PR2 head pose" +\
                          " to any middleware.")
+
+    def set_color(self, color = (0.0, 0.0, 0.8)):
+        #set the head color
+        bpy.data.objects['head_tilt_link'].material_slots['HeadTilt'].material.node_tree.nodes['Material'].material.diffuse_color = color
+
+
