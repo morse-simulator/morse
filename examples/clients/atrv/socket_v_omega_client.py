c4469804806c3082dfdfd1e048cf6f6ef3224373
Gilberto Echeverria <gechever@laas.fr>
2012-01-27 16:37:03 +0100

    Fix sample program to work with new socket mw

--- examples/clients/atrv/socket_v_omega_client.py
+++ examples/clients/atrv/socket_v_omega_client.py
@@ -1,17 +1,15 @@
 import sys
 import socket
 import collections
-if sys.version_info >= (3,0,0):
-    import pickle
-else:
-    import cPickle as pickle
+import json
 
 
-server_ip = "localhost"
-server_port = 60000
+HOST = "localhost"
+server_motion_port = 60000
+server_pose_port = 60001
 connected = False
 
-def read_data(client_socket):
+def _read_data(client_socket):
     """ Read the input socket until no more data is available """
     finished = False
     data_in = ''
@@ -30,12 +28,12 @@ def read_data(client_socket):
         return data_in
 
 
-def print_data(data):
+def _print_data(data):
     """ Choose how to print, depending on the type """
     for item in data:
         # Recursively call this function if item is a list
         if isinstance(item, list):
-            print_data(item)
+            _print_data(item)
         elif isinstance(item, str):
             print ("\t%s" % item)
         elif isinstance(item, collections.OrderedDict):
@@ -44,64 +42,92 @@ def print_data(data):
             print ("\t%.4f" % item)
 
 
-def usage(program_name):
-    print ("Usage: {0} [server_port_number]\n", program_name)
+def _connect_port(port):
+    """ Establish the connection with the given MORSE port"""
+    local_socket = None
+
+    for res in socket.getaddrinfo(HOST, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            local_socket = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            local_socket = None
+            continue
+        try:
+            local_socket.connect(sa)
+        except socket.error as msg:
+            local_socket.close()
+            local_socket = None
+            continue
+        break
+
+    return local_socket
+
+
+
+def _usage(program_name):
+    print ("Usage: {0} [server_motion_port_number] [server_pose_port_number]\n", program_name)
 
 
 def main():
-    global server_port
+    global server_motion_port
+    global server_pose_port
     global connected
 
     # Read the arguments
     argc = len(sys.argv)
     if argc == 2:
-        server_port = int(sys.argv[1])
-    elif argc > 3:
-        usage(sys.argv[0])
+        server_motion_port = int(sys.argv[1])
+    if argc == 3:
+        server_motion_port = int(sys.argv[1])
+        server_pose_port = int(sys.argv[2])
+    elif argc > 4:
+        _usage(sys.argv[0])
         sys.exit()
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host = (server_ip, server_port)
-    client_socket.setblocking(0)
+    pose_socket = _connect_port(server_pose_port)
+    motion_socket = _connect_port(server_motion_port)
 
+    if pose_socket and motion_socket:
+        connected = True
+    else:
+        print ("Error connecting ports. Exiting!")
+        sys.exit(-1)
 
     while True:
-
-    #--------- ASK FOR OPTIONS ----------------#
-
+        #--------- ASK FOR OPTIONS ----------------#
         print ("Select an option:")
         print ("a) Enter speed")
         print ("b) Read coordinates")
         print ("q) Quit client program")
-        if sys.version_info >= (3,0,0):
-            op = input("Enter option: ")
-        else:
-            op = raw_input("Enter option: ")
+        op = input("Enter option: ")
 
         if op == 'a':
             # Ask for the new speeds
-            if sys.version_info >= (3,0,0):
-                v = input("Enter V speed: ")
-                w = input("Enter W speed: ")
-            else:
-                v = raw_input("Enter V speed: ")
-                w = raw_input("Enter W speed: ")
+            v = input("Enter V speed: ")
+            w = input("Enter W speed: ")
 
-            v_w = [float(v), float(w)]
+            # Build the required data structure, a dictionary
+            v_w = {"v": float(v), "w": float(w)}
             print ("Sending the command: {0}".format(v_w))
 
             # Send the data
-            data_out = pickle.dumps((v_w))
-            sent = client_socket.sendto(data_out,host)
+            data_out = (json.dumps((v_w)) + '\n').encode()
+            print ("SENDING: %s through port %d" % (data_out, server_motion_port))
+            sent = motion_socket.send(data_out)
 
             print ("Just sent %d bytes to server" % sent)
-            connected = True
 
         elif op == 'b':
-            data_in = read_data(client_socket)
+            data_in = pose_socket.recv(1024)
             try:
-                pickled_data = pickle.loads(data_in)
-                print_data(pickled_data)
+                # Split the long string received and keep
+                #  only the last complete item
+                data_string = str(data_in)
+                data_string = data_string.split('\\n')[-2]
+                print ("RECEIVED: %s" % data_string)
+                #received_data = json.loads(data_in.decode('utf-8'))
+                #_print_data(received_data)
             except EOFError as detail:
                 print ("\tNo data available for the moment")
             except TypeError as detail:
@@ -112,7 +138,6 @@ def main():
 
         else:
             print ("Unknown option. Try again.")
-
 
 
 if __name__ == "__main__":
