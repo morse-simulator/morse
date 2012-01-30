b35e9d751c13d0b56a58f15ae0c395af633b3f44
Arnaud Degroote <arnaud.degroote@laas.fr>
2012-01-30 16:15:26 +0100

    [testing] Add some tests for the v_w controller

---
+++ testing/base/vw_testing.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+"""
+This script tests some of the base functionalities of MORSE.
+"""
+
+import sys
+import socket
+import json
+import math
+from time import sleep
+from morse.testing.testing import MorseTestCase
+from pymorse import Morse
+
+# Include this import to be able to use your test file as a regular 
+# builder script, ie, usable with: 'morse [run|exec] base_testing.py
+try:
+    from morse.builder.morsebuilder import *
+except ImportError:
+    pass
+
+def send_speed(s, v, w, t):
+    s.send(json.dumps({'v' : v, 'w' : w}).encode())
+    sleep(t)
+    s.send(json.dumps({'v' : 0.0, 'w' : 0.0}).encode())
+
+class VW_Test(MorseTestCase):
+    def setUpEnv(self):
+        """ Defines the test scenario, using the Builder API.
+        """
+        
+        robot = Robot('atrv')
+
+        pose = Sensor('pose')
+        robot.append(pose)
+        pose.configure_mw('socket')
+
+        motion = Actuator('v_omega')
+        robot.append(motion)
+        motion.configure_mw('socket')
+        
+        env = Environment('indoors-1/indoor-1')
+        env.configure_service('socket')
+
+    def test_vw_controller(self):
+        """ This test is guaranteed to be started only when the simulator
+        is ready.
+        """
+        pass
+
+        morse = Morse()
+        
+        # Read the start position, it must be (0.0, 0.0, 0.0)
+        pose_stream = morse.stream('Pose')
+        pose = pose_stream.get()
+        for coord in pose.values():
+            self.assertAlmostEqual(coord, 0.0, delta=0.02)
+
+        # v_w socket
+        port = morse.get_stream_port('Motion_Controller')
+        v_w_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        v_w_client.connect(('localhost', port))
+
+        send_speed(v_w_client, 1.0, 0.0, 2.0)
+
+        pose = pose_stream.get()
+        self.assertAlmostEqual(pose['x'], 2.0, delta=0.02)
+        self.assertAlmostEqual(pose['y'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['z'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['yaw'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['roll'], 0.0, delta=0.02)
+
+        send_speed(v_w_client, -1.0, 0.0, 2.0)
+
+        pose = pose_stream.get()
+        for coord in pose.values():
+            self.assertAlmostEqual(coord, 0.0, delta=0.02)
+
+        send_speed(v_w_client, 1.0, -math.pi/4.0, 2.0)
+        pose = pose_stream.get()
+
+        # for non-null w, we have r = v /  w
+        self.assertAlmostEqual(pose['x'], 4.0/ math.pi , delta=0.02)
+        self.assertAlmostEqual(pose['y'], -4.0/ math.pi , delta=0.02)
+        self.assertAlmostEqual(pose['z'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.02)
+        self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.02)
+        self.assertAlmostEqual(pose['roll'], 0.0, delta=0.02)
+
+        send_speed(v_w_client, 0.5, -math.pi/8.0, 12.0)
+
+        pose = pose_stream.get()
+        for coord in pose.values():
+            self.assertAlmostEqual(coord, 0.0, delta=0.02)
+
+        send_speed(v_w_client, -2.0, math.pi/2.0, 3.0)
+        pose = pose_stream.get()
+        self.assertAlmostEqual(pose['x'], 4.0/ math.pi , delta=0.08)
+        self.assertAlmostEqual(pose['y'], -4.0/ math.pi , delta=0.08)
+        self.assertAlmostEqual(pose['z'], 0.0, delta=0.08)
+        self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.08)
+        self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.08)
+        self.assertAlmostEqual(pose['roll'], 0.0, delta=0.08)
+
+        morse.close()
+
+########################## Run these tests ##########################
+if __name__ == "__main__":
+    import unittest
+    from morse.testing.testing import MorseTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(VW_Test)
+    sys.exit(not MorseTestRunner().run(suite).wasSuccessful())
+
