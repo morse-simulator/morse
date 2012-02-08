fa19ab9036a3b1e360cda7d97fe6c8ec95f6c432
Gilberto Echeverria <gechever@laas.fr>
2012-02-08 21:01:27 +0100

    Update sensor to match with latest kuka armature

--- src/morse/sensors/kuka_posture.py
+++ src/morse/sensors/kuka_posture.py
@@ -1,11 +1,12 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-import math
 import morse.core.sensor
-import mathutils
-import morse.helpers.math as morse_math
+import GameLogic
 
 class KukaPostureClass(morse.core.sensor.MorseSensorClass):
-    """ KUKA posture sensor """
+    """ KUKA posture sensor
+        
+    Reads the position of the KUKA LWR arm with respect to the robot,
+    as well as the angles of each of the segments."""
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -17,87 +18,58 @@ class KukaPostureClass(morse.core.sensor.MorseSensorClass):
         # Call the constructor of the parent class
         super(self.__class__,self).__init__(obj, parent)
 
+        self._kuka_armature = None
+        # Check if robot parent has a child with the name specified
+        #  in this objects property 'KUKAname'.
+        # By default it will look for "kuka_armature"
+        for child in self.robot_parent.blender_obj.children:
+            if str(child) == self.blender_obj['KUKAname']:
+                self._kuka_armature = child
+                break
+
+        if self._kuka_armature:
+            logger.debug("Found kuka_arm: '%s'" % self._kuka_armature.name)
+        else:
+            logger.warning("Kuka arm not found. The 'kuka_posture' sensor will do nothing")
+            return
+
+        # Get the reference to the class instance of the kuka actuator
+        self._kuka_actuator_instance = GameLogic.componentDict[self._kuka_armature.name]
+
+        # Define the variables in 'local_data'
         self.local_data['x'] = 0.0
         self.local_data['y'] = 0.0
         self.local_data['z'] = 0.0
         self.local_data['yaw'] = 0.0
         self.local_data['pitch'] = 0.0
         self.local_data['roll'] = 0.0
-        self.local_data['seg0'] = 0.0
-        self.local_data['seg1'] = 0.0
-        self.local_data['seg2'] = 0.0
-        self.local_data['seg3'] = 0.0
-        self.local_data['seg4'] = 0.0
-        self.local_data['seg5'] = 0.0
-        self.local_data['seg6'] = 0.0
+        for channel in self._kuka_armature.channels:
+            self.local_data[channel.name] = 0.0
+
         logger.info('Component initialized')
 
-        # The axis along which the different segments rotate
-        # Considering the rotation of the arm as installed in Jido
-        self._dofs = ['z', '-y', 'z', 'y', 'z', '-y', 'z']
        
     def default_action(self):
-        """ Get the x, y, z, yaw, pitch and roll of the blender object. """
-        x = self.position_3d.x
-        y = self.position_3d.y
-        z = self.position_3d.z
-        yaw = self.position_3d.yaw
-        pitch = self.position_3d.pitch
-        roll = self.position_3d.roll
+        """ Get the x, y, z, yaw, pitch and roll of the KUKA armature,
+        and the rotation angle for each of the segments. """
+        if not self._kuka_armature:
+            return
 
-        # Gather information about all segments of the kuka-arm
-        self._segments = []
+        # Take the base position from the kuka arm actuator
+        self.local_data['x'] = self._kuka_actuator_instance.position_3d.x
+        self.local_data['y'] = self._kuka_actuator_instance.position_3d.y
+        self.local_data['z'] = self._kuka_actuator_instance.position_3d.z
+        self.local_data['yaw'] = self._kuka_actuator_instance.position_3d.yaw
+        self.local_data['pitch'] = self._kuka_actuator_instance.position_3d.pitch
+        self.local_data['roll'] = self._kuka_actuator_instance.position_3d.roll
 
-        kuka_obj = 0
-        # Check if robot parent has a child named "kuka_base"
-        for child in self.robot_parent.blender_obj.children:
-            if str(child) == self.blender_obj['KUKAname']:
-                kuka_obj = child
-
-        #if kuka_obj != 0:
-            logger.debug("Found kuka_arm")
-        #else:
-            logger.debug("WARNING: Kuka arm not found!")
-
-        segment = kuka_obj.children[0]
-        self._angles = []
-        
-        # Gather all the children of the object which are the segments of the kuka-arm
-        for i in range(len(self._dofs)):
-            self._segments.append(segment)
-                   
-            # Extract the angles
-            rot_matrix = segment.localOrientation
-            segment_matrix = mathutils.Matrix(rot_matrix[0], rot_matrix[1], rot_matrix[2])
-            segment_euler = segment_matrix.to_euler()
+        # Check all the segments of the armature
+        for channel in self._kuka_armature.channels:
+            segment_angle = channel.joint_rotation
 
             # Use the corresponding direction for each rotation
-            if self._dofs[i] == 'y':
-                self._angles.append(segment_euler[1])
-            elif self._dofs[i] == '-y':
-                self._angles.append(-segment_euler[1])
-            elif self._dofs[i] == 'z':
-                self._angles.append(segment_euler[2])
-           
-            try:
-                segment = segment.children[0]
-            # Exit when there are no more children
-            except IndexError as detail:
-                break               
-                        
-        # Store the data acquired by this sensor that could be sent
-        #  via a middleware.
-        self.local_data['x'] = float(x)
-        self.local_data['y'] = float(y)
-        self.local_data['z'] = float(z)    
-        self.local_data['yaw'] = float(yaw)
-        self.local_data['pitch'] = float(pitch)
-        self.local_data['roll'] = float(roll)
-        self.local_data['seg0'] = self._angles[0]
-        self.local_data['seg1'] = self._angles[1]
-        self.local_data['seg2'] = self._angles[2]
-        self.local_data['seg3'] = self._angles[3]
-        self.local_data['seg4'] = self._angles[4]
-        self.local_data['seg5'] = self._angles[5]
-        self.local_data['seg6'] = self._angles[6]
-
+            #  taken directly from the armature instance
+            if self._kuka_actuator_instance._dofs[channel.name][1] == 1:
+                self.local_data[channel.name] = segment_angle[1]
+            elif self._kuka_actuator_instance._dofs[channel.name][2] == 1:
+                self.local_data[channel.name] = segment_angle[2]
