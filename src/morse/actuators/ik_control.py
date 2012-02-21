381f5d5d9bfbc4e0c10655bb9d578dfa2ba7f137
Gilberto Echeverria <gechever@laas.fr>
2012-02-21 14:12:16 +0100

    Rotate the reference frame, and add torso
    The torso should be used as a reference, to make the movements of head
    and hands relative to its position. Currently not working yet

--- src/morse/actuators/ik_control.py
+++ src/morse/actuators/ik_control.py
@@ -19,29 +19,47 @@ class IKControlClass(morse.core.actuator.MorseActuatorClass):
         parent = self.robot_parent
         # Find the IK target empties
         for obj in parent.blender_obj.children:
+            if "IK_Target_Empty.Head" in obj.name:
+                self._head_ik_target = obj
+            if "Torso_Reference_Empty" in obj.name:
+                self._torso_empty = obj
             if "IK_Target_Empty.L" in obj.name:
                 self._left_ik_target = obj
             if "IK_Target_Empty.R" in obj.name:
                 self._right_ik_target = obj
-            # Also get the target for the head
-            if "IK_Target_Empty.Head" in obj.name:
-                self._head_ik_target = obj
 
         self.local_data['left_hand_position'] = [0.3, 0.3, 0.9]
         self.local_data['right_hand_position'] = [0.3, -0.3, 0.9]
-        #self.local_data['left_hand_rotation'] = [0.0, 0.0, 0.0]
-        #self.local_data['right_hand_rotation'] = [0.0, 0.0, 0.0]
-        #self.local_data['head_position'] = [0.5, 0.0, 1.6]
+        self.local_data['head_position'] = [0.5, 0.0, 1.6]
+        self.local_data['torso_position'] = [0.0, 0.0, 0.0]
+        self.local_data['torso_orientation'] = [0.0, 0.0, 0.0]
 
+        # Make the IK targets children of the torso reference
+        self._head_ik_target.setParent(self._torso_empty)
+        self._left_ik_target.setParent(self._torso_empty)
+        self._right_ik_target.setParent(self._torso_empty)
+        
         logger.info('Component initialized')
         logger.setLevel(logging.DEBUG)
 
 
     def default_action(self):
         """ Apply the positions read to the IK targets of the hands """
+        # Reset the orientation of the wais before placing the ik targets
+        self._torso_empty.localOrientation = [0.0, 0.0, 0.0]
+
+        # Adjust the head target to be in front of the face
+        #self.local_data['head_position'][0] += 0.3
+
+        # Put the ik targets in the correct positions
+        self._head_ik_target.localPosition = self.local_data['head_position']
         self._left_ik_target.localPosition = self.local_data['left_hand_position']
         self._right_ik_target.localPosition = self.local_data['right_hand_position']
-        self._head_ik_target.localPosition = self.local_data['head_position']
+
+        #self._torso_empty.localPosition = self.local_data['torso_position']
+
+        # Rotate the torso to get the same reference as the source data
+        self._torso_empty.localOrientation = self.local_data['torso_orientation']
 
 
         #self._print_position("LEFT EMPTY", self._left_ik_target.localPosition)
--- src/morse/middleware/pocolibs/actuators/niut.py
+++ src/morse/middleware/pocolibs/actuators/niut.py
@@ -5,6 +5,7 @@ from morse.middleware.pocolibs.actuators.Niut_Poster import ors_niut_poster
 # Assign constant variables to identify the joints of interest
 # From niut/niutStruct.h
 HEAD_INDEX = 1
+TORSO_INDEX = 3
 LEFT_HAND_INDEX = 9
 RIGHT_HAND_INDEX = 15
 
@@ -22,25 +23,45 @@ def read_niut_ik_positions(self, component_instance):
     poster_id = self._poster_in_dict[component_instance.blender_obj.name]
 
     result = True
+    # Get torso position. This will be used as the reference point
+    #  for the rest of the targets
+    result = result and _store_joint_position(component_instance, poster_id, 'torso_position', TORSO_INDEX, None)
+    torso_position = component_instance.local_data['torso_position']
+    #logger.error("torso (index=%d)\n\tposition: [%.4f, %.4f, %.4f]" % (TORSO_INDEX, torso_position[0], torso_position[1], torso_position[2]))
+
     # Get head position
-    result = result and _store_joint_position(component_instance, poster_id, 'head_position', HEAD_INDEX)
+    result = result and _store_joint_position(component_instance, poster_id, 'head_position', HEAD_INDEX, torso_position)
     # Get left hand position
-    result = result and _store_joint_position(component_instance, poster_id, 'left_hand_position', LEFT_HAND_INDEX)
+    result = result and _store_joint_position(component_instance, poster_id, 'left_hand_position', LEFT_HAND_INDEX, torso_position)
     # Get right hand position
-    result = result and _store_joint_position(component_instance, poster_id, 'right_hand_position', RIGHT_HAND_INDEX)
+    result = result and _store_joint_position(component_instance, poster_id, 'right_hand_position', RIGHT_HAND_INDEX, torso_position)
+
+    # Indicate the rotation expected of the torso
+    component_instance.local_data['torso_orientation'] = [-1.5708, 0.0, -1.5708]
+    #component_instance.local_data['torso_orientation'] = [3.14159, -1.5708, 0.0]
 
     # Return true to indicate that a command has been received
     return result
 
 
-def _store_joint_position(component_instance, poster_id, ik_target, joint_index):
+def _store_joint_position(component_instance, poster_id, ik_target, joint_index, reference_position):
     """ Read the position of the pecified joint """
     joint_position, ok = ors_niut_poster.read_niut_joint_position(poster_id, joint_index)
 
     if ok != 0:
-        #component_instance.local_data[ik_target] = [joint_position.x, joint_position.y, joint_position.z]
+        """
+        if reference_position:
+            logger.debug("Joint '%s' (index=%d)\n\tposition: [%.4f, %.4f, %.4f]" % (ik_target, joint_index, joint_position.x, joint_position.y, joint_position.z))
+            # Get the positions of the joints with respect to the torso
+            joint_position.x = joint_position.x - reference_position[0]
+            joint_position.y = joint_position.y - reference_position[1]
+            joint_position.z = joint_position.z - reference_position[2]
+            logger.debug("Joint '%s' (index=%d) CORRECTED\n\tposition: [%.4f, %.4f, %.4f]" % (ik_target, joint_index, joint_position.x, joint_position.y, joint_position.z))
+        """
+
+        component_instance.local_data[ik_target] = [joint_position.x, joint_position.y, joint_position.z]
         # Reorder the axis
-        component_instance.local_data[ik_target] = [joint_position.z, joint_position.x, joint_position.y]
+        #component_instance.local_data[ik_target] = [joint_position.y, joint_position.x, joint_position.z]
         #logger.debug("Joint '%s' (index=%d)\n\tposition: %s " % (ik_target, joint_index, component_instance.local_data[ik_target]))
         return True
     else:
