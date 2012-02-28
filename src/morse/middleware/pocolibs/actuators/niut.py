1a4f9ca4a49195b28f8c24c4b480ddf904d0d83d
Gilberto Echeverria <gechever@laas.fr>
2012-02-28 14:15:08 +0100

    Correct transformation for Kinect reference frame

--- src/morse/middleware/pocolibs/actuators/niut.py
+++ src/morse/middleware/pocolibs/actuators/niut.py
@@ -1,6 +1,7 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 from morse.middleware.pocolibs_mw import init_extra_actuator
 from morse.middleware.pocolibs.actuators.Niut_Poster import ors_niut_poster
+import mathutils
 
 # Assign constant variables to identify the joints of interest
 # From niut/niutStruct.h
@@ -29,39 +30,57 @@ def read_niut_ik_positions(self, component_instance):
     torso_position = component_instance.local_data['torso_position']
     #logger.error("torso (index=%d)\n\tposition: [%.4f, %.4f, %.4f]" % (TORSO_INDEX, torso_position[0], torso_position[1], torso_position[2]))
 
-    # Get head position
-    result = result and _store_joint_position(component_instance, poster_id, 'head_position', HEAD_INDEX, torso_position)
-    # Get left hand position
-    result = result and _store_joint_position(component_instance, poster_id, 'left_hand_position', LEFT_HAND_INDEX, torso_position)
-    # Get right hand position
-    result = result and _store_joint_position(component_instance, poster_id, 'right_hand_position', RIGHT_HAND_INDEX, torso_position)
+    # This matrix is used to make the movements of head and hands
+    #  relative to the torso
+    translation_matrix = mathutils.Matrix((
+                [1.0, 0.0, 0.0, torso_position[0]], \
+                [0.0, 1.0, 0.0, torso_position[1]], \
+                [0.0, 0.0, 1.0, torso_position[2]], \
+                [0.0, 0.0, 0.0, 1.0]))
 
-    # Indicate the rotation expected of the torso
-    component_instance.local_data['torso_orientation'] = [-1.5708, 0.0, -1.5708]
-    #component_instance.local_data['torso_orientation'] = [3.14159, -1.5708, 0.0]
+	#         Y  X                  Z  Y
+	#         | /                   | /
+	# KinCam  |/ ____ Z  ,   World  |/_____X
+    # Transformation of the Kinect frame of reference to that of Blender
+    kinect_matrix = mathutils.Matrix((
+                [0.0, 0.0, 1.0, 0.0], \
+                [1.0, 0.0, 0.0, 0.0], \
+                [0.0, 1.0, 0.0, 0.0], \
+                [0.0, 0.0, 0.0, 1.0]))
+
+    # Spin the positions around the Z axis, to match with the Blender human
+    rotation_matrix = mathutils.Matrix((
+                [-1.0, 0.0, 0.0, 0.0], \
+                [0.0, -1.0, 0.0, 0.0], \
+                [0.0, 0.0, 1.0, 0.1], \
+                [0.0, 0.0, 0.0, 1.0]))
+
+    transformation_matrix = translation_matrix.inverted() * kinect_matrix * rotation_matrix
+
+    # Get head position
+    result = result and _store_joint_position(component_instance, poster_id, 'head_position', HEAD_INDEX, transformation_matrix)
+    # Get left hand position
+    result = result and _store_joint_position(component_instance, poster_id, 'left_hand_position', LEFT_HAND_INDEX, transformation_matrix)
+    # Get right hand position
+    result = result and _store_joint_position(component_instance, poster_id, 'right_hand_position', RIGHT_HAND_INDEX, transformation_matrix)
 
     # Return true to indicate that a command has been received
     return result
 
 
-def _store_joint_position(component_instance, poster_id, ik_target, joint_index, reference_position):
+def _store_joint_position(component_instance, poster_id, ik_target, joint_index, transformation_matrix):
     """ Read the position of the pecified joint """
     joint_position, ok = ors_niut_poster.read_niut_joint_position(poster_id, joint_index)
 
     if ok != 0:
-        """
-        if reference_position:
-            logger.debug("Joint '%s' (index=%d)\n\tposition: [%.4f, %.4f, %.4f]" % (ik_target, joint_index, joint_position.x, joint_position.y, joint_position.z))
-            # Get the positions of the joints with respect to the torso
-            joint_position.x = joint_position.x - reference_position[0]
-            joint_position.y = joint_position.y - reference_position[1]
-            joint_position.z = joint_position.z - reference_position[2]
-            logger.debug("Joint '%s' (index=%d) CORRECTED\n\tposition: [%.4f, %.4f, %.4f]" % (ik_target, joint_index, joint_position.x, joint_position.y, joint_position.z))
-        """
+        # Convert the GEN_POINT_3D into a Blender vector
+        position_vector = mathutils.Vector([joint_position.x, joint_position.y, joint_position.z])
+        if transformation_matrix:
+            new_position = position_vector * transformation_matrix
+        else:
+            new_position = position_vector
 
-        component_instance.local_data[ik_target] = [joint_position.x, joint_position.y, joint_position.z]
-        # Reorder the axis
-        #component_instance.local_data[ik_target] = [joint_position.y, joint_position.x, joint_position.z]
+        component_instance.local_data[ik_target] = new_position
         #logger.debug("Joint '%s' (index=%d)\n\tposition: %s " % (ik_target, joint_index, component_instance.local_data[ik_target]))
         return True
     else:
