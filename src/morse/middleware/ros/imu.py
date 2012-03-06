12f586d86d00708d9a62f68b05ccafb39f5a1087
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-03-06 18:44:42 +0100

    Rewritten the ROS IMU sensor to export a proper Odometry message
    The IMU sensor, used with ROS, now exports a proper Odometry message on the /<robot>/imu topic.
    
    While here:
    - removed old ROS odometry sensor which is non standard in ROS
    - (temporarly) added the tfMessage directly in MORSE code for
      Py3 compatibility reasons. To properly fix upstream

--- src/morse/middleware/ros/imu.py
+++ src/morse/middleware/ros/imu.py
@@ -1,8 +1,15 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-import roslib; roslib.load_manifest('roscpp'); roslib.load_manifest('rospy'); roslib.load_manifest('geometry_msgs')
+
+import copy
+import mathutils
+
+import roslib; roslib.load_manifest('rospy'); roslib.load_manifest('nav_msgs'); roslib.load_manifest('geometry_msgs')
+
+from morse.middleware.ros.tfMessage import tfMessage
+
 import rospy
-import std_msgs
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TransformStamped
+from nav_msgs.msg import Odometry
 
 def init_extra_module(self, component_instance, function, mw_data):
     """ Setup the middleware connection with this data
@@ -17,10 +24,92 @@ def init_extra_module(self, component_instance, function, mw_data):
     component_instance.output_functions.append(function)
 
     # Generate one publisher and one topic for each component that is a sensor and uses post_message
-    if mw_data[1] == "post_velocity_twist":  
+    if mw_data[1] == "post_velocity_twist":
         self._topics.append(rospy.Publisher(parent_name + "/" + component_name, Twist))
+    else:
+        self._topics.append(rospy.Publisher(parent_name + "/" + component_name, Odometry))
 
-    logger.info('######## IMU-SENSOR INITIALIZED ########')
+    self.initial_position = copy.copy(component_instance.robot_parent.position_3d)
+
+    self.pub_tf = rospy.Publisher("/tf", tfMessage)
+
+    self._seq = 0
+
+    logger.info('Initialized ROS IMU sensor')
+
+def sendTransform(self, translation, rotation, time, child, parent):
+        """
+        :param translation: the translation of the transformtion as a tuple (x, y, z)
+        :param rotation: the rotation of the transformation as a tuple (x, y, z, w)
+        :param time: the time of the transformation, as a rospy.Time()
+        :param child: child frame in tf, string
+        :param parent: parent frame in tf, string
+
+        Broadcast the transformation from tf frame child to parent on ROS topic ``"/tf"``.
+        """
+
+        t = TransformStamped()
+        t.header.frame_id = parent
+        t.header.stamp = time
+        t.child_frame_id = child
+        t.transform.translation.x = translation[0]
+        t.transform.translation.y = translation[1]
+        t.transform.translation.z = translation[2]
+
+        t.transform.rotation.x = rotation[0]
+        t.transform.rotation.y = rotation[1]
+        t.transform.rotation.z = rotation[2]
+        t.transform.rotation.w = rotation[3]
+
+        tfm = tfMessage([t])
+        self.pub_tf.publish(tfm)
+
+
+def post_odometry(self, component_instance):
+    """ Publish the data of the IMU sensor as a ROS-Odometry message
+    """
+    parent = component_instance.robot_parent
+    parent_name = parent.blender_obj.name
+    current_time = rospy.Time.now()
+
+    odometry = Odometry()
+    odometry.header.seq = self._seq
+    odometry.header.stamp = current_time
+    odometry.header.frame_id = "/odom"
+    odometry.child_frame_id = "/base_footprint"
+
+    # fill pose
+    pos = self.initial_position.transformation3d_with(parent.position_3d)
+    odometry.pose.pose.position.x = pos.x
+    odometry.pose.pose.position.y = pos.y
+    odometry.pose.pose.position.z = pos.z
+
+    euler = mathutils.Euler((pos.roll, pos.pitch, pos.yaw))
+    quaternion = euler.to_quaternion()
+    odometry.pose.pose.orientation = quaternion
+
+    # fill twist
+    vx, vy, vz, vyaw, vpitch, vroll = component_instance.local_data["velocity"]
+    odometry.twist.twist.linear.x = vx
+    odometry.twist.twist.linear.y = vy
+    odometry.twist.twist.linear.z = vz
+    odometry.twist.twist.angular.x = vroll
+    odometry.twist.twist.angular.y = vpitch
+    odometry.twist.twist.angular.z = vyaw
+
+    for topic in self._topics: 
+        # publish the message on the correct w
+        if str(topic.name) == str("/" + parent_name + "/" + component_instance.blender_obj.name): 
+            topic.publish(odometry)
+
+    # publish the odom init
+    sendTransform(self, (pos.x, pos.y, pos.z),
+                (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                    rospy.Time.now(),
+                    "/base_footprint",
+                    "/odom")
+
+    self._seq += 1
 
 def post_velocity_twist(self, component_instance):
     """ Publish the data of the IMU sensor as a ROS-Twist message
