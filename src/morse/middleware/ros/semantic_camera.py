bafd41348da193af837880d0cb0a5353536d7870
Michael Karg <kargm@in.tum.de>
2012-03-15 17:09:07 +0100

    [ros] Semantic camera now sends tf frames

--- src/morse/middleware/ros/semantic_camera.py
+++ src/morse/middleware/ros/semantic_camera.py
@@ -1,5 +1,5 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-import roslib; roslib.load_manifest('roscpp'); roslib.load_manifest('rospy'); roslib.load_manifest('std_msgs')
+import roslib; roslib.load_manifest('roscpp'); roslib.load_manifest('rospy'); roslib.load_manifest('std_msgs');  roslib.load_manifest('geometry_msgs')
 import rospy
 import std_msgs
 import bge
@@ -7,6 +7,8 @@ import math
 import mathutils
 
 from std_msgs.msg import String
+from morse.middleware.ros.tfMessage import tfMessage
+from geometry_msgs.msg import TransformStamped
 
 def init_extra_module(self, component_instance, function, mw_data):
     """ Setup the middleware connection with this data
@@ -24,7 +26,34 @@ def init_extra_module(self, component_instance, function, mw_data):
     self._topics.append(rospy.Publisher(parent_name + "/" + component_name, String))
     
     logger.info('######## ROS SEMANTIC CAMERA PUBLISHER INITIALIZED ########')
+
+def sendTransform(self, translation, rotation, time, child, parent):
+    """
+    :param translation: the translation of the transformtion as a tuple (x, y, z)
+    :param rotation: the rotation of the transformation as a tuple (x, y, z, w)
+    :param time: the time of the transformation, as a rospy.Time()
+    :param child: child frame in tf, string
+    :param parent: parent frame in tf, string
     
+    Broadcast the transformation from tf frame child to parent on ROS topic ``"/tf"``.
+    """
+    
+    t = TransformStamped()
+    t.header.frame_id = parent
+    t.header.stamp = time
+    t.child_frame_id = child
+    t.transform.translation.x = translation[0]
+    t.transform.translation.y = translation[1]
+    t.transform.translation.z = translation[2]
+    
+    t.transform.rotation.x = rotation[0]
+    t.transform.rotation.y = rotation[1]
+    t.transform.rotation.z = rotation[2]
+    t.transform.rotation.w = rotation[3]
+    
+    tfm = tfMessage([t])
+    self.pub_tf.publish(tfm)
+
 def post_string(self, component_instance):
     """ Publish the data of the semantic camera as a string-message with newlines (for better visualization in console).
 
@@ -39,6 +68,10 @@ def post_string(self, component_instance):
             #if object has no description, set to '-'
             if obj['description'] == '':
                 description = '-'
+
+            # send tf-frame for every object
+            sendTransform(self, obj['position'], obj['orientation'],rospy.Time.now(), str(obj['name']), "/map")
+
             # Build string from name, description, location and orientation in the global world frame
             message = message + "[" + str(obj['name']) + ", " + description + ", " + str(obj['position']) + ", " + str(obj['orientation']) + " ]\n"    
             string.data = message
