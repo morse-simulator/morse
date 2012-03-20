4f69fea73376c80fcbf5e4d58f94f6828120adf1
Michael Karg <kargm@in.tum.de>
2012-03-20 17:40:27 +0100

    [scenarii] Fixed tum kitchen to be compliant with semantic map

--- examples/scenarii/ias_kitchen_pr2.py
+++ examples/scenarii/ias_kitchen_pr2.py
@@ -10,11 +10,10 @@ james.head.configure_overlay('ros', 'morse.middleware.ros.overlays.pr2.PR2')
 james.l_arm.configure_overlay('ros', 'morse.middleware.ros.overlays.pr2.PR2')
 james.r_arm.configure_overlay('ros', 'morse.middleware.ros.overlays.pr2.PR2')
 james.torso_lift.configure_overlay('ros', 'morse.middleware.ros.overlays.pr2.PR2')
-james.translate(x=2.5, y=3.2, z=0.0)
+james.translate(x=1, y=1, z=0.0)
 
 human = Human()
-human.translate(x=2.5, y=0, z=0.0)
-#human.rotate(z=-3.0)
+human.rotate(z=-3.0)
 
 human_pose = Sensor('pose')
 human.append(human_pose)
@@ -68,21 +67,21 @@ semantic_camera.configure_mw('ros', ['morse.middleware.ros_mw.ROSClass', 'post_l
 # Add passive objects
 cornflakes = PassiveObject('props/kitchen_objects.blend', 'Cornflakes')
 cornflakes.setgraspable()
-cornflakes.translate(x=0.5, y=1.67, z=0.9)
+cornflakes.translate(x=1.37, y=0.5, z=0.9)
 
 fork = PassiveObject('props/kitchen_objects.blend', 'Fork')
 fork.setgraspable()
-fork.translate(x=0.5, y=1.87, z=0.86)
+fork.translate(x=1.38, y=0.5, z=0.86)
 fork.rotate(z=1.45)
 
 knife = PassiveObject('props/kitchen_objects.blend', 'Knife')
 knife.setgraspable()
-knife.translate(x=0.5, y=1.97, z=0.86)
+knife.translate(x=1.39, y=0.5, z=0.86)
 knife.rotate(z=1.45)
 
 plate = PassiveObject('props/kitchen_objects.blend', 'Plate')
 plate.setgraspable()
-plate.translate(x=0.5, y=1.97, z=0.86)
+plate.translate(x=-1.36, y=2.27, z=0.86)
 plate.rotate(z=1.45)
 
 #bread = PassiveObject('props/kitchen_objects.blend', 'Bread')
@@ -92,17 +91,17 @@ plate.rotate(z=1.45)
 
 bowl = PassiveObject('props/kitchen_objects.blend', 'Bowl')
 bowl.setgraspable()
-bowl.translate(x=0.5, y=1.97, z=0.86)
+bowl.translate(x=-1.38, y=2.30, z=0.86)
 bowl.rotate(z=1.45)
 
 jam = PassiveObject('props/kitchen_objects.blend', 'Jam')
 jam.setgraspable()
-jam.translate(x=0.5, y=1.97, z=0.86)
+jam.translate(x=1.33, y=0.45, z=0.86)
 jam.rotate(z=1.45)
 
 nutella = PassiveObject('props/kitchen_objects.blend', 'Nutella')
 nutella.setgraspable()
-nutella.translate(x=0.5, y=1.97, z=0.86)
+nutella.translate(x=1.35, y=0.42, z=0.86)
 nutella.rotate(z=1.45)
 
 # Set scenario
