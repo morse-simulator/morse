e359794b0308496e01c1b36b9c974252ca22e77b
Gilberto Echeverria <gechever@laas.fr>
2012-06-04 15:00:59 +0200

    Add code to include robot definition file
    This will permit the scripts to find the python file with the definition
    of the Dala class (with the equipped robot), regardless of where 'morse'
    is called

--- examples/tutorials/multinode/tutorial-hla.py
+++ examples/tutorials/multinode/tutorial-hla.py
@@ -1,3 +1,14 @@
+# Add the path of this file to PYTHONPATH
+import os, sys, inspect
+# cmd_folder = os.path.dirname(os.path.abspath(__file__)) # DO NOT USE __file__ !!!
+# __file__ fails if script is called in different ways on Windows
+# __file__ fails if someone does os.chdir() before
+# sys.argv[0] also fails because it doesn't not always contains the path
+cmd_folder = os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])
+if cmd_folder not in sys.path:
+        sys.path.insert(0, cmd_folder)
+
+
 from morse.builder.morsebuilder import *
 
 from dala_simple import equipped_robot
--- examples/tutorials/multinode/tutorial-socket.py
+++ examples/tutorials/multinode/tutorial-socket.py
@@ -1,9 +1,20 @@
+# Add the path of this file to PYTHONPATH
+import os, sys, inspect
+# cmd_folder = os.path.dirname(os.path.abspath(__file__)) # DO NOT USE __file__ !!!
+# __file__ fails if script is called in different ways on Windows
+# __file__ fails if someone does os.chdir() before
+# sys.argv[0] also fails because it doesn't not always contains the path
+cmd_folder = os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])
+if cmd_folder not in sys.path:
+        sys.path.insert(0, cmd_folder)
+
+
 from morse.builder.morsebuilder import *
 
 from dala_simple import equipped_robot
 
-dala1 = equipped_robot()
-dala2 = equipped_robot()
+dala1 = equipped_robot('socket')
+dala2 = equipped_robot('socket')
 dala2.translate(5, -3, 0)
 
 env = Environment('laas/grande_salle')
