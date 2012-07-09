c537562b745bd8504d1b9a6fea8fc0bf33ad8505
Gilberto Echeverria <gechever@laas.fr>
2012-07-09 12:18:59 +0200

    Use the first (by index) tracked human from xtion
    The module will loop through the possible 16 humans and use the first
    one whose motion is being tracked. This allows changing users on the
    fly.
    However, it is possible for a new user (with a lower index in the xtion)
    to steal control from a previous user.

--- src/morse/middleware/pocolibs/actuators/Niut_Poster/ors_niut_poster.c
+++ src/morse/middleware/pocolibs/actuators/Niut_Poster/ors_niut_poster.c
@@ -11,14 +11,26 @@ GEN_POINT_3D read_niut_joint_position (PosterHandler* handler, int joint_index, 
     //NIUT_JOINT_STR joint;
     GEN_POINT_3D joint_position;
 
+    static int previous_user = 0;
+
     read_poster(handler, ok, &list, sizeof(NIUT_HUMAN_LIST));
 
-    // Extract the data for the specified joint of the first human in the list
-    //user = list.users[0];
-    //skeleton = user.skeleton;
-    //joint = skeleton.joint[joint_index];
-    //joint_position = joint.position;
-    joint_position = list.users[0].skeleton.joint[joint_index].position;
+    for (int i=0; i<16; i++)
+        // Use the first active user tracked
+        if (list.users[i].state == NIUT_TRACKING)
+        {
+            // Extract the data for the specified joint of the first human in the list
+            //user = list.users[i];
+            //skeleton = user.skeleton;
+            //joint = skeleton.joint[joint_index];
+            //joint_position = joint.position;
+            joint_position = list.users[i].skeleton.joint[joint_index].position;
+            if (i != previous_user)
+            {
+                previous_user = i;
+                printf ("Niut reader: Now tracking user %d\n", i+1);
+            }
+        }
 
 	return (joint_position);
 }
