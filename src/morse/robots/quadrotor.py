f5f410ce81e85b6399d65aa9976624afc91be93b
Arnaud Degroote <arnaud.degroote@laas.fr>
2012-08-22 10:16:55 +0200

    [robots] Import of a quadrotor model, corresponding to the ISAE one

---
+++ src/morse/robots/quadrotor.py
@@ -0,0 +1,23 @@
+import logging; logger = logging.getLogger("morse." + __name__)
+import morse.core.robot
+
+
+class QuadrotorClass(morse.core.robot.MorseRobotClass):
+    """ Class definition for the Quad2012 (Club Micro Drone
+        ISAE-Toulouse FRANCE).  Sub class of Morse_Object.
+    """
+
+    def __init__(self, obj, parent=None):
+        """ Constructor method.
+            Receives the reference to the Blender object.
+            Optionally it gets the name of the object's parent,
+            but that information is not currently used for a robot. """
+        # Call the constructor of the parent class
+        logger.info('%s initialization' % obj.name)
+        super(self.__class__, self).__init__(obj, parent)
+
+        logger.info('Component initialized')
+
+    def default_action(self):
+        """ Main function of this component. """
+        pass
