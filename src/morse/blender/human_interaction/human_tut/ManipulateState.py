b562b46b159ea846efe51693ceaf0c76ebaeeb97
kargm <kargm@in.tum.de>
2012-08-27 15:17:20 +0200

    [tutorial] Fixed interactive human tutorial

--- src/morse/blender/human_interaction/human_tut/ManipulateState.py
+++ src/morse/blender/human_interaction/human_tut/ManipulateState.py
@@ -6,5 +6,5 @@ act = co.actuators[0]
 objects = logic.getCurrentScene().objects
 
 def add_level():
-    if sens.positive and (objects['Human_Camera'].worldPosition == objects['POS_1P_Camera'].worldPosition):
+    if sens.positive and (objects['Human_Camera'].worldPosition == objects['POS_1P_Cam'].worldPosition):
         co.activate(act)
