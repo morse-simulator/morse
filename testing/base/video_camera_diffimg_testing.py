291e33e8d788c807e7d8b733de5314089abe3979
Pierrick Koch <pierrick.koch@gmail.com>
2013-01-11 11:26:25 +0100

    [test] update camera test abs(sum) -> sum(abs)

--- testing/base/video_camera_diffimg_testing.py
+++ testing/base/video_camera_diffimg_testing.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 This script tests the 'data stream' oriented feature of the socket interface.
 """
@@ -18,6 +19,10 @@ import time
 import struct
 import zlib
 from pymorse import Morse
+
+IMAGE_WIDTH=320
+IMAGE_HEIGHT=240
+DEBUG_PGM=True
 
 def send_angles(s, yaw, pitch, roll):
     s.publish({'yaw' : yaw, 'pitch' : pitch, 'roll' : roll})
@@ -48,18 +53,31 @@ class CameraTest(MorseTestCase):
         env = Environment('indoors-1/boxes')
 
     def rgb2gray8u(self, image_rgb_data):
-        # Grayscale model used for HDTV developed by the ATSC (Wikipedia)
+        # Grayscale model used for HDTV developed by the ATSC (Wikipedia)
         return [int(0.2126 * px['r'] + 0.7152 * px['g'] + 0.0722 * px['b']) \
                 for px in image_rgb_data ]
 
-    def diff_image(self, imageA, imageB):
-        # returns abs(sum( imageA - imageB )) pixel per pixel
-        return abs(sum([pxA - pxB for pxA, pxB in zip(imageA, imageB)]))
+    def diff_image(self, imageA, imageB, debug=False):
+        diff = [abs(pxA - pxB) for pxA, pxB in zip(imageA, imageB)]
+        # returns sum(abs( imageA - imageB )) pixel per pixel
+        sum_diff = sum(diff)
+        if debug:
+            pgm_path = '%s.%i.pgm'%(os.path.abspath(__file__), int(time.time()*1000))
+            self.save_pgm(pgm_path, diff, IMAGE_WIDTH, IMAGE_HEIGHT)
+            print("%i :: %.3f %% :: %s"%(sum_diff, sum_diff*100./(320*240*255), pgm_path))
+        return sum_diff
 
     def save_image(self, filepath, image8u):
         with open(filepath, 'wb') as f:
             # Write binary image (Bytes)
             f.write(zlib.compress(struct.pack('%iB'%len(image8u), *image8u)))
+
+    def save_pgm(self, filepath, image8u, width, height):
+        assert(len(image8u) == height * width)
+        with open(filepath, 'w') as f:
+            # Write binary image (Bytes)
+            f.write('P2\n%i %i\n255\n'%(width, height) + \
+                    '\n'.join(['%i'%px for px in image8u]))
 
     def load_image(self, filepath, size):
         image = []
@@ -76,13 +94,15 @@ class CameraTest(MorseTestCase):
 
     def assert_image_diff_greater(self, imageA, imageB, delta):
         # delta in percent (of the maximum difference)
-        diff = self.diff_image(imageA, imageB)
+        diff = self.diff_image(imageA, imageB, \
+                               debug=DEBUG_PGM)
         # Diff max is: width * height * 255
         self.assertGreater(diff, delta * 2.55 * len(imageA))
 
     def get_diff_file_image(self, filepath, image8u):
         # Diff max is: width * height * 255
-        return self.diff_image(image8u, self.load_image(filepath, len(image8u)))
+        return self.diff_image(image8u, self.load_image(filepath, len(image8u)),\
+                               debug=DEBUG_PGM)
 
     def test_camera(self):
         """ Test if we can connect to the pose data stream, and read from it.
@@ -114,7 +134,8 @@ class CameraTest(MorseTestCase):
             imageA = self.rgb2gray8u(cam['image']) # grayscale
             #self.save_image(imageA_path, imageA) # save the image
             # delta is choosen as 0.05 percent
-            self.assert_image_diff_less(imageA_path, imageA, 0.05)
+            # assert that the camera image differ < .5 percent from the expected
+            self.assert_image_diff_less(imageA_path, imageA, 0.5)
 
             send_angles(orientation_stream, 2.70, 0.0, 0)
             time.sleep(2.0)
@@ -123,10 +144,11 @@ class CameraTest(MorseTestCase):
             imageB = self.rgb2gray8u(cam['image']) # grayscale
             #self.save_image(imageB_path, imageB) # save the image
             # delta is choosen as 0.1 percent (physics engine tilt)
-            self.assert_image_diff_less(imageB_path, imageB, 0.1)
+            # assert that the camera image differ < 2 percent from the expected
+            self.assert_image_diff_less(imageB_path, imageB, 2)
 
-            # assert that the second image differ > 2 percent from the first
-            self.assert_image_diff_greater(imageA, imageB, 2)
+            # assert that the second image differ > 5 percent from the first
+            self.assert_image_diff_greater(imageA, imageB, 5)
 
 ########################## Run these tests ##########################
 if __name__ == "__main__":
