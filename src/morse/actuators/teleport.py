d5f52959fb7b1a12419d4f0485ed3a17bea28282
Séverin Lemaignan <severin.lemaignan@laas.fr>
2013-01-14 15:21:07 +0100

    [teleport] Added 'translate' and 'rotate' services

--- src/morse/actuators/teleport.py
+++ src/morse/actuators/teleport.py
@@ -1,6 +1,7 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import mathutils
 import morse.core.actuator
+from morse.core.services import service
 
 class TeleportActuatorClass(morse.core.actuator.Actuator):
     """ Motion controller changing the robot pose (position and orientation)
@@ -25,6 +26,35 @@ class TeleportActuatorClass(morse.core.actuator.Actuator):
         self.local_data['yaw'] = orientation.z
 
         logger.info('Component initialized')
+
+    @service
+    def translate(self, x, y = 0.0, z=0.0):
+        """ Translate the actuator owner by the given (x,y,z) vector.
+
+        This is a **relative** displacement.
+
+        :param x: X translation, in meters
+        :param y: (default: 0.0) Y translation, in meters
+        :param z: (default: 0.0) Z translation, in meters
+        """
+        self.local_data['x'] += x
+        self.local_data['y'] += y
+        self.local_data['z'] += z
+
+    @service
+    def rotate(self, roll, pitch = 0.0, yaw=0.0):
+        """ Rotates the actuator owner by the given (roll,pitch,yaw).
+
+        This is a **relative** rotation.
+
+        :param roll: roll rotation, in radians
+        :param pitch: (default: 0.0) pitch rotation, in radians
+        :param yaw: (default: 0.0) yaw rotation, in radians
+        """
+        self.local_data['roll'] += roll
+        self.local_data['pitch'] += pitch
+        self.local_data['yaw'] += yaw
+
 
     def default_action(self):
         """ Change the parent robot pose. """
