a2555dd8ae0c231fe0ba8e80c67740205940731d
Séverin Lemaignan <severin.lemaignan@laas.fr>
2013-01-21 17:27:19 +0100

    [ros] Improved the overlay for JointTrajectoryActions (armatures)
    The order of joints is now correctly parsed.
    The overlay now accept a 'namespace' argument to override the default component name.

--- src/morse/middleware/ros/overlays/armatures.py
+++ src/morse/middleware/ros/overlays/armatures.py
@@ -2,22 +2,49 @@ from morse.middleware.ros_request_manager import ros_service, ros_action
 from morse.core.services import interruptible
 from morse.core.overlay import MorseOverlay
 from morse.core import status
+from morse.core.exceptions import MorseServiceError
 
 import logging; logger = logging.getLogger("morse."+ __name__)
+
+import rospy # to set the parameters
 
 from pr2_controllers_msgs.msg import *
 
 class ArmatureController(MorseOverlay):
+    """
+    This overlay provides a ROS JointTrajectoryAction interface to armatures.
 
-    def __init__(self, overlaid_object):
+    It is meant to be applied on a Armature actuator.
+
+    Besides the ROS action server, it also sets a parameter with the list of
+    joints.
+    """
+
+    def __init__(self, overlaid_object, namespace = None):
         # Call the constructor of the parent class
         super(self.__class__,self).__init__(overlaid_object)
+
+        joints = list(overlaid_object.local_data.keys())
+
+        self.namespace = namespace
+
+        name = self.name().replace(".", "/")
+
+        rospy.set_param(name + "/joints", joints)
+        rospy.set_param(name + "/joint_trajectory_action/joints", joints)
 
     def _stamp_to_secs(self, stamp):
         return stamp.secs + stamp.nsecs / 1e9
 
     def joint_trajectory_action_result(self, result):
         return result
+
+    def name(self):
+
+        if self.namespace:
+            return self.namespace
+        else:
+            return super(self.__class__, self).name()
 
     @interruptible
     @ros_action(type = JointTrajectoryAction)
@@ -29,12 +56,27 @@ class ArmatureController(MorseOverlay):
         req = req.trajectory
         traj["starttime"] = self._stamp_to_secs(req.header.stamp)
 
+        joint_names = req.joint_names
+        target_joints = self.overlaid_object.local_data.keys()
+        diff = set(joint_names).difference(set(target_joints))
+
+        if diff:
+            raise MorseServiceError("Trajectory contains unknown joints! %s" % diff)
+
         points = []
         for p in req.points:
             point = {}
-            point["positions"] = p.positions
-            point["velocities"] = p.velocities
-            point["accelerations"] = p.accelerations
+
+            # Re-order joint values to match the local_data order
+
+            pos = dict(zip(joint_names, p.positions))
+            point["positions"] = [pos[j] for j in target_joints if j in pos]
+            vel = dict(zip(joint_names, p.velocities))
+            point["velocities"] = [vel[j] for j in target_joints if j in vel]
+
+            acc = dict(zip(joint_names, p.accelerations))
+            point["accelerations"] = [acc[j] for j in target_joints if j in acc]
+
             point["time_from_start"] = self._stamp_to_secs(p.time_from_start)
             points.append(point)
 
