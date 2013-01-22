995055e0113be200f0178ee97b463821107fcb3c
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-01-22 17:38:59 +0100

    [actuator] Prepare orientation for automatic documentation
    while here, handle a style issue

--- src/morse/actuators/orientation.py
+++ src/morse/actuators/orientation.py
@@ -1,18 +1,32 @@
 import logging; logger = logging.getLogger("morse." + __name__)
-import mathutils
 import morse.core.actuator
+from morse.helpers.components import add_data
+from morse.core import mathutils
 
 class OrientationActuatorClass(morse.core.actuator.Actuator):
-    """ Motion controller changing the robot orientation
-
-    This class will read angles as input from an external middleware,
-    and then change the robot orientation accordingly.
     """
+    Motion controller changing immediately the robot orientation.
+
+    This actuator reads the values of angles of rotation around the 3
+    axis and applies them to the associated robot. This rotation is
+    applied instantly (not in a realist way). Angles are expected in
+    radians.
+    """
+
+    _name = "Orientation Actuator"
+    _short_desc = "An actuator to change instantly robot orientation."
+
+    add_data('yaw', 'Initial robot yaw', "float",
+                    'Rotation of the robot around Z axis, in radian')
+    add_data('pitch', 'Initial robot pitch', "float",
+                      'Rotation of the robot around Y axis, in radian')
+    add_data('roll', 'Initial robot roll', "float",
+                      'Rotation of the robot around X axis, in radian')
 
     def __init__(self, obj, parent=None):
         logger.info('%s initialization' % obj.name)
         # Call the constructor of the parent class
-        super(self.__class__,self).__init__(obj, parent)
+        super(self.__class__, self).__init__(obj, parent)
 
         self.orientation = self.bge_object.orientation.to_euler('XYZ')
 
