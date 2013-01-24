73b063edd0b1ef51767cc398ac398ac9f0f3e88d
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-01-24 14:00:42 +0100

    [sensor] Rewrite semantic_camera logic + improve documentation

--- src/morse/sensors/semantic_camera.py
+++ src/morse/sensors/semantic_camera.py
@@ -5,28 +5,43 @@ import morse.sensors.camera
 import morse.helpers.colors
 
 from morse.helpers import passive_objects
+from morse.helpers.components import add_data
 
 class SemanticCameraClass(morse.sensors.camera.CameraClass):
     """
-    This module implements a "semantic camera" sensor for MORSE
+    This sensor emulates a hight level camera that outputs the names of
+    the objects that are located within the field of view of the camera.
 
-    This special camera returns the list of objects as seen by the robot's
-    cameras, with unique id, possibly (if set in the objects' properties)
-    the type of object and the colour of the object.
-    This camera is able to recognise objects marked with a 'Object' property
-    An additional 'Description' property can be set that defines the object 
-    category (like 'book', 'box', 'glass'...). If this property is not set, the 
-    default value 'object' is used instead.
+    The sensor determines first which objects are to be tracked (objects
+    marked with a **Logic Property** called ``Object``, cf documentation
+    on :doc:`passive objects <../others/passive_objects>` for more on
+    that). If the ``Label`` property is defined, it is used as exported
+    name. Else the Blender object name is used.
 
-    Other such high-level information (the semantic description of the scene)
-    can be added.
+    Then a test is made to identify which of these objects are inside of
+    the view frustum of the camera. Finally, a single visibility test is
+    performed by casting a ray from the center of the camera to the
+    center of the object. If anything other than the test object is
+    found first by the ray, the object is considered to be occluded by
+    something else, even if it is only the center that is being blocked.
 
-    Version: 1.0
-    Date: 16 Nov. 2009
-    Author: Severin Lemaignan <severin.lemaignan@laas.fr>
-
-    Copyright LAAS-CNRS 2009
+    The cameras make use of Blender's **bge.texture** module, which
+    requires a graphic card capable of GLSL shading. Also, the 3D view
+    window in Blender must be set to draw **Textured** objects.
     """
+
+    _name = "Semantic camera"
+
+    add_data('visible_objects', [], 'list<objects>',
+           "A list containing the different objects visible by the camera. \
+            Each object is represented by a dictionary composed of: \n\
+                - **name** (String): the name of the object \n\
+                - **type** (String): the type of the object \n\
+                - **position** (vec3<float>): the position of the \
+                  object, in meter, in the blender frame       \n\
+                - **orientation** (quaternion): the orientation of the \
+                  object, in the blender frame")
+
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -36,7 +51,7 @@ class SemanticCameraClass(morse.sensors.camera.CameraClass):
         """
         logger.info('%s initialization' % obj.name)
         # Call the constructor of the parent class
-        super(self.__class__,self).__init__(obj, parent)
+        super(self.__class__, self).__init__(obj, parent)
 
         # Locate the Blender camera object associated with this sensor
         main_obj = self.bge_object
@@ -46,38 +61,29 @@ class SemanticCameraClass(morse.sensors.camera.CameraClass):
                 logger.info("Camera object: {0}".format(self.blender_cam))
                 break
         if not self.blender_cam:
-            logger.error("no camera object associated to the semantic camera. " +\
-                  "The semantic camera requires a standard Blender camera in its children.")
+            logger.error("no camera object associated to the semantic camera. \
+                         The semantic camera requires a standard Blender  \
+                         camera in its children.")
 
-        # TrackedObject is a dictionary containing the list of tracked objects 
+        # TrackedObject is a dictionary containing the list of tracked objects
         # (->meshes with a class property set up) as keys
         #  and the bounding boxes of these objects as value.
         if not 'trackedObjects' in blenderapi.persistantstorage():
             logger.info('Initialization of tracked objects:')
-            scene = blenderapi.scene()
-            blenderapi.persistantstorage().trackedObjects = dict.fromkeys(passive_objects.active_objects())
+            blenderapi.persistantstorage().trackedObjects = \
+                            dict.fromkeys(passive_objects.active_objects())
 
             # Store the bounding box of the marked objects
             for obj in blenderapi.persistantstorage().trackedObjects.keys():
 
                 # bound_box returns the bounding box in local space
                 #  instead of world space.
-                blenderapi.persistantstorage().trackedObjects[obj] = blenderapi.objectdata(obj.name).bound_box
+                blenderapi.persistantstorage().trackedObjects[obj] = \
+                                    blenderapi.objectdata(obj.name).bound_box
 
                 details = passive_objects.details(obj)
-                logger.info('    - {0} (type:{1})'.format(details['label'], details['type']))
-
-
-        # Prepare the exportable data of this sensor
-        # In this case, it is the list of currently visible objects
-        # by each independent robot.
-        
-        # Array for lables of visible objects        
-        self.visibles = []
-
-        self.local_data['visible_objects'] = []
-        # Variable to indicate this is a camera
-        self.semantic_tag = True
+                logger.info('    - {%s} (type:%s)'%
+                            (details['label'], details['type']))
 
         logger.info('Component initialized')
 
@@ -85,91 +91,53 @@ class SemanticCameraClass(morse.sensors.camera.CameraClass):
     def default_action(self):
         """ Do the actual semantic 'grab'.
 
-        Iterate over all the tracked objects,
-        and check if they are visible for the robot.
-        Visible objects must have a boundin box and be active
-        for physical simulation (have the 'Actor' checkbox selected)
+        Iterate over all the tracked objects, and check if they are
+        visible for the robot.  Visible objects must have a bounding box
+        and be active for physical simulation (have the 'Actor' checkbox
+        selected)
         """
         # Call the action of the parent class
-        super(self.__class__,self).default_action()
+        super(self.__class__, self).default_action()
 
-        visibles = self.visibles
-
-        # check which objects are visible
-        for obj in blenderapi.persistantstorage().trackedObjects.keys():
-            label = passive_objects.label(obj)
-            visible = self._check_visible(obj)
-            obj_dict = dict([('name', label), ('position', obj.worldPosition)]) 
-            # Object is visible and not yet in the visible_objects list...
-            if visible and label not in visibles:
-                # Create dictionary to contain object name, type, description, position and orientation
-                self.visibles.append(label)
-                # Scale the object to show it is visible
-                #obj.localScale = [1.2, 1.2, 1.2]
-                logger.debug("Semantic %s: %s just appeared" % (self.bge_object.name, label))
-            
-            # Object is not visible and was in the visible_objects list...
-            if not visible and label in visibles:
-                self.visibles.remove(label)
-                # Return the object to normal size
-                #  when it is no longer visible
-                #obj.localScale = [1.0, 1.0, 1.0]
-                logger.debug("Semantic %s: %s just disappeared" % (self.bge_object.name, label))
-        
         # Create dictionaries
         self.local_data['visible_objects'] = []
         for obj in blenderapi.persistantstorage().trackedObjects.keys():
-            label = passive_objects.label(obj)
-            if label in visibles:
-                # Create dictionary to contain object name, type, description, position and orientation
-                obj_dict = dict([('name', label), ('description', ''), ('type', ''), ('position', obj.worldPosition), ('orientation', obj.worldOrientation.to_quaternion())])  
-                # Set description and type if those properties exist
-                try:
-                    obj_dict['description'] = obj['Description']
-                except KeyError:
-                    pass
-                try:
-                    obj_dict['type'] = obj['Type']
-                except KeyError:
-                    pass
+            if self._check_visible(obj):
+                # Create dictionary to contain object name, type,
+                # description, position and orientation
+                obj_dict = {'name': passive_objects.label(obj),
+                            'description': obj.get('Description', ''),
+                            'type': obj.get('Type', ''),
+                            'position': obj.worldPosition,
+                            'orientation': obj.worldOrientation.to_quaternion()}
                 self.local_data['visible_objects'].append(obj_dict)
-                
-        logger.debug("Visible objects: "+ str(self.local_data['visible_objects']))
 
-        # Create dictionary to contain object name, type, description, position and orientation
-        #obj_dict = dict([('name', label), ('description', ''), ('type', ''), ('position', obj.worldPosition), ('orientation', obj.worldOrientation.to_quaternion())]) 
-        # Set description and type if those properties exist
-        #try:
-        #    obj_dict['description'] = obj['Description']
-        #except KeyError:
-        #    pass
-        #try:
-        #    obj_dict['type'] = obj['Type']
-        #except KeyError:
-        #    pass
+        logger.debug("Visible objects: %s" % self.local_data['visible_objects'])
 
 
     def _check_visible(self, obj):
-        """ Check if an object lies inside of the camera frustrum. """
+        """ Check if an object lies inside of the camera frustum. """
         # TrackedObjects was filled at initialization
         #  with the object's bounding boxes
         bb = blenderapi.persistantstorage().trackedObjects[obj]
         pos = obj.position
+        bbox = [[bb_corner[i] + pos[i] for i in range(3)] for bb_corner in bb]
 
         logger.debug("\n--- NEW TEST ---")
         logger.debug("OBJECT '{0}' AT {1}".format(obj, pos))
-        logger.debug("CAMERA '{0}' AT {1}".format(self.blender_cam, self.blender_cam.position))
-        logger.debug("BBOX: >{0}<".format([[bb_corner[i] + pos[i] for i in range(3)] for bb_corner in bb]))
+        logger.debug("CAMERA '{0}' AT {1}".format(
+                                self.blender_cam, self.blender_cam.position))
+        logger.debug("BBOX: >{0}<".format(bbox))
         logger.debug("BBOX: {0}".format(bb))
 
         # Translate the bounding box to the current object position
-        #  and check if it is in the frustrum
-        if self.blender_cam.boxInsideFrustum([[bb_corner[i] + pos[i] for i in range(3)] for bb_corner in bb]) != self.blender_cam.OUTSIDE:
+        #  and check if it is in the frustum
+        if self.blender_cam.boxInsideFrustum(bbox) != self.blender_cam.OUTSIDE:
             # Check that there are no other objects between the camera
-            #  and the selected object
+            # and the selected object
             # NOTE: This is a very simple test. Hiding only the 'center'
-            #  of an object will make it invisible, even if the rest is still
-            #  seen from the camera
+            # of an object will make it invisible, even if the rest is
+            # still seen from the camera
             closest_obj = self.bge_object.rayCastTo(obj)
             if closest_obj in [obj] + list(obj.children):
                 return True
