a54b70b0d9b3fa83a3fcca3fdac1badbb8e4ae90
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-01-25 09:43:36 +0100

    [testing] Add some unit-test for pioneer3dx robot
    Cover issue #245
    Don't link it with the build for moment, as it is completely broken.

---
+++ testing/robots/pionner3dx/pionner3dx_testing.py
@@ -0,0 +1,221 @@
+#! /usr/bin/env python
+"""
+This script tests the Segway RMP400 robot with differential drive actuator
+"""
+
+import sys
+from time import sleep
+from morse.testing.testing import MorseTestCase
+from pymorse import Morse
+
+# Include this import to be able to use your test file as a regular 
+# builder script, ie, usable with: 'morse [run|exec] base_testing.py
+try:
+    from morse.builder import *
+except ImportError:
+    pass
+
+def gradual_speed(s, v, w, t):
+    """ Start and finish applying only half of the desired speed """
+    tic = t/20.0
+    s.publish({'v' : v/4, 'w' : w/4})
+    sleep(tic*2)
+    s.publish({'v' : v/2, 'w' : w/2})
+    sleep(tic*1)
+    s.publish({'v' : v, 'w' : w})
+    sleep(tic*18)
+    s.publish({'v' : v/2, 'w' : w/2})
+    sleep(tic*2)
+    s.publish({'v' : v/4, 'w' : w/4})
+    sleep(tic*2)
+    s.publish({'v' : 0.0, 'w' : 0.0})
+
+
+def send_speed(s, v, w, t):
+    #s.publish({'v' : v, 'w' : w})
+    #sleep(t)
+    #s.publish({'v' : 0.0, 'w' : 0.0})
+    gradual_speed(s, v, w, t)
+    sleep(1)
+
+def send_service_speed(s, v, w, t):
+    s.set_speed(v, w)
+    sleep(t)
+    s.stop()
+    sleep(1)
+
+class Pioneer3DXTest(MorseTestCase):
+    def setUpEnv(self):
+        """ Defines the test scenario, using the Builder API.
+        """
+        
+        robot = Pioneer3DX()
+        robot.translate(z=0.1)
+        robot.unparent_wheels()
+
+        pose = Pose()
+        robot.append(pose)
+        pose.translate(z=-0.1)
+        pose.add_stream('socket')
+
+        motion = MotionVWDiff()
+        robot.append(motion)
+        motion.add_stream('socket')
+        motion.configure_service('socket')
+        
+        env = Environment('empty', fastmode = True)
+        env.configure_service('socket')
+
+    def test_vw_controller(self):
+        with Morse() as morse:
+        
+            # Read the start position, it must be (0.0, 0.0, 0.0)
+            pose_stream = morse.robot.pose
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.03)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.03)
+
+            sleep(1)
+
+            # Check that it does not dance :)
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.03)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.03)
+
+            v_w = morse.robot.motion
+
+            send_speed(v_w, 1.0, 0.0, 2.0)
+
+            pose = pose_stream.get()
+            self.assertAlmostEqual(pose['x'], 2.0, delta=0.15)
+            self.assertAlmostEqual(pose['y'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['z'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['yaw'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.15)
+
+            send_speed(v_w, -1.0, 0.0, 2.0)
+
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.15)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.15)
+
+            """
+            send_speed(v_w, 0.0, -math.pi/4.0, 2.0)
+
+            pose = pose_stream.get()
+            # for non-null w, we have r = v /  w
+            self.assertAlmostEqual(pose['x'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['y'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['z'], 0.10, delta=0.15)
+            self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.15)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.15)
+
+            send_speed(v_w, 0.0, math.pi/4.0, 2.0)
+
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.15)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.15)
+            """
+
+
+            send_speed(v_w, 1.0, -math.pi/4.0, 2.0)
+
+            pose = pose_stream.get()
+            # for non-null w, we have r = v /  w
+            self.assertAlmostEqual(pose['x'], 4.0/ math.pi , delta=0.20)
+            self.assertAlmostEqual(pose['y'], -4.0/ math.pi , delta=0.20)
+            self.assertAlmostEqual(pose['z'], 0.0, delta=0.20)
+            self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.20)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.20)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.20)
+
+            send_speed(v_w, 0.5, -math.pi/8.0, 12.0)
+
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.15)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.20)
+
+            """
+            # The robot slips too much to expect a precise position.
+            # However, the behaviour seems reasonnable considering its
+            # real-life behaviour. 
+
+            send_speed(v_w, -2.0, math.pi/2.0, 3.0)
+
+            pose = pose_stream.get()
+            self.assertAlmostEqual(pose['x'], 4.0/ math.pi , delta=0.20)
+            self.assertAlmostEqual(pose['y'], -4.0/ math.pi , delta=0.20)
+            self.assertAlmostEqual(pose['z'], 0.0, delta=0.20)
+            self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.20)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.20)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.20)
+            """
+
+    def test_vw_service_controller(self):
+        with Morse() as morse:
+        
+            # Read the start position, it must be (0.0, 0.0, 0.0)
+            pose_stream = morse.robot.pose
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.02)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.02)
+
+            v_w = morse.robot.motion
+
+            send_service_speed(v_w, 1.0, 0.0, 2.0)
+
+            pose = pose_stream.get()
+            self.assertAlmostEqual(pose['x'], 2.0, delta=0.15)
+            self.assertAlmostEqual(pose['y'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['z'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['yaw'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.15)
+
+            send_service_speed(v_w, -1.0, 0.0, 2.0)
+
+            pose = pose_stream.get()
+            for key,coord in pose.items():
+                if key == 'z':
+                    self.assertAlmostEqual(coord, 0.10, delta=0.15)
+                else:
+                    self.assertAlmostEqual(coord, 0.0, delta=0.15)
+
+            send_service_speed(v_w, 1.0, -math.pi/4.0, 2.0)
+
+            pose = pose_stream.get()
+            # for non-null w, we have r = v /  w
+            self.assertAlmostEqual(pose['x'], 4.0/ math.pi , delta=0.2)
+            self.assertAlmostEqual(pose['y'], -4.0/ math.pi , delta=0.2)
+            self.assertAlmostEqual(pose['z'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['yaw'], -math.pi/2.0, delta=0.15)
+            self.assertAlmostEqual(pose['pitch'], 0.0, delta=0.15)
+            self.assertAlmostEqual(pose['roll'], 0.0, delta=0.15)
+
+########################## Run these tests ##########################
+if __name__ == "__main__":
+    import unittest
+    from morse.testing.testing import MorseTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(Pioneer3DXTest)
+    sys.exit(not MorseTestRunner().run(suite).wasSuccessful())
+
