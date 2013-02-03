16ce5f2901aa731b6c27d1a34ba2a5a820708517
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-03 13:29:54 +0100

    [depth] add AbstractCamera and RawImage classes
    RawImage wont convert the Z-Buffer in the sensor code.
    Let the middleware deal with image processing. For example convert and
    publish in C++, without having to re-serialize a Python object.

--- src/morse/sensors/depth_camera.py
+++ src/morse/sensors/depth_camera.py
@@ -8,17 +8,7 @@ from morse.helpers.components import add_data
 from morse.sensors.zbufferto3d import ZBufferTo3D
 from morse.sensors.zbuffertodepth import ZBufferToDepth
 
-
-class DepthCameraClass(VideoCameraClass):
-    """
-    This sensor generates a 3D point cloud from the camera perspective.
-    """
-
-    _name = "Depth (XYZ) camera"
-
-    add_data('points', 'none', 'memoryview', "List of 3D points from the depth "
-             "camera. memoryview of a set of float(x,y,z). The data is of size "
-             "``(cam_width * cam_height * 12)`` bytes (12=3*sizeof(float).")
+class AbstractCamera(VideoCameraClass):
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -28,12 +18,15 @@ class DepthCameraClass(VideoCameraClass):
         """
         # Call the constructor of the VideoCameraClass class
         VideoCameraClass.__init__(self, obj, parent)
+        # Component specific initialize (converters)
+        self.initialize()
 
-        # Store the camera parameters necessary for image processing
-        self.converter = ZBufferTo3D(self.local_data['intrinsic_matrix'][0][0],\
-                                     self.local_data['intrinsic_matrix'][1][1],\
-                                     self.near_clipping, self.far_clipping, \
-                                     self.image_width, self.image_height)
+    # abstractmethod
+    def process_image(self, image):
+        pass
+    # abstractmethod
+    def initialize(self):
+        pass
 
     def default_action(self):
         """ Update the texture image. """
@@ -43,10 +36,8 @@ class DepthCameraClass(VideoCameraClass):
             # Call the action of the CameraClass class
             CameraClass.default_action(self)
 
-            image_data = morse.core.blenderapi.cameras()[self.name()].source
+            self.process_image(morse.core.blenderapi.cameras()[self.name()].source)
 
-            # Convert the Z-Buffer
-            self.local_data['points'] = self.converter.recover(image_data)
             self.capturing = True
 
             if (self._n > 0):
@@ -57,7 +48,30 @@ class DepthCameraClass(VideoCameraClass):
             self.capturing = False
 
 
-class DepthVideoCameraClass(VideoCameraClass):
+
+class DepthCameraClass(AbstractCamera):
+    """
+    This sensor generates a 3D point cloud from the camera perspective.
+    """
+
+    _name = "Depth (XYZ) camera"
+
+    add_data('points', 'none', 'memoryview', "List of 3D points from the depth "
+             "camera. memoryview of a set of float(x,y,z). The data is of size "
+             "``(cam_width * cam_height * 12)`` bytes (12=3*sizeof(float).")
+
+    def initialize(self):
+        # Store the camera parameters necessary for image processing
+        self.converter = ZBufferTo3D(self.local_data['intrinsic_matrix'][0][0],\
+                                     self.local_data['intrinsic_matrix'][1][1],\
+                                     self.near_clipping, self.far_clipping, \
+                                     self.image_width, self.image_height)
+
+    def process_image(self, image):
+        self.local_data['points'] = self.converter.recover(image)
+
+
+class DepthVideoCameraClass(AbstractCamera):
     """
     This sensor generates a Depth 'image' from the camera perspective.
 
@@ -78,36 +92,31 @@ class DepthVideoCameraClass(VideoCameraClass):
              "converted in meters. memoryview of float of size "
              "``(cam_width * cam_height * sizeof(float))`` bytes.")
 
-    def __init__(self, obj, parent=None):
-        """ Constructor method.
-
-        Receives the reference to the Blender object.
-        The second parameter should be the name of the object's parent.
-        """
-        # Call the constructor of the parent class
-        VideoCameraClass.__init__(self, obj, parent)
-
+    def initialize(self):
         # Store the camera parameters necessary for image processing
         self.converter = ZBufferToDepth(self.near_clipping, self.far_clipping, \
                                         self.image_width, self.image_height)
 
-    def default_action(self):
-        """ Update the texture image. """
-        # Grab an image from the texture
-        if self.bge_object['capturing'] and (self._n != 0) :
+    def process_image(self, image):
+        # Convert the Z-Buffer
+        self.local_data['image'] = self.converter.recover(image)
 
-            # Call the action of the parent class
-            CameraClass.default_action(self)
 
-            image_data = morse.core.blenderapi.cameras()[self.name()].source
+class RawImage(AbstractCamera):
+    """
+    This sensor gets raw Z-Buffer from the camera perspective.
+    """
 
-            # Convert the Z-Buffer
-            self.local_data['image'] = self.converter.recover(image_data)
-            self.capturing = True
+    _name = "Depth camera (raw Z-Buffer)"
 
-            if (self._n > 0):
-                self._n -= 1
-                if (self._n == 0):
-                    self.completed(status.SUCCESS)
-        else:
-            self.capturing = False
+    add_data('image', 'none', 'buffer', "Raw Z-Buffer captured by the camera, "
+             "not converted. bgl.Buffer of float of size "
+             "``(cam_width * cam_height * sizeof(float))`` bytes.")
+
+    def process_image(self, image):
+        """Same behaviour as VideoCameraClass
+
+        Let the middleware deal with image processing. For example convert and
+        publish in C++, without having to re-serialize a Python object.
+        """
+        self.local_data['image'] = image
