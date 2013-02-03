8e38b9fd4d70232b106456d83e3d5aa290bbce4c
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-03 14:21:52 +0100

    [loading] remove duplicate code for class loading
    While here, use classpath as key for services

--- src/morse/helpers/loading.py
+++ src/morse/helpers/loading.py
@@ -4,7 +4,7 @@ Helpers for 'classpath' configuration
 import logging; logger = logging.getLogger("morse." + __name__)
 import sys
 
-def get_class(module_name, class_name):
+def load_module_attribute(module_name, attribute_name):
     """Dynamically import a Python class."""
     try:
         __import__(module_name)
@@ -14,16 +14,16 @@ def get_class(module_name, class_name):
     module = sys.modules[module_name]
     # Create an instance of the object class
     try:
-        klass = getattr(module, class_name)
+        attribute = getattr(module, attribute_name)
     except AttributeError as detail:
         logger.error("Module attribute not found: %s" % detail)
         return None
-    return klass
+    return attribute
 
 def create_instance(classpath, *args, **kwargs):
     """Creates an instances of a class."""
     module_name, class_name = classpath.rsplit('.', 1)
-    klass = get_class(module_name, class_name)
+    klass = load_module_attribute(module_name, class_name)
     if not klass:
         logger.error("Could not create an instance of %s"%str(classpath))
         return None
