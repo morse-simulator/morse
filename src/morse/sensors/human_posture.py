6e4ed01c6599c556fcdf89861e14a41e10e5e476
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-02-05 10:42:20 +0100

    [sensor/human_posture] Prepare it to automatic doc generation
    While here, fix some style issues

--- src/morse/sensors/human_posture.py
+++ src/morse/sensors/human_posture.py
@@ -1,18 +1,140 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import math
 import morse.core.sensor
+from morse.helpers.components import add_data
 
 class HumanPostureClass(morse.core.sensor.Sensor):
-    """ Class definition for the human posture exporter.
-        This sensor exports the posture of a human model moving in the simulator.
-        
-        A human posture is a vector of 39 floats. The 3 first are the [x,y,z] 
-        world position, the 3 next are the global rotation on the [x,y,z] axis.
-        The 33 other one are described on a separate document (ask easisbot@laas.fr)
-        
-        Note: all angles are expressed in radians
-        
-        """
+    """ 
+    This sensor collects the positions of the bones in the human armature
+    for the file ``$MORSE_ROOT/data/robots/human.blend``.
+
+    It stores the position and orientation of the general armature
+    object, as well as the local rotation of each individual bone. The
+    rotation angles are given in radians.
+
+    This sensor will only work for the ``human.blend`` model, as it
+    uses a specific naming convention for each of the bones.
+
+    You can also check to general documentation of the :doc:`human
+    component <../others/human>`.
+
+    .. image:: ../../../media/human_joints.png
+      :align: center
+      :width: 600
+    """
+
+    _name = "Human Posture"
+
+    add_data('x', 0.0, "float",
+             'global X position of the armature in the scene, in meter')
+    add_data('y', 0.0, "float",
+             'global Y position of the armature in the scene, in meter')
+    add_data('z', 0.0, "float",
+             'global Z position of the armature in the scene, in meter')
+    add_data('yaw', 0.0, "float",
+             'rotation angle with respect to the Z axis, in radian')
+    add_data('pitch', 0.0, "float",
+             'rotation angle with respect to the Z axis, in radian')
+    add_data('roll', 0.0, "float",
+             'rotation angle with respect to the Z axis, in radian')
+    add_data('empty1', 0.0, "float")
+    add_data('empty2', 0.0, "float")
+    add_data('empty3', 0.0, "float")
+    add_data('empty4', 0.0, "float")
+    add_data('empty5', 0.0, "float")
+    add_data('empty6', 0.0, "float")
+
+    add_data('dof_12', 0.0, 'float',
+             'rotation around the X axis for the torso, in radian')
+    add_data('dof_13', 0.0, 'float',
+             'rotation around the Y axis for the torso, in radian')
+    add_data('dof_14', 0.0, 'float',
+             'rotation around the Z axis for the torso, in radian')
+
+    add_data('dof_15', 0.0, 'float',
+             'rotation around the Z axis for the head, in radian')
+    add_data('dof_16', 0.0, 'float',
+             'rotation around the Y axis for the head, in radian')
+    add_data('dof_17', 0.0, 'float',
+             'rotation around the X axis for the head, in radian')
+
+    add_data('dof_18', 0.0, 'float',
+             'rotation around the X axis for the right shoulder, in radian')
+    add_data('dof_19', 0.0, 'float',
+             'rotation around the Y axis for the right shoulder, in radian')
+    add_data('dof_20', 0.0, 'float',
+             'rotation around the Z axis for the right shoulder, in radian')
+
+    add_data('dof_21', 0.0, 'float',
+             'elongation of the right arm, in meter')
+
+    add_data('dof_22', 0.0, 'float',
+            'rotation around the Z axis for the right elbow, in radian')
+
+    add_data('dof_23', 0.0, 'float', 'R_POINT')
+
+    add_data('dof_24', 0.0, 'float',
+             'rotation around the X axis for the right wrist, in radian')
+    add_data('dof_25', 0.0, 'float',
+             'rotation around the Y axis for the right wrist, in radian')
+    add_data('dof_26', 0.0, 'float',
+             'rotation around the Z axis for the right write, in radian')
+
+    add_data('dof_27', 0.0, 'float',
+            'rotation around the X axis for the left shoulder, in radian')
+    add_data('dof_28', 0.0, 'float',
+            'rotation around the Y axis for the left shoulder, in radian')
+    add_data('dof_29', 0.0, 'float',
+            'rotation around the Z axis for the left shoulder, in radian')
+
+    add_data('dof_30', 0.0, 'float',
+             'elongation of the left arm, in meter')
+
+    add_data('dof_31', 0.0, 'float',
+             'rotation around the Z axis for the left elbow, in radian')
+
+    add_data('dof_32', 0.0, 'float', 'L_POINT')
+
+    add_data('dof_33', 0.0, 'float', 
+            'rotation around the X axis for the left wrist, in radian')
+    add_data('dof_34', 0.0, 'float', 
+            'rotation around the Y axis for the left wrist, in radian')
+    add_data('dof_35', 0.0, 'float', 
+            'rotation around the Z axis for the left wrist, in radian')
+
+    add_data('dof_36', 0.0, 'float', 
+            'rotation around the X axis for the right hip, in radian')
+    add_data('dof_37', 0.0, 'float', 
+            'rotation around the Y axis for the right hip, in radian')
+    add_data('dof_38', 0.0, 'float', 
+            'rotation around the Z axis for the right hip, in radian')
+
+    add_data('dof_39', 0.0, 'float',
+            'rotation around the Z axis for the right knee, in radian')
+
+    add_data('dof_40', 0.0, 'float',
+            'rotation around the X axis for the right ankle, in radian')
+    add_data('dof_41', 0.0, 'float',
+            'rotation around the Y axis for the right ankle, in radian')
+    add_data('dof_42', 0.0, 'float',
+            'rotation around the Z axis for the right ankle, in radian')
+
+    add_data('dof_43', 0.0, 'float',
+             'rotation around the X axis for the left hip, in radian')
+    add_data('dof_44', 0.0, 'float',
+             'rotation around the Y axis for the left hip, in radian')
+    add_data('dof_45', 0.0, 'float',
+             'rotation around the Z axis for the left hip, in radian')
+
+    add_data('dof_46', 0.0, 'float',
+             'rotation around the Z axis for the left knee, in radian')
+
+    add_data('dof_47', 0.0, 'float',
+             'rotation around the X axis for the left ankle, in radian')
+    add_data('dof_48', 0.0, 'float',
+             'rotation around the Y axis for the left ankle, in radian')
+    add_data('dof_49', 0.0, 'float',
+             'rotation around the Z axis for the left ankle, in radian')
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -20,68 +142,7 @@ class HumanPostureClass(morse.core.sensor.Sensor):
             The second parameter should be the name of the object's parent. """
         logger.info('%s initialization' % obj.name)
         # Call the constructor of the parent class
-        super(self.__class__,self).__init__(obj, parent)
-
-        self.local_data['x'] = 0.0
-        self.local_data['y'] = 0.0
-        self.local_data['z'] = 0.0
-        self.local_data['roll'] = 0.0
-        self.local_data['pitch'] = 0.0
-        self.local_data['yaw'] = 0.0
-        self.local_data['empty1'] = 0.0
-        self.local_data['empty2'] = 0.0
-        self.local_data['empty3'] = 0.0
-        self.local_data['empty4'] = 0.0
-        self.local_data['empty5'] = 0.0
-        self.local_data['empty6'] = 0.0
-        self.local_data['dof_12'] = 0.0     # Torso X
-        self.local_data['dof_13'] = 0.0     # Torso Y
-        self.local_data['dof_14'] = 0.0     # Torso Z
-        self.local_data['dof_15'] = 0.0     # Head Z
-        self.local_data['dof_16'] = 0.0     # Head Y
-        self.local_data['dof_17'] = 0.0     # Head X
-        self.local_data['dof_18'] = 0.0     # R_Shoulder X
-        self.local_data['dof_19'] = 0.0     # R_Shoulder Z
-        self.local_data['dof_20'] = 0.0     # R_Shoulder Y
-        self.local_data['dof_21'] = 0.0     # R_Arm_Trans
-        self.local_data['dof_22'] = 0.0     # R_Elbow_Z
-        self.local_data['dof_23'] = 0.0     # L_Point
-        self.local_data['dof_24'] = 0.0     # R_Wrist X
-        self.local_data['dof_25'] = 0.0     # R_Wrist Y
-        self.local_data['dof_26'] = 0.0     # R_Wrist Z
-        self.local_data['dof_27'] = 0.0     # L_Shoulder X
-        self.local_data['dof_28'] = 0.0     # L_Shoulder Z
-        self.local_data['dof_29'] = 0.0     # L_Shoulder Y
-        self.local_data['dof_30'] = 0.0     # L_Arm_Trans
-        self.local_data['dof_31'] = 0.0     # L_Elbow_Z
-        self.local_data['dof_32'] = 0.0     # L_Point
-        self.local_data['dof_33'] = 0.0     # L_Wrist X
-        self.local_data['dof_34'] = 0.0     # L_Wrist Y
-        self.local_data['dof_35'] = 0.0     # L_Wrist Z
-        self.local_data['dof_36'] = 0.0     # R_Hip X
-        self.local_data['dof_37'] = 0.0     # R_Hip Y
-        self.local_data['dof_38'] = 0.0     # R_Hip Z
-        self.local_data['dof_39'] = 0.0     # R_Knee
-        self.local_data['dof_40'] = 0.0     # R_Ankle X
-        self.local_data['dof_41'] = 0.0     # R_Ankle Y
-        self.local_data['dof_42'] = 0.0     # R_Ankle Z
-        self.local_data['dof_43'] = 0.0     # L_Hip X
-        self.local_data['dof_44'] = 0.0     # L_Hip Y
-        self.local_data['dof_45'] = 0.0     # L_Hip Z
-        self.local_data['dof_46'] = 0.0     # L_Knee
-        self.local_data['dof_47'] = 0.0     # L_Ankle X
-        self.local_data['dof_48'] = 0.0     # L_Ankle Y
-        self.local_data['dof_49'] = 0.0     # L_Ankle Z
-
-
-        
-        """
-        self.data_keys = self.local_data.keys()
-
-        # Initialise the copy of the data
-        for variable in self.data_keys:
-            self.modified_data.append(self.local_data[variable])
-        """
+        super(self.__class__, self).__init__(obj, parent)
 
         logger.info('Component initialized')
 
@@ -89,7 +150,11 @@ class HumanPostureClass(morse.core.sensor.Sensor):
 
         for channel in armature.channels:
             if 'X_' not in channel.name:
-                logger.debug("\tChannel '%s': (%.4f, %.4f, %.4f)" % (channel, channel.joint_rotation[0], channel.joint_rotation[1], channel.joint_rotation[2]))
+                logger.debug("\tChannel '%s': (%.4f, %.4f, %.4f)" %
+                                        (channel,
+                                         channel.joint_rotation[0],
+                                         channel.joint_rotation[1],
+                                         channel.joint_rotation[2]))
                 if channel.name == 'Chest':
                     self.local_data['dof_12'] = channel.joint_rotation[2] #y
                     self.local_data['dof_13'] = channel.joint_rotation[0] #x
@@ -149,7 +214,7 @@ class HumanPostureClass(morse.core.sensor.Sensor):
         self.local_data['x'] = float(self.position_3d.x)
         self.local_data['y'] = float(self.position_3d.y)
         self.local_data['z'] = float(self.position_3d.z)
-        self.local_data['yaw'] = float(self.position_3d.yaw) - (math.pi/2) # 1.57 #pi/2
+        self.local_data['yaw'] = float(self.position_3d.yaw) - (math.pi/2)
         self.local_data['pitch'] = float(self.position_3d.pitch)
         self.local_data['roll'] = float(self.position_3d.roll)
 
