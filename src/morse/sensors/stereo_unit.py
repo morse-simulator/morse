b85d1d66367ad1fcda085a816fedd94e8341775a
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-02-06 14:18:22 +0100

    [doc] Prepare stereo_unit to doc generation
    While here, improve a bit the style

--- src/morse/sensors/stereo_unit.py
+++ src/morse/sensors/stereo_unit.py
@@ -5,11 +5,66 @@ from morse.core import blenderapi
 from functools import partial
 
 class StereoUnitClass(morse.core.sensor.Sensor):
-    """ Base for stereo pairs
-
-    It is used to link two camera objects, and export the images
-    as a stereo pair.
     """
+    The purpose of this component is to link together one or more
+    cameras, and provide them with the possibility to move together as a
+    single unit. It will also provide the connection interface to use
+    the information of the cameras attached to it. In the case of two
+    cameras, it will provide the stereo information generated from the
+    two camera images.
+
+    Related components
+    ------------------
+
+    A stereo unit needs to be the parent of one or more :doc:`cameras <video_camera>`.
+    Otherwise, it does no useful function.
+
+    The movement of the stereo unit is implemented by making it the child of a
+    :doc:`Pan-Tilt unit <../actuators/ptu>` actuator.
+
+    Here is an example of how to construct the whole stereo system to mount on top
+    of a robot, using the Builder API. Note the order in which components are
+    appended to each other, as this is important to get the desired functionality:
+
+    .. code-block:: python
+
+        from morse.builder import *
+
+        # Add a robot
+        atrv = ATRV()
+        atrv.translate(z=0.1000)
+        
+        # A pan-tilt unit to be able to orient the cameras
+        Platine = PTU()
+        Platine.translate(x=0.2000, z=0.9000)
+        atrv.append(Platine)
+        
+        # The STEREO UNIT, where the two cameras will be fixed
+        Stereo = StereoUnit()
+        Stereo.translate(z=0.0400)
+        Platine.append(Stereo)
+        
+        # Left camera
+        CameraL = VideoCamera()
+        CameraL.translate(x=0.1000, y=0.2000, z=0.0700)
+        Stereo.append(CameraL)
+        CameraL.properties(capturing = True)
+        CameraL.properties(cam_width = 320)
+        CameraL.properties(cam_height = 240)
+        CameraL.properties(cam_focal = 25.0000)
+        
+        # Right camera
+        CameraR = VideoCamera()
+        CameraR.translate(x=0.1000, y=-0.2000, z=0.0700)
+        Stereo.append(CameraR)
+        CameraR.properties(capturing = True)
+        CameraR.properties(cam_width = 320)
+        CameraR.properties(cam_height = 240)
+        CameraR.properties(cam_focal = 25.0000)
+
+    """
+
+    _name = "stereo_unit"
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -18,7 +73,7 @@ class StereoUnitClass(morse.core.sensor.Sensor):
         """
         logger.info('%s initialization' % obj.name)
         # Call the constructor of the parent class
-        super(self.__class__,self).__init__(obj, parent)
+        super(self.__class__, self).__init__(obj, parent)
 
         self.num_cameras = 0
         self.camera_list = []
@@ -28,14 +83,14 @@ class StereoUnitClass(morse.core.sensor.Sensor):
             # It is most likely just a geometric shape object
             try:
                 child['Component_Tag']
-            except KeyError as detail:
+            except KeyError:
                 continue
 
             camera_name = child.name
-            # Store only the name of the camera
-            # All data from the camera can be accessed later
-            #  by using blenderapi.persistantstorage().componentDict[camera_name],
-            #  which will return the instance of the camera object
+            # Store only the name of the camera All data from the camera
+            # can be accessed later by using
+            # blenderapi.persistantstorage().componentDict[camera_name],
+            # which will return the instance of the camera object
             self.camera_list.append(camera_name)
             self.num_cameras += 1
 
@@ -56,6 +111,13 @@ class StereoUnitClass(morse.core.sensor.Sensor):
 
     @async_service
     def capture(self, n):
+        """
+        The service takes an integer an argument and dispatch the call
+        to all its individual cameras. The service ends when each camera
+        has terminated its work.
+
+        :param n: the number of call to each individual camera
+        """
         self._expected_answer = self.num_cameras
         for camera in self.camera_list:
             camera_instance = blenderapi.persistantstorage().componentDict[camera]
