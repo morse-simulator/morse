11d7931629aa5452622fffc4c4d4bb2936e90a67
Séverin Lemaignan <severin.lemaignan@laas.fr>
2013-02-07 17:30:17 +0100

    [doc] Minor formatting in doc generation

--- doc/exts/tag.py
+++ doc/exts/tag.py
@@ -3,19 +3,20 @@
 
 from docutils import nodes
 
+
 def tag_role(role, rawtext, text, lineno, inliner,
                        options={}, content=[]):
 
-    categories = {'pymorse': 'middleware',
-                  'ros': 'middleware',
-                  'pocolibs': 'middleware',
-                  'yarp_json': 'middleware',
-                  'text': 'middleware',
-                  'socket': 'middleware',
-                  'yarp': 'middleware',
-                  'builder': 'api',
-                  'datastream': 'access',
-                  'service': 'access'}
+    categories = {'pymorse': ('middleware', 'pymorse'),
+                  'ros': ('middleware', 'user/middlewares/ros'),
+                  'pocolibs': ('middleware', 'user/middlewares/pocolibs'),
+                  'yarp_json': ('middleware', 'user/middlewares/yarp'),
+                  'text': ('middleware', 'user/middlewares/text'),
+                  'socket': ('middleware', 'user/middlewares/socket'),
+                  'yarp': ('middleware', 'user/middlewares/yarp'),
+                  'builder': ('api', 'user/builder'),
+                  'datastream': ('access', None),
+                  'service': ('access', 'dev/services')}
     if text not in categories:
         msg = inliner.reporter.error(
             'Unknown category for tag "%s". Check exts/tag.py '
@@ -23,9 +24,21 @@ def tag_role(role, rawtext, text, lineno, inliner,
         prb = inliner.problematic(rawtext, rawtext, msg)
         return [prb], [msg]
     
+    # deseperately tried to generate a relative path to the doc root
+    # but it's a failure.
+    # inliner.document.settings.env may be a good starting point
+    
+    #url = categories[text][1]
+    #node = None
+    #if url:
+    #    import pdb;pdb.set_trace()
+    #    node = nodes.reference(rawtext, text, refuri = url + ".html")
+    #else:
+    #    node = nodes.literal(rawtext, text)
+
     node = nodes.literal(rawtext, text)
     node['classes'].append("tag")
-    node['classes'].append("tag-" + categories[text])
+    node['classes'].append("tag-" + categories[text][0])
 
     return [node], []
 
