74bc33cd219d23e4eedc1d4c55f1a50d00dd7398
Séverin Lemaignan <severin.lemaignan@laas.fr>
2013-02-08 16:17:19 +0100

    [pymorse] Moved pymorse into its own module.
    This should have 0 impact on the way pymorse is used in scripts

--- bindings/pymorse/setup.py
+++ bindings/pymorse/setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='pymorse',
-      version='0.1',
+      version='1.0',
+      license='BSD 3-clauses',
       description='Python bindings for the Modular OpenRobots Simulation Engine (MORSE)',
       author='Séverin Lemaignan',
       author_email='severin.lemaignan@laas.fr',
       url='http://morse.openrobots.org',
-      py_modules=['pymorse'],
+      package_dir = {'':'src'},
+      packages=['pymorse'],
       )
---
+++ bindings/pymorse/src/pymorse/__init__.py
@@ -0,0 +1,2 @@
+from .pymorse import *
+
