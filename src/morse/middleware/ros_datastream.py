6bfc4442d4cb891474c74a333c222b7b385bec42
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-08 16:40:46 +0100

    [ros] allow classes outside morse package

--- src/morse/middleware/ros_datastream.py
+++ src/morse/middleware/ros_datastream.py
@@ -12,10 +12,10 @@ class ROS(Datastream):
 
         datastream_classpath = mw_data[1] # aka. function name
         # Check for new configuration
-        if datastream_classpath.startswith('morse.middleware.ros.'):
-            datastream_args = None
-            if len(mw_data) > 2:
-                datastream_args = mw_data[2] # aka. kwargs, a dictonnary of args
-            register_datastream(datastream_classpath, component_instance, datastream_args)
-        else:
-            logger.error("%s must starts with 'morse.middleware.ros.'"%datastream_classpath)
+        if not datastream_classpath.startswith('morse.middleware.ros.'):
+            logger.info("%s is not part of the 'morse.middleware.ros' package"%\
+                        datastream_classpath)
+        datastream_args = None
+        if len(mw_data) > 2:
+            datastream_args = mw_data[2] # aka. kwargs, a dictonnary of args
+        register_datastream(datastream_classpath, component_instance, datastream_args)
