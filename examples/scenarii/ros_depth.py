0405a9a4a54ed193750278f74144addf83a8fc18
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-12 15:40:36 +0100

    [example] update ros depth example

--- examples/scenarii/ros_depth.py
+++ examples/scenarii/ros_depth.py
@@ -6,24 +6,19 @@ atrv = ATRV()
 # Append an actuator
 motion = MotionVW()
 motion.translate(z=0.3)
-motion.name = 'Motion'
 atrv.append(motion)
 motion.add_stream('ros')
 
 # Append an Odometry sensor
 odometry = Odometry()
-odometry.name = 'Odometry'
 odometry.translate(z=0.73)
 atrv.append(odometry)
 odometry.add_stream('ros')
 
 # Append a camera
 camera = DepthCamera()
-camera.name = 'DepthCamera'
 camera.translate(x = 0.3, z = 0.76)
-camera.properties(cam_width = 640, cam_height = 480,
-                  cam_near = 1.0, cam_far = 20.0,
-                  capturing = True, Depth = True)
+camera.properties(cam_width = 640, cam_height = 480)
 camera.frequency(15)
 atrv.append(camera)
 camera.add_stream('ros')
