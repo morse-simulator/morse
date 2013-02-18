2dfacbfafd97a3c6c38fd93b134f091cd58feec2
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-18 14:21:07 +0100

    [examples] cat-mouse uses pymorse

--- examples/tutorials/cat_mouse_game.py
+++ examples/tutorials/cat_mouse_game.py
@@ -1,39 +1,43 @@
 from morse.builder import *
 
-Cat = ATRV()
-Cat.name = "CAT"
-Cat.translate(x=-6.0, z=0.2)
-#Cat.rotate(z=-1.5708)
+#
+# "cat" robot
+#
+cat = ATRV()
+cat.translate(x=-6.0, z=0.2)
 
-V_W = MotionVW()
-Cat.append(V_W)
+motion = MotionVW()
+cat.append(motion)
 
-Semantic_L = SemanticCamera()
-Semantic_L.translate(x=0.2, y=0.3, z=0.9)
-Semantic_L.name = "Camera_L"
-Cat.append(Semantic_L)
+semanticL = SemanticCamera()
+semanticL.translate(x=0.2, y=0.3, z=0.9)
+cat.append(semanticL)
+semanticL.properties(Vertical_Flip=False)
 
-Semantic_R = SemanticCamera()
-Semantic_R.translate(x=0.2, y=-0.3, z=0.9)
-Semantic_R.name = "Camera_R"
-Cat.append(Semantic_R)
+semanticR = SemanticCamera()
+semanticR.translate(x=0.2, y=-0.3, z=0.9)
+cat.append(semanticR)
+semanticR.properties(Vertical_Flip=False)
 
-V_W.add_stream('socket')
-Semantic_L.add_stream('socket')
-Semantic_R.add_stream('socket')
+motion.add_stream('socket')
+semanticL.add_stream('socket')
+semanticR.add_stream('socket')
 
+#
+# "mouse" robot
+#
+mouse = ATRV()
+mouse.properties(Object = True, Graspable = False, Label = "MOUSE")
+mouse.translate (x=1.0, z=0.2)
 
-Mouse = ATRV()
-Mouse.name = "MOUSE"
-Mouse.properties(Object = True, Graspable = False, Label = "MOUSE")
-Mouse.translate (x=1.0, z=0.2)
+keyboard = Keyboard()
+keyboard.properties(Speed=3.0)
+mouse.append(keyboard)
 
-Keyb = Keyboard()
-Keyb.properties(Speed=3.0)
-Mouse.append(Keyb)
-
-
+#
+# Environment
+#
 env = Environment('land-1/trees')
 env.place_camera([10.0, -10.0, 10.0])
 env.aim_camera([1.0470, 0, 0.7854])
-env.select_display_camera(Semantic_L)
+env.select_display_camera(semanticL)
