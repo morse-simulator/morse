b20ee06239f8c21151b7304349afb6e1f92783c3
Cyril Robin <cyril.robin@laas.fr>
2013-02-18 16:37:46 +0100

    [Examples] Typo and correct x-axis camera rotation

--- examples/tutorials/flying_outdoor_cat_mouse_game.py
+++ examples/tutorials/flying_outdoor_cat_mouse_game.py
@@ -3,41 +3,29 @@ from morse.builder import *
 
 """ Cat (Quadrotor) """
 cat = Quadrotor()
-cat.name = "CAT"
 cat.translate(x=-6.0, z=2.2)
 #Cat.rotate(z=-1.5708)
 
 # Waypoint controller (x,y,z, yaw and tolerance (default is 0.2))
 waypoint = RotorcraftWaypoint()
-waypoint.name = 'waypoint'
 cat.append(waypoint)
 waypoint.add_stream('socket')
 
 semanticC = SemanticCamera()
 semanticC.translate(x=0.3, z=-0.05)
-semanticC.rotate(x=-0.2)
-semanticC.name = "Camera"
+semanticC.rotate(x=+0.2)
 cat.append(semanticC)
 semanticC.add_stream('socket')
 semanticC.properties(Vertical_Flip=False)
 
 catPose = Pose()
-catPose.name = "Cat_Pose"
 cat.append(catPose)
 catPose.add_stream('socket')
 
-# imu = IMU()
-# imu.name = 'imu'
-# # IMU with z-axis down (NED)
-# imu.rotate(x=math.pi)
-# imu.add_stream('ros')
-# cat.append(imu)
 
 
-
-""" Mouse (atrv)"""
+""" mouse (atrv)"""
 mouse = ATRV()
-mouse.name = "MOUSE"
 mouse.properties(Object = True, Graspable = False, Label = "MOUSE")
 mouse.translate (x=1.0, z=0.2)
 
@@ -46,7 +34,6 @@ keyb.properties(Speed=4.0)
 mouse.append(keyb)
 
 mousePose = Pose()
-mousePose.name = "Mouse_Pose"
 mouse.append(mousePose)
 mousePose.add_stream('socket')
 
