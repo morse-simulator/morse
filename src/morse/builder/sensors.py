d208d4e26728bb9f7f88a2f66a3bfc3e5e135bb0
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-26 12:38:47 +0100

    [builder] add VelodyneZB (z-buffer Velodyne)

--- src/morse/builder/sensors.py
+++ src/morse/builder/sensors.py
@@ -369,6 +369,25 @@ class DepthCamera(VideoCamera):
                         Vertical_Flip=False)
         self.camera.rotate(z=math.pi)
 
+class VelodyneZB(DepthCamera):
+    def __init__(self, name=None):
+        DepthCamera.__init__(self, name)
+        # Always (MORSE_LOGIC) - And - Motion (rotation z: 0.017453)
+        morselogic = [sensor for sensor in self._bpy_object.game.sensors \
+                      if sensor.name.startswith('MORSE_LOGIC')]
+        sensor = morselogic[0]
+        bpymorse.add_controller(type='LOGIC_AND')
+        controller = self._bpy_object.game.controllers[-1]
+        # Motion (rotation z: 0.017453)
+        bpymorse.add_actuator(type='MOTION', name='MORSE_ROTATE')
+        actuator = self._bpy_object.game.actuators[-1]
+        actuator.offset_rotation.y = math.radians(1)
+        controller.link(sensor = sensor, actuator = actuator)
+    def set_rotation(self, rotation):
+        """set rotation per tic (in radian, relative to component frequency)"""
+        actuator = self._bpy_object.game.actuators['MORSE_ROTATE']
+        actuator.offset_rotation.y = rotation
+
 class SemanticCamera(VideoCamera):
     def __init__(self, name=None):
         VideoCamera.__init__(self, name, \
