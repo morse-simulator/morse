7ac08162b230796d6a748a183f0ed717490d899e
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-02-27 16:56:12 +0100

    [tools] Mostly rewrite simple_example.py so it looks like python
    It include correct indentation, removal of global, removal of useless
    import, and clean the code so it works for 1.0
    
    Other tools in this directory needs to be cleaned too, and maybe pass to
    pymorse.

--- tools/simple_example.py
+++ tools/simple_example.py
@@ -9,62 +9,55 @@ and 4 and 6 for left and right.
 """
 
 import sys
-import time
 import socket
-import json
 import tty, termios
-
-id_ = 0
 
 HOST = '127.0.0.1'
 PORT = 4000
-local_socket = None
 
 def getchar():
-   #Returns a single character from standard input
+    """ Returns a single character from standard input """
 
-   fd = sys.stdin.fileno()
-   old_settings = termios.tcgetattr(fd)
-   try:
-      tty.setraw(sys.stdin.fileno())
-      ch = sys.stdin.read(1)
-   finally:
-      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-   return ch
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 
 def _connect_port(port):
     """ Establish the connection with the given MORSE port"""
-    local_socket = None
+    sock = None
 
     for res in socket.getaddrinfo(HOST, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
         af, socktype, proto, canonname, sa = res
         try:
-            local_socket = socket.socket(af, socktype, proto)
-        except socket.error as msg:
-            local_socket = None
+            sock = socket.socket(af, socktype, proto)
+        except socket.error:
+            sock = None
             continue
         try:
-            local_socket.connect(sa)
-        except socket.error as msg:
-            local_socket.close()
-            local_socket = None
+            sock.connect(sa)
+        except socket.error:
+            sock.close()
+            sock = None
             continue
         break
 
-    return local_socket
-
+    return sock
 
 def main():
-    global local_socket
-    global id_
-    local_socket = _connect_port(PORT)
-    if not socket:
+    sock = _connect_port(PORT)
+    if not sock:
         sys.exit(1)
 
-    print("Socket connected")
+    print("sock connected")
     print("Please press q to quit and use 8456 to move")
     esc = 0
+    _id = 0
 
     while not esc:
         c = getchar()
@@ -79,15 +72,15 @@ def main():
         elif (c == "6"):
             rot = -0.1
         if (speed != 0 or rot != 0):
-            data_out = "id%s Human move [%s, %s]\n" % (str(id_), str(speed), str(rot))
-            sent = local_socket.send(data_out)
+            data_out = "id%d human move [%f, %f]\n" % (_id, speed, rot)
+            sent = sock.send(data_out)
             print ("SENT DATA (%d bytes): %s" % (sent, data_out))
-            id_ = id_ + 1
+            _id = _id + 1
 
         if c == "q":
             esc = 1
 
-    local_socket.close()
+    sock.close()
     print("\nBye bye!")
 
 main()
