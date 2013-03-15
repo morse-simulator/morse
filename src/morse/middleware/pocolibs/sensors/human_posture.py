e6cfc070d9d848ce35af9799a98841e718805687
Gregoire Milliez <gregoire.milliez@gmail.com>
2013-03-15 13:49:39 +0100

    Human mapping for spark is finished
    Note that spark was also updated to take 50 joints.

--- src/morse/middleware/pocolibs/sensors/human_posture.py
+++ src/morse/middleware/pocolibs/sensors/human_posture.py
@@ -10,31 +10,42 @@ class HumanPoster(PocolibsDataStreamOutput):
         super(self.__class__, self).initialize(STRUCT_SPARK_CONFIGURATION)
 
         self.obj = STRUCT_SPARK_CONFIGURATION()
-        self.obj.dofNb = 46
+        self.obj.dofNb = 50
         self.obj.changed = 1
 
     def default(self, component):
         raw_dofs = list(self.data.values())
         #for i in range(self.obj.dofNb):
-        for i in range(21):
+        for i in range(18):
             self.obj.dof[i] = raw_dofs[i]
 
-            #swap pitch and yaw
-            self.obj.dof[5] = raw_dofs[3]
-            self.obj.dof[3] = raw_dofs[5]
-            #Right arm length 
-            self.obj.dof[21] = 0
-            self.obj.dof[22] = raw_dofs[21]
-            self.obj.dof[23] = 0
+        #swap pitch and yaw
+        self.obj.dof[5] = raw_dofs[3]
+        self.obj.dof[3] = raw_dofs[5]
+        #swap right shoulder: X <- Y, Y <- Z, Z <- X
+        self.obj.dof[18] = raw_dofs[19]
+        self.obj.dof[19] = raw_dofs[20]
+        self.obj.dof[20] = raw_dofs[18]
+        #Right arm length
+        self.obj.dof[21] = 0
+        self.obj.dof[22] = raw_dofs[21]
+        self.obj.dof[23] = 0
 
-            for i in range(24, 30):
-                self.obj.dof[i] = raw_dofs[i-2]
+        for i in range(24, 27):
+            self.obj.dof[i] = raw_dofs[i-2]
+        #left shoulder:
+        #X <- Y
+        self.obj.dof[27] = -raw_dofs[26]
+        #Y <- Z
+        self.obj.dof[28] = raw_dofs[27]
+        #Z <- X
+        self.obj.dof[29] = raw_dofs[25]
 
-            self.obj.dof[30] = 0
-            self.obj.dof[31] = raw_dofs[28]
-            self.obj.dof[32] = 0
-            for i in range(33, self.obj.dofNb):
+        self.obj.dof[30] = 0
+        self.obj.dof[31] = raw_dofs[28]
+        self.obj.dof[32] = 0
+
+        for i in range(33, self.obj.dofNb):
                 self.obj.dof[i] = raw_dofs[i-4]
-
         self.write(self.obj)
 
