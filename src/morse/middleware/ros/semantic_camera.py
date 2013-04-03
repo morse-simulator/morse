77ccb70f724b7983d9d36a0cbcc1dced279f4e16
Pierrick Koch <pierrick.koch@gmail.com>
2013-04-03 12:06:47 +0200

    [ros] semantic camera use MorseEncoder

--- src/morse/middleware/ros/semantic_camera.py
+++ src/morse/middleware/ros/semantic_camera.py
@@ -1,30 +1,24 @@
 import logging; logger = logging.getLogger("morse." + __name__)
+import json
 import roslib; roslib.load_manifest('rospy'); roslib.load_manifest('std_msgs')
 import rospy
 from std_msgs.msg import String
 from morse.middleware.ros import ROSPublisherTF
+from morse.middleware.socket_datastream import MorseEncoder
 
 class SemanticCameraPublisher(ROSPublisherTF):
-    """ Publish the data of the semantic camera as a ROS String message, each
-    field separated by a comma, with newlines (for better visualization in console).
+    """ Publish the data of the semantic camera as JSON in a ROS String message.
+    And send TF transform between '/map' and ``object.name``.
     """
     ros_class = String
 
     def default(self, ci='unused'):
-        string = String()
         for obj in self.data['visible_objects']:
-            # if object has no description, set to '-'
-            if obj['description'] == '':
-                description = '-'
-
             # send tf-frame for every object
             self.sendTransform(obj['position'], obj['orientation'], \
                                rospy.Time.now(), str(obj['name']), "/map")
-
-            # Build string from name, description, location and orientation in the global world frame
-            string.data += "[" + str(obj['name']) + ", " + description + ", " + \
-                           str(obj['position']) + ", " + str(obj['orientation']) + " ]\n"
-
+        string = String()
+        string.data = json.dumps(self.data['visible_objects'], cls=MorseEncoder)
         self.publish(string)
 
 
