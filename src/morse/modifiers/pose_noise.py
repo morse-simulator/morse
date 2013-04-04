8f0b9d484c7a02cccd97496a1119c2e1a8dc7ca1
Felix Ruess <felix.ruess@gmail.com>
2013-04-04 14:59:57 +0200

    [modifiers] pose_noise: separate std_dev for each axis
    This makes it possible to specify the noise for each axis separately as a dict.
    If a single value is passed, it is used for all axes.
    E.g.:
    pose.alter('', 'morse.modifiers.pose_noise.PoseNoiseModifier', pos_std={'x':0.01, 'y':0.01, 'z':0.1}, rot_std=math.radians(1))
    or
    pose.alter('', 'morse.modifiers.pose_noise.PoseNoiseModifier', pos_std={'x':0.01, 'y':0.01, 'z':0.1}, rot_std={'roll':0.01, 'pitch':0.01, 'yaw':0.05})

--- src/morse/modifiers/pose_noise.py
+++ src/morse/modifiers/pose_noise.py
@@ -23,22 +23,36 @@ class NoiseModifier(AbstractModifier):
     * :py:class:`morse.modifiers.pose_noise.PoseNoiseModifier`
 
     """
-    
+
     _name = "PoseNoise"
-    
-    add_property('_pos_std_dev', 0.05, "pos_std", type = "float", 
-                 doc = "Standard deviation for position noise")
-    add_property('_rot_std_dev', radians(5), "rot_std", type = "float", 
-                 doc = "Standard deviation for rotation noise")
-    add_property('_2D', False, "_2D", type = "bool", 
-                 doc = "If True, noise is only applied to 2D pose attributes (i.e., x, y and yaw)")
-    
+
+    add_property('_pos_std_dev', {'x': 0.05, 'y': 0.05, 'z': 0.05}, "pos_std", type="dict",
+                 doc="Standard deviation for position noise as dictionary with x,y,z as floats")
+    add_property('_rot_std_dev', {'roll': radians(5), 'pitch': radians(5), 'yaw': radians(5)},
+                "rot_std", type="dict",
+                 doc="Standard deviation for rotation noise of roll,pitch,yaw axes as floats in radians")
+    add_property('_2D', False, "_2D", type="bool",
+                 doc="If True, noise is only applied to 2D pose attributes (i.e., x, y and yaw)")
+
     def initialize(self):
-        self._pos_std_dev = float(self.parameter("pos_std", default=0.05))
-        self._rot_std_dev = float(self.parameter("rot_std", default=radians(5)))
+        pos_std = self.parameter("pos_std", default=0.05)
+        if isinstance(pos_std, dict):
+            self._pos_std_dev = pos_std
+        else:
+            self._pos_std_dev = {'x': float(pos_std), 'y': float(pos_std), 'z': float(pos_std)}
+        rot_std = self.parameter("rot_std", default=radians(5))
+        if isinstance(rot_std, dict):
+            self._rot_std_dev = rot_std
+        else:
+            self._rot_std_dev = {'roll': float(rot_std), 'pitch': float(rot_std), 'yaw': float(rot_std)}
         self._2D = bool(self.parameter("_2D", default=False))
-        logger.info("Noise modifier standard deviations: position %.4f, rotation %.4f deg"
-                    % (self._pos_std_dev, degrees(self._rot_std_dev)))
+        if self._2D:
+            logger.info("Noise modifier standard deviations: x:%.4f, y:%.4f, yaw:%.3f deg"
+                    % (self._pos_std_dev['x'], self._pos_std_dev['y'], degrees(self._rot_std_dev['yaw'])))
+        else:
+            logger.info("Noise modifier standard deviations: x:%.4f, y:%.4f, z:%.4f, roll:%.3f deg, pitch:%.3f deg, yaw:%.3f deg"
+                    % (self._pos_std_dev['x'], self._pos_std_dev['y'], self._pos_std_dev['z'],
+                       degrees(self._rot_std_dev['roll']), degrees(self._rot_std_dev['pitch']), degrees(self._rot_std_dev['yaw'])))
 
 class PositionNoiseModifier(NoiseModifier):
     """ Add a gaussian noise to a position 
@@ -49,7 +63,7 @@ class PositionNoiseModifier(NoiseModifier):
             if not self._2D:
                 data_vars.append('z')
             for variable in data_vars:
-                self.data[variable] = random.gauss(self.data[variable], self._pos_std_dev)
+                self.data[variable] = random.gauss(self.data[variable], self._pos_std_dev[variable])
         except KeyError as detail:
             self.key_error(detail)
 
@@ -59,8 +73,13 @@ class OrientationNoiseModifier(NoiseModifier):
     def modify(self):
         # generate a gaussian noise rotation vector
         rot_vec = mathutils.Vector((0.0, 0.0, 0.0))
-        for i in range(0, 3):
-            rot_vec[i] = random.gauss(rot_vec[i], self._rot_std_dev)
+        if self._2D:
+            rot_vec[0] = random.gauss(rot_vec[0], self._rot_std_dev['roll'])
+            rot_vec[1] = random.gauss(rot_vec[1], self._rot_std_dev['pitch'])
+        else:
+            rot_vec[0] = 0.0
+            rot_vec[1] = 0.0
+        rot_vec[2] = random.gauss(rot_vec[2], self._rot_std_dev['yaw'])
         # convert rotation vector to a quaternion representing the random rotation
         angle = rot_vec.length
         if angle > 0:
@@ -73,13 +92,13 @@ class OrientationNoiseModifier(NoiseModifier):
             self.data['orientation'] = (noise_quat * self.data['orientation']).normalized()
         except KeyError:
             # for eulers this is a bit crude, maybe should use the noise_quat here as well...
-            try: 
+            try:
                 data_vars = ['yaw']
                 if not self._2D:
                     data_vars.append('roll')
                     data_vars.append('pitch')
                 for variable in data_vars:
-                    self.data[variable] = random.gauss(self.data[variable], self._rot_std_dev)
+                    self.data[variable] = random.gauss(self.data[variable], self._rot_std_dev[variable])
             except KeyError as detail:
                 self.key_error(detail)
 
