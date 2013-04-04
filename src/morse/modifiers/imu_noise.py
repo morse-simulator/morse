e0d89259ff25c0053d2fe77abd0c80d1f950bb63
Felix Ruess <felix.ruess@gmail.com>
2013-04-04 19:58:30 +0200

    [modifiers] imu_noise: dict for separate std_dev on each axis
    There is probably a more pythonic way to mix the index and key based access in a loop...

--- src/morse/modifiers/imu_noise.py
+++ src/morse/modifiers/imu_noise.py
@@ -1,5 +1,6 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import random
+from collections import defaultdict
 
 from morse.helpers.components import add_property
 from morse.modifiers.abstract_modifier import AbstractModifier
@@ -13,23 +14,39 @@ class IMUNoiseModifier(AbstractModifier):
 
     _name = "IMUNoise"
     
-    add_property('_gyro_std_dev', 0.5, "gyro_std", type = "float", 
-                 doc = "Standard deviation for noise applied to angular velocities")
-    add_property('_accel_std_dev', 0.5, "accel_std", type = "float", 
-                 doc = "Standard deviation for noise applied to linear accelerations")
+    add_property('_gyro_std_dev', {'x': 0.5, 'y': 0.5, 'z': 0.5}, "gyro_std", type = "dict", 
+                 doc = "Standard deviation for noise applied to angular velocities as dictionary with x,y,z as floats")
+    add_property('_accel_std_dev', {'x': 0.5, 'y': 0.5, 'z': 0.5}, "accel_std", type = "dict", 
+                 doc="Standard deviation for noise applied to linear accelerations as dictionary with x,y,z as floats")
     
     def initialize(self):
-        self._gyro_std_dev = float(self.parameter("gyro_std", default=0.5))
-        self._accel_std_dev = float(self.parameter("accel_std", default=0.5))
-        logger.info("IMU Noise standard deviations: gyro %.4f, accel %.4f"
-                    % (self._gyro_std_dev, self._accel_std_dev))
+        gyro_std = self.parameter("gyro_std", default=0.5)
+        if isinstance(gyro_std, dict):
+            self._gyro_std_dev = defaultdict(lambda: 0.0, gyro_std)
+        else:
+            self._gyro_std_dev = {'x': float(gyro_std), 'y': float(gyro_std), 'z': float(gyro_std)}
+        accel_std = self.parameter("accel_std", default=0.5)
+        if isinstance(accel_std, dict):
+            self._accel_std_dev = defaultdict(lambda: 0.0, accel_std)
+        else:
+            self._accel_std_dev = {'x': float(accel_std), 'y': float(accel_std), 'z': float(accel_std)}
+        logger.info("IMU Noise standard deviations: gyro x:%.4f, y:%.4f, z:%.4f,  accel x:%.4f, y:%.4f, z:%.4f,"
+                    % (self._gyro_std_dev['x'], self._gyro_std_dev['y'], self._gyro_std_dev['z'],
+                       self._accel_std_dev['x'], self._accel_std_dev['y'], self._accel_std_dev['z']))
 
     def modify(self):
         try:
-            for i in range(0, 3):
-                self.data['angular_velocity'][i] = \
-                random.gauss(self.data['angular_velocity'][i], self._gyro_std_dev)
-                self.data['linear_acceleration'][i] = \
-                random.gauss(self.data['linear_acceleration'][i], self._accel_std_dev)
+            self.data['angular_velocity'][0] = \
+                random.gauss(self.data['angular_velocity'][0], self._gyro_std_dev['x'])
+            self.data['linear_acceleration'][0] = \
+                random.gauss(self.data['linear_acceleration'][0], self._accel_std_dev['x'])
+            self.data['angular_velocity'][1] = \
+                random.gauss(self.data['angular_velocity'][1], self._gyro_std_dev['y'])
+            self.data['linear_acceleration'][1] = \
+                random.gauss(self.data['linear_acceleration'][1], self._accel_std_dev['y'])
+            self.data['angular_velocity'][2] = \
+                random.gauss(self.data['angular_velocity'][2], self._gyro_std_dev['z'])
+            self.data['linear_acceleration'][2] = \
+                random.gauss(self.data['linear_acceleration'][2], self._accel_std_dev['z'])
         except KeyError as detail:
             self.key_error(detail)
