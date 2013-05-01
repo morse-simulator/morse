c45fe2c42a8ae19cf202f940d0f18e3f5bcd4bee
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-05-01 10:52:11 +0200

    [testing] Support testing for invalid scenes
    This commit introduces a new class of tests: MorseBuilderFailureTestcase. These
    tests pass if MORSE *properly fails* to load the scene (ie, it tests that the
    GameEngine is not started).
    
    The commit also include 2 tests that currently do not pass (cf bug #374).
    
    While here:
     - improve management of PIDs (you do not need to get it from MORSE log,
       Popen.subprocess give it back to us). This allow to kill MORSE even
       if it did not reach the initialization stage.
    
    TODO: I've no example of invalid scripts that are correctly handled by
    MORSE, so I could not test that MorseBuilderFailureTestcase actually
    pass when MORSE dies before starting the BGE.

---
+++ testing/failures/invalid_builder_syntax.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+"""
+This script tests how MORSE handles invalid Builder scripts.
+"""
+
+import sys
+import math
+from time import sleep
+from morse.testing.testing import MorseBuilderFailureTestCase
+from pymorse import Morse
+
+# Include this import to be able to use your test file as a regular 
+# builder script, ie, usable with: 'morse [run|exec] <script>.py
+try:
+    from morse.builder import *
+except ImportError:
+    pass
+
+class InvalidName_Test(MorseBuilderFailureTestCase):
+    def setUpEnv(self):
+        """ Defines the test scenario, using the Builder API.
+        """
+        
+        robot = Morsy()
+        robot.name = "toto.toto"
+
+        env = Environment('empty', fastmode = True)
+        env.add_service('socket')
+
+    def test_builder_script(self):
+        with Morse() as simu:
+            pass # we only test the parsing of the script
+
+
+class NoEnvironment_Test(MorseBuilderFailureTestCase):
+    def setUpEnv(self):
+        """ Defines the test scenario, using the Builder API.
+        """
+        
+        robot = Morsy()
+
+        env = Environment('i_do_not_exist', fastmode = True)
+        env.add_service('socket')
+
+    def test_builder_script(self):
+        with Morse() as simu:
+            pass # we only test the parsing of the script
+
+########################## Run these tests ##########################
+if __name__ == "__main__":
+    import unittest
+    from morse.testing.testing import MorseTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(InvalidName_Test)
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(NoEnvironment_Test))
+    sys.exit(not MorseTestRunner().run(suite).wasSuccessful())
+
