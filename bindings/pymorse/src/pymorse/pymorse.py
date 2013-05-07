ea36d22c0236bac27581e2036495e9384b477a3d
Pierrick Koch <pierrick.koch@gmail.com>
2013-05-07 15:14:48 +0200

    [pymorse] patch asynchat.async_chat.initiate_send

--- bindings/pymorse/src/pymorse/pymorse.py
+++ bindings/pymorse/src/pymorse/pymorse.py
@@ -706,6 +706,42 @@ class Stream(asynchat.async_chat):
     def publish(self, msg):
         """ encode :param msg: and append the resulting bytes to the output queue """
         self.push(self.encode( msg ))
+    #### patch code from asynchat, ``del deque[0]`` is not safe #####
+    def initiate_send(self):
+        while self.producer_fifo and self.connected:
+            first = self.producer_fifo.popleft()
+            # handle empty string/buffer or None entry
+            if not first:
+                if first is None:
+                    self.handle_close()
+                    return
+
+            # handle classic producer behavior
+            obs = self.ac_out_buffer_size
+            try:
+                data = first[:obs]
+            except TypeError:
+                data = first.more()
+                if data:
+                    self.producer_fifo.appendleft(data)
+                continue
+
+            if isinstance(data, str) and self.use_encoding:
+                data = bytes(data, self.encoding)
+
+            # send the data
+            try:
+                num_sent = self.send(data)
+            except socket.error:
+                self.handle_error()
+                return
+
+            if num_sent:
+                if num_sent < len(data) or obs < len(first):
+                    self.producer_fifo.appendleft(first[num_sent:])
+            # we tried to send some actual data
+            return
+
 
     #### CODEC ####
     def decode(self, msg_bytes):
