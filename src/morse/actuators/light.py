5531be1beac245a3a5b6025b5eec08ac31067bd8
Charles Lesire <charles.lesire@onera.fr>
2013-06-18 11:32:24 +0200

    [actuator] add a service to toggle the light

--- src/morse/actuators/light.py
+++ src/morse/actuators/light.py
@@ -3,6 +3,7 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 from morse.core import blenderapi
 import morse.core.actuator
+from morse.core.services import service
 from morse.helpers.components import add_data, add_property
 from math import pi
 
@@ -19,11 +20,11 @@ class Light(morse.core.actuator.Actuator):
 
     add_data('emit', True, 'bool', 'On/Off light switch')
     
-    add_property('_emit', True, 'emit', doc="Set to False to initially disable the light")
-    add_property('_distance', 10, 'distance', doc="Distance at which the light energy is halfed")
-    add_property('_color', "(0,0,0)", 'color', doc="Light color")
-    add_property('_size', pi/2, 'size', doc="Light spot size")
-    add_property('_energy', 1.0, 'energy', doc="Light energy when On")
+    add_property('_emit', True, 'emit', 'bool', "Set to False to initially disable the light")
+    add_property('_distance', 10, 'distance', 'float', "Distance at which the light energy is halfed")
+    add_property('_color', "(0,0,0)", 'color', 'string', "Light color")
+    add_property('_size', pi/2, 'size', 'float', "Light spot size")
+    add_property('_energy', 1.0, 'energy', 'float', "Light energy when On")
 
     def __init__(self, obj, parent=None):
         logger.info("%s initialization" % obj.name)
@@ -45,3 +46,17 @@ class Light(morse.core.actuator.Actuator):
         else:
             self.light.energy = 0.0
 
+    @service
+    def toggle(self, emit=None):
+        """
+        Toggle the light.
+
+        :param emit: if emit is set to True/False, switch the light On/Off;
+            if emit is not set, toggle the light (from On to Off and conversely)
+        :return: Returns always True
+        """
+        if emit:
+            self.local_data['emit'] = emit
+        else:
+            self.local_data['emit'] = not self.local_data['emit']
+        return True
