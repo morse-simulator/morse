655132e10754d7d5a0e69e48261f858cae64e3c8
Charles Lesire <charles.lesire@onera.fr>
2013-06-19 17:34:23 +0200

    [thermometer] remove useless log

--- src/morse/sensors/thermometer.py
+++ src/morse/sensors/thermometer.py
@@ -44,8 +44,6 @@ class Thermometer(morse.core.sensor.Sensor):
         # Call the constructor of the parent class
         super(self.__class__, self).__init__(obj, parent)
 
-        logger.info("Thermometer initialized: default temperature %s", self._zero)
-
         logger.info("Component initialized, runs at %.2f Hz ", self.frequency)
 
     def default_action(self):
