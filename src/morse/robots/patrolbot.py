85d2a4292eab6cc64e07b747bc96fc284560fd6d
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-08-29 09:01:42 +0200

    [robots] Add missing _name for Patrolbot

--- src/morse/robots/patrolbot.py
+++ src/morse/robots/patrolbot.py
@@ -2,13 +2,15 @@ import logging; logger = logging.getLogger("morse." + __name__)
 import morse.core.wheeled_robot
 
 class PatrolBot(morse.core.wheeled_robot.MorsePhysicsRobot):
-    """ 
+    """
     The Research PatrolBot, developed by MobileRobots, is a
     differential-drive robot to carry payloads and sensors over all
     normal indoor surfaces in wheelchair-accessible facilities. The
     PatrolBot is an all-purpose indoor base, it can travel at speeds up
     to 2 m/s, and can carry up to 40kg over flat surfaces.
     """
+
+    _name = 'PatrolBot robot'
 
     def __init__(self, obj, parent=None):
         # Call the constructor of the parent class
