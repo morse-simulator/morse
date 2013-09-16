0bc934891a0d19b5e5fb0ae61e34365b17599800
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-09-16 11:48:59 +0200

    [bindings] Increment version number of pymorse

--- bindings/pymorse/setup.py
+++ bindings/pymorse/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pymorse',
-      version='1.0',
+      version='1.1',
       license='BSD 3-clauses',
       description='Python bindings for the Modular OpenRobots Simulation Engine (MORSE)',
       author='Séverin Lemaignan',
