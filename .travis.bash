18caa693d4b2a42aa8681d5224993b00776db096
Pierrick Koch <pierrick.koch@gmail.com>
2013-10-25 16:18:14 +0200

    [ci] run morse test w/xvfb
    - revert to blender 2.64 w/python3.2.3
      to sync with system's python3 version.
    - NOTE: increase color depth to 16,
      the default 8 bits is not enough for OpenGL.
    
    Thanks to Séverin Lemaignan for his help!

--- .travis.bash
+++ .travis.bash
@@ -1,28 +1,31 @@
 #!/bin/bash -e
 echo "MORSE installation and builder test"
 echo "========================================"
-lsb_release -a; uname -a; file $(which ls)
+lsb_release -a; uname -a
 [[ -z "$(uname -p | grep 64)" ]] && arch="i686" || arch="x86_64"
-BLENDER="blender-2.66-linux-glibc211-$arch"
+BLENDER="blender-2.64a-linux-glibc27-$arch"
+
 echo "Download Blender ${BLENDER}"
-(wget -q http://download.blender.org/release/Blender2.66/${BLENDER}.tar.bz2
+(wget -q http://download.blender.org/release/Blender2.64/${BLENDER}.tar.bz2
 tar jxf ${BLENDER}.tar.bz2 )& blenderpid=$!
-echo "Download CMake Python3 libSDL"
-sudo apt-get -q update
-sudo apt-get -mqy install cmake python3.2-dev libsdl1.2debian
-echo "========================================"
-echo "Check if libSDL was installed"
-ls -l /usr/lib/*-linux-gnu/libSDL*
+
 echo "Setup MORSE env"
-export MORSE_BLENDER=$(pwd)/${BLENDER}/blender
+echo -e "#! /bin/sh\n$(pwd)/${BLENDER}/blender -setaudio NULL \$@" > blender
+chmod +x blender
+export MORSE_BLENDER=$(pwd)/blender
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.2/site-packages
-export MORSE_ROOT=/usr/local
+
 echo "Build and install MORSE"
-mkdir build && cd build && cmake -DPYMORSE_SUPPORT=ON .. && make && sudo make install
+mkdir build && cd build
+cmake -DPYMORSE_SUPPORT=ON ..
+make
+sudo make install
 wait $blenderpid # Blender should be ready now
-echo "Run Blender in background mode (travis=headless). Test the builder."
+
+echo "Run testing/base/pose_testing.py"
 echo "========================================"
-morselib=${MORSE_ROOT}/share/morse
-$MORSE_BLENDER -setaudio NULL -b ${morselib}/data/morse_default.blend -P ${morselib}/examples/scenarii/travis-save.py
-echo "========================================"
-ls -l travis-succeed.blend && echo "Bravo ! MORSE installation completed."
+
+touch PoseTest.log
+tail -f PoseTest.log &
+xvfb-run --server-args="-screen 0 1x21x16" python3 ../testing/base/pose_testing.py
+kill % # kill tail
