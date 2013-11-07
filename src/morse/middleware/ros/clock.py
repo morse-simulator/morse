0b22047ce616fc02adc85af1b9ecf20cabac8228
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-11-07 14:11:42 +0100

    [style] Remove trailing semi-colons
    Automatic cleanup with PyCharm

--- src/morse/middleware/ros/clock.py
+++ src/morse/middleware/ros/clock.py
@@ -1,4 +1,4 @@
-import roslib; roslib.load_manifest('rospy'); roslib.load_manifest('rosgraph_msgs');
+import roslib; roslib.load_manifest('rospy'); roslib.load_manifest('rosgraph_msgs')
 import rospy
 from rosgraph_msgs.msg import Clock
 from morse.middleware.ros import ROSPublisher
