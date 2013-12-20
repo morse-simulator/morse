092407fd16706f604c9f79db186928d36c09705c
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-12-20 18:04:14 +0100

    Add support for IK in armatures
    Kinematic chains can now be controlled via inverse kinematics
    by placing/moving so-called IK targets that are tracked by your
    armature.
    
    A new service (Armature.place_IK_target) allows to instanteanously
    place and orient an IK target, while the asynchronous service
    Armature.move_IK_target allows to move at a given speed the target.
    
    The list of IK targets available on a given armature can be obtain
    with Armature.list_IK_targets.
    
    To easily create the IK targets, the morse.builder.Armature offers the
    create_IK_targets([joints]) that can be called in Builder scripts.
    
    Note that internally, all rotations are handled as quaternions to
    ease interpolation over time.
    
    While here:
    - removed the IK constraint in KUKA LWR Blend file
    - added (from Python) an IK target to the KUKA LWR. Cf
      morse.builder.actuators.KukaLWR
      to see how this is done.
    - updated testing/base/armature_testing

--- src/morse/core/blenderapi.py
+++ src/morse/core/blenderapi.py
@@ -55,6 +55,7 @@ RIGHTMOUSE = None
 F5KEY = None
 F8KEY = None
 
+CONSTRAINT_TYPE_KINEMATIC = None
 
 if not fake:
     UPARROWKEY = bge.events.UPARROWKEY
@@ -88,6 +89,8 @@ if not fake:
 
     F8KEY = bge.events.F8KEY
     F5KEY = bge.events.F5KEY
+
+    CONSTRAINT_TYPE_KINEMATIC = bge.logic.CONSTRAINT_TYPE_KINEMATIC
 
 def input_active():
     if not fake:
