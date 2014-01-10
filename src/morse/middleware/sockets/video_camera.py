3ee8b0bf92f8ed744ea8e84de503c97b98b758cd
Pierrick Koch <pierrick.koch@gmail.com>
2014-01-10 17:06:24 +0100

    [test] use base64 image for light and camera

--- src/morse/middleware/sockets/video_camera.py
+++ src/morse/middleware/sockets/video_camera.py
@@ -5,20 +5,23 @@ import logging; logger = logging.getLogger("morse." + __name__)
 from morse.middleware.socket_datastream import SocketPublisher
 
 class VideoCameraPublisher(SocketPublisher):
-    """ Publish a base64 encoded image """
+    """ Publish a base64 encoded RGBA image """
 
-    _type_name = 'base64 encoded image'
+    _type_name = 'base64 encoded RGBA image'
+
+    def process(self, image):
+        if sys.version_info < (3,4):
+            return bytes( image )
+        else:
+            # Python 3.4 base64.b64encode supports memoryview, which is faster
+            # TODO Blender 2.7 ? else: image = memoryview( image )
+            return image
 
     def encode(self):
         if not self.component_instance.capturing:
             return bytes() # press [Space] key to enable capturing
 
-        image = self.data['image']
-
-        if sys.version_info < (3,4):
-            image = bytes( image )
-        # Python 3.4 base64.b64encode supports memoryview, which is faster
-        # TODO Blender 2.7 ? else: image = memoryview( image )
+        image = self.process( self.data['image'] )
 
         data = base64.b64encode( image ).decode() # get string
         intrinsic = [ list(vec) for vec in self.data['intrinsic_matrix'] ]
@@ -33,30 +36,16 @@ class VideoCameraPublisher(SocketPublisher):
 
         return (json.dumps(res) + '\n').encode()
 
-#
-# deprecated publisher
-#
-class VideoPublisher(SocketPublisher):
+class Video8uPublisher(VideoCameraPublisher):
+    """ Publish a base64 encoded grayscale (8U) image """
 
-    #    _type_name = "a JSON-encoded image: the height, the width and a list of height * width points {'r':..., 'g':..., 'b':..., 'a':...}"
-    _type_name = "a JSON-encoded image"
+    _type_name = 'base64 encoded grayscale (8U) image'
 
-    def encode(self):
-        if not self.component_instance.capturing:
-            return bytes() # press [Space] key to enable capturing
-
-        res = {'height': self.component_instance.image_height,
-               'width': self.component_instance.image_width,
-               'image': []}
-
-        # bge.texture.ImageRender is an image buffer, which is
-        #   faster to convert than its raw `image` attribute.
-        image = memoryview(self.component_instance.local_data['image'])
-        for i in range(0, res['height'] * res['width'] * 4, 4):
-            o = {'r': image[i],
-                 'g': image[i + 1],
-                 'b': image[i + 2],
-                 'a': image[i + 3]}
-            res['image'].append(o)
-
-        return (json.dumps(res) + '\n').encode()
+    def process(self, image):
+        memv = memoryview( image )
+        # Grayscale model used for HDTV developed by the ATSC (Wikipedia)
+        i8u = [ int(0.2126 * memv[index] +
+                    0.7152 * memv[index + 1] +
+                    0.0722 * memv[index + 2] )
+                for index in range(0, len(memv), 4) ]
+        return bytearray( i8u )
