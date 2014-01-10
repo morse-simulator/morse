99e341c00ac8c37999d76589b81cc5ff7f3452f9
Pierrick Koch <pierrick.koch@gmail.com>
2014-01-10 15:41:38 +0100

    [socket] use base64 for VideoCamera
    based on Johannes Horvath proposal for the DepthCamera

--- src/morse/middleware/sockets/video_camera.py
+++ src/morse/middleware/sockets/video_camera.py
@@ -1,7 +1,41 @@
-import logging; logger = logging.getLogger("morse." + __name__)
+import sys
 import json
+import base64
+import logging; logger = logging.getLogger("morse." + __name__)
 from morse.middleware.socket_datastream import SocketPublisher
 
+class VideoCameraPublisher(SocketPublisher):
+    """ Publish a base64 encoded image """
+
+    _type_name = 'base64 encoded image'
+
+    def encode(self):
+        if not self.component_instance.capturing:
+            return bytes() # press [Space] key to enable capturing
+
+        image = self.data['image']
+
+        if sys.version_info < (3,4):
+            image = bytes( image )
+        # Python 3.4 base64.b64encode supports memoryview, which is faster
+        # TODO Blender 2.7 ? else: image = memoryview( image )
+
+        data = base64.b64encode( image ).decode() # get string
+        intrinsic = [ list(vec) for vec in self.data['intrinsic_matrix'] ]
+
+        res = {
+            'timestamp': self.data['timestamp'],
+            'height':    self.component_instance.image_height,
+            'width':     self.component_instance.image_width,
+            'image':     data,
+            'intrinsic_matrix': intrinsic,
+        }
+
+        return (json.dumps(res) + '\n').encode()
+
+#
+# deprecated publisher
+#
 class VideoPublisher(SocketPublisher):
 
     #    _type_name = "a JSON-encoded image: the height, the width and a list of height * width points {'r':..., 'g':..., 'b':..., 'a':...}"
