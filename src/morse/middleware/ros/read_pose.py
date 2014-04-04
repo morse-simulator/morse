a7ef34f0262212a5ed2edab949aae3ba5e8f304c
Felix Ruess <felix.ruess@dlr.de>
2014-04-04 17:42:26 +0200

    [ros] read_pose: add PoseStampedReader

--- src/morse/middleware/ros/read_pose.py
+++ src/morse/middleware/ros/read_pose.py
@@ -1,6 +1,6 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import roslib; roslib.load_manifest('geometry_msgs')
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 from morse.middleware.ros import ROSSubscriber, mathutils
 
 class PoseReader(ROSSubscriber):
@@ -15,6 +15,27 @@ class PoseReader(ROSSubscriber):
         self.data["z"] = message.position.z
 
         quaternion = mathutils.Quaternion((message.orientation.w, message.orientation.x, message.orientation.y, message.orientation.z))
+        euler = quaternion.to_euler()
+        self.data['roll'] = euler.x
+        self.data['pitch'] = euler.y
+        self.data['yaw'] = euler.z
+
+
+class PoseStampedReader(ROSSubscriber):
+    """ Subscribe to a PoseStamped topic and set ``x``, ``y``, ``z`` and ``roll``,
+    ``pitch``, ``yaw`` local data.
+    """
+    ros_class = PoseStamped
+
+    def update(self, message):
+        self.data["x"] = message.pose.position.x
+        self.data["y"] = message.pose.position.y
+        self.data["z"] = message.pose.position.z
+
+        quaternion = mathutils.Quaternion((message.pose.orientation.w,
+                                           message.pose.orientation.x,
+                                           message.pose.orientation.y,
+                                           message.pose.orientation.z))
         euler = quaternion.to_euler()
         self.data['roll'] = euler.x
         self.data['pitch'] = euler.y
