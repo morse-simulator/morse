8176fc12d9889a13db65a6f64e9f38df253733f4
Pierrick Koch <pierrick.koch@gmail.com>
2014-06-05 11:19:28 +0200

    [ci] use blender ppa 2.7 + python3.4
    increase xvfb window to avoid llvmpipe segfault
    xvfb auto-servernum avoid Xvfb failed to start
    
    http://manpages.ubuntu.com/manpages/precise/man1/xvfb-run.1.html

--- .travis.bash
+++ .travis.bash
@@ -1,33 +1,28 @@
 #!/bin/bash -e
-echo "MORSE installation and builder test"
-echo "========================================"
+echo "MORSE ci setup and test"
+echo "======================="
 lsb_release -a; uname -a
-[[ -z "$(uname -p | grep 64)" ]] && arch="i686" || arch="x86_64"
-BLENDER="blender-2.64a-linux-glibc27-$arch"
-
-echo "Download Blender ${BLENDER}"
-(wget -q http://download.blender.org/release/Blender2.64/${BLENDER}.tar.bz2
-tar jxf ${BLENDER}.tar.bz2 )& blenderpid=$!
 
 echo "Setup MORSE env"
-echo -e "#! /bin/sh\n$(pwd)/${BLENDER}/blender -setaudio NULL \$@" > blender
+# Avoid ALSA errors on system without sound card
+echo -e "#! /bin/sh\nblender -setaudio NULL \$@" > blender
 chmod +x blender
 export MORSE_BLENDER=$(pwd)/blender
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.2/site-packages
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3/dist-packages
 
 echo "Build and install MORSE"
 mkdir build && cd build
-cmake -DPYMORSE_SUPPORT=ON ..
+cmake -DPYTHON_EXECUTABLE=/usr/bin/python3.4 -DPYMORSE_SUPPORT=ON ..
 make
 sudo make install
-wait $blenderpid # Blender should be ready now
 
 morse_test() {
     echo "Run $1"
     echo "========================================"
     touch $2
     tail -f $2 &
-    xvfb-run --server-args="-screen 0 1x21x16" python3 ../testing/$1
+    xvfb-run --auto-servernum --server-args="-screen 0 160x120x16" python3.4 ../testing/$1
     kill % # kill tail
 }
 
