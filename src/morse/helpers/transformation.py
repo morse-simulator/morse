feff6c7bc08368c98188256bcfffba38a801e7f7
Arnaud Degroote <arnaud.degroote@isae.fr>
2014-06-17 10:12:30 +0200

    [core] Remove test against blender < 2.62
    We only support Blender 2.62+, so remove test against previous version.
    It should be particularly useful here, as transformation3D is probably
    an 'hot path'.

--- src/morse/helpers/transformation.py
+++ src/morse/helpers/transformation.py
@@ -44,30 +44,21 @@ class Transformation3d:
         """
         Return the translation along the x-axis
         """
-        if blenderapi.version() < (2, 62, 0):
-            return self.matrix[3][0]
-        else:
-            return self.matrix[0][3]
+        return self.matrix[0][3]
 
     @property
     def y(self):
         """
         Return the translation along the y-axis
         """
-        if blenderapi.version() < (2, 62, 0):
-            return self.matrix[3][1]
-        else:
-            return self.matrix[1][3]
+        return self.matrix[1][3]
 
     @property
     def z(self):
         """
         Return the translation along the z-axis
         """
-        if blenderapi.version() < (2, 62, 0):
-            return self.matrix[3][2]
-        else:
-            return self.matrix[2][3]
+        return self.matrix[2][3]
 
     @property
     def yaw(self):
@@ -167,10 +158,7 @@ class Transformation3d:
 
         pos = obj.worldPosition
         for i in range(0, 3):
-            if blenderapi.version() < (2, 62, 0):
-                self.matrix[3][i] = pos[i]
-            else:
-                self.matrix[i][3] = pos[i]
+            self.matrix[i][3] = pos[i]
         self.matrix[3][3] = 1
 
         self.euler = self.matrix.to_euler()
@@ -190,10 +178,7 @@ class Transformation3d:
 
         pos = obj.worldPosition
         for i in range(0, 3):
-            if blenderapi.version() < (2, 62, 0):
-                self.matrix[3][i] = pos[i]
-            else:
-                self.matrix[i][3] = pos[i]
+            self.matrix[i][3] = pos[i]
         self.matrix[3][3] = 1
 
         self.euler = self.matrix.to_euler()
