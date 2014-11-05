4c007b5ec21cb5485dbd808ea379c09457a66885
SimonRohou <simon.rohou@gmail.com>
2014-11-05 11:36:14 +0100

    [mw/moos] Teleport actuator now working

--- src/morse/middleware/moos/motion.py
+++ src/morse/middleware/moos/motion.py
@@ -10,6 +10,16 @@ class MotionReader(AbstractMOOS):
         # register for control variables from the database
         self.m.Register("cVelocity")
         self.m.Register("cYawRate")
+        self.m.Register("cSteer")
+        self.m.Register("cThrottle")
+        self.m.Register("cBrake")
+        # register for position variables from the database
+        self.m.Register("pX")
+        self.m.Register("pY")
+        self.m.Register("pZ")
+        self.m.Register("pRoll")
+        self.m.Register("pPitch")
+        self.m.Register("pYaw")
 
     def default(self, ci='unused'):
         current_time = pymoos.MOOSCommClient.MOOSTime()
@@ -18,8 +28,8 @@ class MotionReader(AbstractMOOS):
 
         new_information = False
 
-        # look for command messages: cYawRate and cVelocity
         for message in messages:
+            # look for command messages
             if (message.GetKey() =="cVelocity") and (message.IsDouble()):
                 self.data['v'] = message.GetDouble() # command linear velocity [m/s]
                 new_information = True
@@ -34,6 +44,25 @@ class MotionReader(AbstractMOOS):
                 new_information = True
             elif  (message.GetKey() =="cBrake") and (message.IsDouble()):
                 self.data['brake'] = message.GetDouble() # command angular velocity [m/s]
+                new_information = True
+            # look for position messages
+            elif  (message.GetKey() =="pX") and (message.IsDouble()):
+                self.data['x'] = message.GetDouble() # robot X position [m]
+                new_information = True
+            elif  (message.GetKey() =="pY") and (message.IsDouble()):
+                self.data['y'] = message.GetDouble() # robot Y position [m]
+                new_information = True
+            elif  (message.GetKey() =="pZ") and (message.IsDouble()):
+                self.data['z'] = message.GetDouble() # robot Z position [m]
+                new_information = True
+            elif  (message.GetKey() =="pRoll") and (message.IsDouble()):
+                self.data['roll'] = message.GetDouble() # robot roll [rad]
+                new_information = True
+            elif  (message.GetKey() =="pPitch") and (message.IsDouble()):
+                self.data['pitch'] = message.GetDouble() # robot pitch [rad]
+                new_information = True
+            elif  (message.GetKey() =="pYaw") and (message.IsDouble()):
+                self.data['yaw'] = message.GetDouble() # robot yaw [rad]
                 new_information = True
 
         return new_information
