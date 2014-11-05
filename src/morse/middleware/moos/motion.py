34bf691b0b223792a298e1cff0c3857d4b11bb78
SimonRohou <simon.rohou@gmail.com>
2014-11-05 18:34:43 +0100

    [mw/moos] Add PoseReader from MotionReader

--- src/morse/middleware/moos/motion.py
+++ src/morse/middleware/moos/motion.py
@@ -13,13 +13,6 @@ class MotionReader(AbstractMOOS):
         self.m.Register("cSteer")
         self.m.Register("cThrottle")
         self.m.Register("cBrake")
-        # register for position variables from the database
-        self.m.Register("pX")
-        self.m.Register("pY")
-        self.m.Register("pZ")
-        self.m.Register("pRoll")
-        self.m.Register("pPitch")
-        self.m.Register("pYaw")
 
     def default(self, ci='unused'):
         current_time = pymoos.MOOSCommClient.MOOSTime()
@@ -30,39 +23,20 @@ class MotionReader(AbstractMOOS):
 
         for message in messages:
             # look for command messages
-            if (message.GetKey() =="cVelocity") and (message.IsDouble()):
+            if (message.GetKey() == "cVelocity") and (message.IsDouble()):
                 self.data['v'] = message.GetDouble() # command linear velocity [m/s]
                 new_information = True
-            elif  (message.GetKey()=="cYawRate") and (message.IsDouble()):
+            elif  (message.GetKey() == "cYawRate") and (message.IsDouble()):
                 self.data['w'] = message.GetDouble() # command angular velocity [m/s]
                 new_information = True
-            elif  (message.GetKey() =="cSteer") and (message.IsDouble()):
+            elif  (message.GetKey() == "cSteer") and (message.IsDouble()):
                 self.data['steer'] = message.GetDouble() # command steer angle [deg]
                 new_information = True
-            elif  (message.GetKey( )=="cThrottle") and (message.IsDouble()):
+            elif  (message.GetKey() == "cThrottle") and (message.IsDouble()):
                 self.data['force'] = message.GetDouble() # command engine force
                 new_information = True
-            elif  (message.GetKey() =="cBrake") and (message.IsDouble()):
+            elif  (message.GetKey() == "cBrake") and (message.IsDouble()):
                 self.data['brake'] = message.GetDouble() # command angular velocity [m/s]
-                new_information = True
-            # look for position messages
-            elif  (message.GetKey() =="pX") and (message.IsDouble()):
-                self.data['x'] = message.GetDouble() # robot X position [m]
-                new_information = True
-            elif  (message.GetKey() =="pY") and (message.IsDouble()):
-                self.data['y'] = message.GetDouble() # robot Y position [m]
-                new_information = True
-            elif  (message.GetKey() =="pZ") and (message.IsDouble()):
-                self.data['z'] = message.GetDouble() # robot Z position [m]
-                new_information = True
-            elif  (message.GetKey() =="pRoll") and (message.IsDouble()):
-                self.data['roll'] = message.GetDouble() # robot roll [rad]
-                new_information = True
-            elif  (message.GetKey() =="pPitch") and (message.IsDouble()):
-                self.data['pitch'] = message.GetDouble() # robot pitch [rad]
-                new_information = True
-            elif  (message.GetKey() =="pYaw") and (message.IsDouble()):
-                self.data['yaw'] = message.GetDouble() # robot yaw [rad]
                 new_information = True
 
         return new_information
--- src/morse/middleware/moos/pose.py
+++ src/morse/middleware/moos/pose.py
@@ -19,3 +19,46 @@ class PoseNotifier(AbstractMOOS):
         self.m.Notify('simYaw', self.data['yaw'], cur_time)
         self.m.Notify('simRoll', self.data['roll'], cur_time)
         self.m.Notify('simPitch', self.data['pitch'], cur_time)
+
+class PoseReader(AbstractMOOS):
+    """ Read pose commands and update local data. """
+
+    def initialize(self):
+        AbstractMOOS.initialize(self)
+        # register for position variables from the database
+        self.m.Register("pX")
+        self.m.Register("pY")
+        self.m.Register("pZ")
+        self.m.Register("pRoll")
+        self.m.Register("pPitch")
+        self.m.Register("pYaw")
+
+    def default(self, ci='unused'):
+        current_time = pymoos.MOOSCommClient.MOOSTime()
+        # get latest mail from the MOOS comm client
+        messages = self.m.FetchRecentMail()
+
+        new_information = False
+
+        for message in messages:
+            # look for position messages
+            if  (message.GetKey() == "pX") and (message.IsDouble()):
+                self.data['x'] = message.GetDouble() # robot X position [m]
+                new_information = True
+            elif  (message.GetKey() == "pY") and (message.IsDouble()):
+                self.data['y'] = message.GetDouble() # robot Y position [m]
+                new_information = True
+            elif  (message.GetKey() == "pZ") and (message.IsDouble()):
+                self.data['z'] = message.GetDouble() # robot Z position [m]
+                new_information = True
+            elif  (message.GetKey() == "pRoll") and (message.IsDouble()):
+                self.data['roll'] = message.GetDouble() # robot roll [rad]
+                new_information = True
+            elif  (message.GetKey() == "pPitch") and (message.IsDouble()):
+                self.data['pitch'] = message.GetDouble() # robot pitch [rad]
+                new_information = True
+            elif  (message.GetKey() == "pYaw") and (message.IsDouble()):
+                self.data['yaw'] = message.GetDouble() # robot yaw [rad]
+                new_information = True
+
+        return new_information
