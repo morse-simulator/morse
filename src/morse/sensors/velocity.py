efc8051642a8f4ca6d98db757eba301c1a4463d2
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-03-12 13:45:22 +0100

    [sensor] Add a way to compute velocity for robot without physics

--- src/morse/sensors/velocity.py
+++ src/morse/sensors/velocity.py
@@ -2,6 +2,7 @@ import logging; logger = logging.getLogger("morse." + __name__)
 
 import morse.core.sensor
 from morse.helpers.components import add_data
+from morse.core.mathutils import * 
 from math import degrees
 
 class Velocity(morse.core.sensor.Sensor):
@@ -34,10 +35,12 @@ class Velocity(morse.core.sensor.Sensor):
         # Call the constructor of the parent class
         morse.core.sensor.Sensor.__init__(self, obj, parent)
 
-        # The robot needs a physics controller!
-        # Since the sensor does not have physics
-        if not bool(self.robot_parent.bge_object.getPhysicsId()):
-            logger.error("The robot doesn't have a physics controller!")
+        self.pp = Vector((0.0, 0.0, 0.0)) # previous position
+        self.pq = Quaternion((1.0, 0.0, 0.0, 0.0)) # previous quaternion
+        self.pt = 0.0 # previous timestamp
+        self.dt = 0.0 # diff
+
+        self.has_physics = bool(self.robot_parent.bge_object.getPhysicsId())
 
         # make new references to the robot velocities and use those.
         self.robot_w = self.robot_parent.bge_object.localAngularVelocity
@@ -52,10 +55,30 @@ class Velocity(morse.core.sensor.Sensor):
         logger.info("Component initialized, runs at %.2f Hz", self.frequency)
 
 
+    def _sim_simple(self):
+        self.dt = self.robot_parent.gettime() - self.pt
+        self.pt = self.robot_parent.gettime()
+
+        v = (self.position_3d.translation - self.pp) / self.dt
+        dq = self.pq.rotation_difference(self.position_3d.rotation)
+        w = Vector(dq.to_euler('ZYX')) / self.dt
+
+        self.pp = self.position_3d.translation
+        self.pq = self.position_3d.rotation
+
+        w2a = self.position_3d.rotation_matrix.transposed()
+
+        self.local_data['linear_velocity'] = w2a * v
+        self.local_data['angular_velocity'] =  w
+        self.local_data['world_linear_velocity'] = v
+
     def default_action(self):
         """ Get the linear and angular velocity of the blender object. """
 
-        # Store the important data
-        self.local_data['linear_velocity'] = self.rot_b2s * self.robot_v
-        self.local_data['angular_velocity'] = self.rot_b2s * self.robot_w
-        self.local_data['world_linear_velocity'] = self.robot_world_v.copy()
+        if self.has_physics:
+            # Store the important data
+            self.local_data['linear_velocity'] = self.rot_b2s * self.robot_v
+            self.local_data['angular_velocity'] = self.rot_b2s * self.robot_w
+            self.local_data['world_linear_velocity'] = self.robot_world_v.copy()
+        else:
+            self._sim_simple()
