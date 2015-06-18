morse-simulator/morse

commit 6b8726391c1a66227b3740df77439ef46322c088

[doc] Added a gallery of available environments.
While here, create the Sphinx directive 'widegallery' for such large images

doc/exts/gallery.py
@@ -16,10 +16,19 @@ def css(d):
     return "; ".join(sorted("%s: %s" % kv for kv in d.items()))
 
 class gallery(nodes.General, nodes.Element): pass
+class widegallery(nodes.General, nodes.Element): pass
 
-def visit_gallery_node(self, node):
+def visit_widegallery_node(self, node):
+    visit_gallery_node(self, node, wide = True)
 
-    images_per_row = 4
+def visit_gallery_node(self, node, wide = False):
+
+    if wide:
+        images_per_row = 2
+        width = "400px"
+    else:
+        images_per_row = 4
+        width = "200px"
 
     style = {
         "position": "relative",
@@ -31,7 +40,7 @@ def visit_gallery_node(self, node):
         for j in range(images_per_row):
             if i + j < len(images):
                 obj = images[i + j][:-4] #remove extension
-                self.body.append("\t<td style='text-align:center'><a href='" + HTML_PATH + node["directory"] + "/" + obj + ".html'><img style='width: 200px;' src='" + IMAGE_PATH + obj + ".png' /><br/><pre>" + obj + "</pre></a></td>\n")
+                self.body.append("\t<td style='text-align:center'><a href='" + HTML_PATH + node["directory"] + "/" + obj + ".html'><img style='width: " + width + ";' src='" + IMAGE_PATH + images[i+j] + "' /><br/><pre>" + obj + "</pre></a></td>\n")
         self.body.append("</tr>\n")
     self.body.append("</table>\n")
 
@@ -47,6 +56,18 @@ class Gallery(Directive):
     def run(self):
         return [gallery(directory=self.arguments[0])]
 
+class WideGallery(Directive):
+    has_content = True
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+
+    def run(self):
+        return [widegallery(directory=self.arguments[0])]
+
 def setup(app):
+    app.add_node(widegallery, html=(visit_widegallery_node, depart_gallery_node))
+    app.add_directive("widegallery", WideGallery)
+    
     app.add_node(gallery, html=(visit_gallery_node, depart_gallery_node))
     app.add_directive("gallery", Gallery)

commit b98cf57e57c4232d45598f6f5d9aa893e7f52893

[doc] Reworked documentation of environments
Commit 6b8726391c1a662 introduced a gallery of available 3D
environment, but it was broken (issue #631) because since the
gallery was auto-generated by a sphinx extension, the URL of images
never appeared, and sphinx wouldn't copy the required images.

This commit actually generate (in generate_doc) one doc page per available
environment (in, all *.blend in data/environments) with a picture.

Hence, the pictures are listed, copied, and displayed in the gallery.

While here, moved 2 images of the sandbox and outdoors environment to
the correct location, and added a link the the gallery of environments
in the component library.

Fix #631

CMakeLists.txt
@@ -133,7 +133,7 @@ ELSE()
 	SET (SPHINX_MAN_OUTPUT ${CMAKE_DOC_OUT}/man)
 	SET (SPHINX_FALSE_PYTHON_ENV ${CMAKE_CURRENT_BINARY_DIR}/fakeenv)
 
-	SET (PYTHON_DOC_ENV env PYTHONPATH=${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/src:${SPHINX_FALSE_PYTHON_ENV}:${CMAKE_CURRENT_SOURCE_DIR}/src:${CMAKE_CURRENT_SOURCE_DIR}/testing:${CMAKE_CURRENT_SOURCE_DIR}/bindings/pymorse/src/:$$PYTHONPATH PYTHONDONTWRITEBYTECODE="morse" ${PYTHON_EXECUTABLE})
+ 	SET (PYTHON_DOC_ENV env PYTHONPATH=${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/src:${SPHINX_FALSE_PYTHON_ENV}:${CMAKE_CURRENT_SOURCE_DIR}/src:${CMAKE_CURRENT_SOURCE_DIR}/testing:${CMAKE_CURRENT_SOURCE_DIR}/bindings/pymorse/src/:$$PYTHONPATH PYTHONDONTWRITEBYTECODE="morse" MORSESOURCE=${CMAKE_CURRENT_SOURCE_DIR} ${PYTHON_EXECUTABLE})
 	SET (SPHINX_BUILD_ENV ${PYTHON_DOC_ENV} ${SPHINX_BUILD})
 
 	# Copy the documentation in builddir, so we can include the generated part too

doc/exts/gallery.py
@@ -37,7 +37,7 @@ def visit_gallery_node(self, node, wide = False):
     self.body.append(self.starttag(node, "table", style=css(style)))
     img_path = os.path.join(MEDIA_PATH, node["directory"])
     images =  os.listdir(img_path)
-    images = [img for img in images if os.path.isfile(os.path.join(img_path, img))]
+    images = sorted([img for img in images if os.path.isfile(os.path.join(img_path, img))])
     for i in range(len(images))[::images_per_row]:
         self.body.append("<tr>\n")
         for j in range(images_per_row):

doc/tools/generate_doc
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 """
-This tools import various components of MORSE (currently, the sensors and actuators)
-and generates a set of documentation in RST format based on the Python source.
+This tools import various components of MORSE and generates a set of
+documentation in RST format based on the Python source.
 
 It generates doc for:
     - the component itself, based on the component class docstring,
@@ -10,6 +10,9 @@ It generates doc for:
     - the services exported by the conmponent
     - the abstraction levels exposed by the component
     - the interfaces and serialization types for each input/output
+
+The tool also generates simple doc page for each environments available with
+MORSE
 """
 
 import os
@@ -63,6 +66,7 @@ modules = [
 import sys, os, codecs
 import fnmatch
 from copy import copy
+import glob
 
 from morse.core.actuator import Actuator
 from morse.core.robot import Robot
@@ -76,6 +80,9 @@ sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 PREFIX = "."
 MEDIA_PATH = "../../media"
+print("MORSE source is located at " + os.environ['MORSESOURCE'])
+DATA_PATH = os.path.join(os.environ['MORSESOURCE'], "data")
+
 
 # docstring role that, if present, prevent automatic generation of a code sample
 NOAUTOEXAMPLE = ":noautoexample:"
@@ -93,7 +100,7 @@ def insert_code(code):
 def insert_image(name):
     matches = []
     for root, dirnames, filenames in os.walk(MEDIA_PATH):
-      for filename in fnmatch.filter(filenames, '%s.png' % name):
+      for filename in fnmatch.filter(filenames, '%s.*' % name):
             matches.append(os.path.join(root, filename))
 
     if matches:
@@ -590,7 +597,7 @@ def print_services(out, name, props):
 if not os.path.exists(PREFIX):
     os.makedirs(PREFIX)
 
-for directory in ['actuators', 'sensors', 'modifiers', 'robots']:
+for directory in ['actuators', 'sensors', 'modifiers', 'robots', 'environments']:
     path = os.path.join(PREFIX, directory)
     if not os.path.exists(path):
         os.makedirs(path)
@@ -628,6 +635,29 @@ for name, props in components.items():
             print_files(out, name, props)
 
         out.write('\n\n*(This page has been auto-generated from MORSE module %s.)*\n' % (props['object'].__module__) )
+
+environments = glob.glob(os.path.join(DATA_PATH + "/environments/*.blend"))
+environments += glob.glob(os.path.join(DATA_PATH + "/environments/**/*.blend"))
+environments = [env.split(DATA_PATH + "/environments/")[1].split(".blend")[0] for env in environments]
+
+print("Found environments: %s" % environments)
+
+for env in environments:
+    path = env
+    name = env.split("/")[1] if "/" in env else env
+
+    with open(os.path.join(PREFIX, 'environments',  name + ".rst"), 'w',
+              encoding='utf-8') as out:
+        out.write(underline("<%s> environment" % name) + '\n')
+
+        # if an image if available, use it
+        out.write(insert_image(name))
+
+        out.write(underline("To use it", "-") + "\n")
+        out.write(".. code-block:: python\n\n  from morse.builder import *\n\n")
+        out.write("  # your robots...\n\n  env = Environment('%s')\n" % path)
+        out.write('\n\n*(This page has been auto-generated from MORSE available environments.)*\n')
+
 
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""