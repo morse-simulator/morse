1f443d352474674fffed7008096735d725b5dfeb
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-10-07 16:40:18 +0200

    [cmake] Adjust fakeenv for mavlink

--- config/generate_false_env.cmake
+++ config/generate_false_env.cmake
@@ -2,3 +2,5 @@
 
 FILE(WRITE ${SPHINX_FALSE_PYTHON_ENV}/hla/omt.py "HLAfloat32LE=0\ndef HLAfixedArray(x,y,z):\n    pass")
 FILE(WRITE ${SPHINX_FALSE_PYTHON_ENV}/hla/rti.py "class FederateAmbassador:\n    pass")
+FILE(WRITE ${SPHINX_FALSE_PYTHON_ENV}/pymavlink/mavutil.py "def mavlink_connection():\n    pass")
+FILE(WRITE ${SPHINX_FALSE_PYTHON_ENV}/pymavlink/quaternion.py "class QuaternionBase:\n    pass")
