da30065a5428586c00b880523571381b05d1245b
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2015-10-09 17:32:01 +0100

    [human] renamed armature to 'skeleton', added missing service configuration

--- src/morse/builder/robots/human.py
+++ src/morse/builder/robots/human.py
@@ -7,9 +7,6 @@ from morse.builder.sensors import ArmaturePose
 class Human(GroundRobot):
     """ Append a human model to the scene.
 
-    It also exposes a :doc:`human posture component <../sensors/human_posture>`
-    that can be accessed by the ``armature`` member.
-
     Usage example:
 
     .. code-block:: python
@@ -22,16 +19,17 @@ class Human(GroundRobot):
        human.translate(x=5.5, y=-3.2, z=0.0)
        human.rotate(z=-3.0)
 
-       human.armature.add_stream('pocolibs')
+       human.skeleton.add_stream('pocolibs')
 
     Currently, only one human per simulation is supported.
 
     Detailed documentation
     ----------------------
 
-    The MORSE human avatar is based on a 3D model generated from MakeHuman, and stored in human_rig.blend.
+    The MORSE human avatar is based on a 3D model generated from MakeHuman, and
+    stored in human_rig.blend.
 
-    The model is rigged with an armature ('HumanSkeleton'), used to control the
+    The model is rigged with an armature (`human.skeleton`), used to control the
     postures of the human and to deform accordingly the human mesh.
 
     This armature is used by MORSE as both a :doc:`sensor to read and export
@@ -57,41 +55,42 @@ class Human(GroundRobot):
         GroundRobot.__init__(self, filename, name)
         self.properties(classpath = "morse.robots.human.Human")
 
-        self.armature = None
+        self.skeleton = None
 
         try:
-            self.armature = Armature(armature_name = "HumanSkeleton")
-            self.append(self.armature)
+            self.skeleton = Armature(armature_name = "HumanSkeleton")
+            self.append(self.skeleton)
         except KeyError:
             logger.error("Could not find the human armature! (I was looking " +\
                          "for an object called 'HumanSkeleton' in the children " + \
                          "objects of the human). I won't be able to export the " + \
                          "human pose to any middleware.")
 
-        if self.armature:
-            self.armature.create_ik_targets(self.IK_TARGETS)
-            for t, name in self.armature.ik_targets:
+        if self.skeleton:
+            self.skeleton.create_ik_targets(self.IK_TARGETS)
+            for t, name in self.skeleton.ik_targets:
                 t.parent = self._bpy_object
 
             # Add an armature sensor. "joint_stateS" to match standard ROS spelling.
             self.joint_states = ArmaturePose("joint_states")
-            self.armature.append(self.joint_states)
+            self.skeleton.append(self.joint_states)
 
 
     def add_interface(self, interface):
         if interface == "socket":
             self.joint_states.add_stream("socket")
-            self.armature.add_service('socket')
+            self.joint_states.add_service("socket")
+            self.skeleton.add_service('socket')
 
         elif interface == "ros":
 
             self.joint_states.add_stream("ros")
 
-            self.armature.add_service("ros")
-            self.armature.add_overlay("ros",
+            self.skeleton.add_service("ros")
+            self.skeleton.add_overlay("ros",
               "morse.middleware.ros.overlays.armatures.ArmatureController")
 
         elif interface == "pocolibs":
-            self.armature.properties(classpath="morse.sensors.human_posture.HumanPosture")
+            self.skeleton.properties(classpath="morse.sensors.human_posture.HumanPosture")
             self.add_stream(interface)
 
