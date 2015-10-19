2d457ebf04dc6889fb8926b640140465d340715c
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-10-19 10:38:03 +0200

    [helpers] Cleanup morse_math
    Most methods are not used, and not very useful nowadays. So just remove
    them

--- src/morse/helpers/morse_math.py
+++ src/morse/helpers/morse_math.py
@@ -1,40 +1,6 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import math
 
-# euler_angle(object) returns a tuple yaw, pitch, roll, in degree,
-# corresponding to the transformation between the origin frame
-# and the object frame.
-#
-# It uses the built-in blender function to_euler
-def euler_angle(ob):
-    rot_matrix = ob.worldOrientation
-
-    # XXX If we want to have the right handle, we definivly need to transpose
-    # the matrix here, but why ?
-    rot_matrix.transpose()
-    euler = rot_matrix.to_euler()
-
-    return [euler.z, euler.x, euler.y]
-
-
-def get_rotation_matrix(object):
-    """ Return a the rotation matrix of an object.
-    Used to transform another object to this one's coordinate system. """
-
-    import copy
-    return copy.copy(object.worldOrientation)
-
-def invert_rotation_matrix(object):
-    """ Return the inverse of the rotation matrix of an object.
-
-    Used to get the position of another object with respect to
-    this one. """
-    rotation_matrix = get_rotation_matrix(object)
-
-    # Return a new inverted matrix (requires Blender 2.58)
-    return rotation_matrix.inverted()
-
-
 def normalise_angle(angle):
     """ Force the given angle to be between PI and -PI
     
@@ -63,27 +29,3 @@ def rotation_direction(current_angle, target_angle, tolerance, speed):
         rotation = 0
 
     return rotation
-
-
-def print_matrix_33(matrix):
-    """ print_matrix_33 prints a 3x3 @matrix on stdout """
-    for row in matrix:
-        line = "[% .4f % .4f % .4f]" % (row[0], row[1], row[2])
-        logger.info(line)
-
-def print_matrix_44(matrix):
-    """ print_matrix_44 prints a 4x4 @matrix on stdout """
-    for row in matrix:
-        line = "[% .4f % .4f % .4f % .4f]" % (row[0], row[1], row[2], row[3])
-        logger.info(line)
-
-def print_vector (vector):
-    """ print a vector with 4 decimals per value """
-    line = "[% .4f % .4f % .4f]" % (vector[0], vector[1], vector[2])
-    logger.info(line)
-
-
-def fill_vector(vector, point):
-    """ Copy the contents of a list into an existing vector structure. """
-    for i in range(3):
-        vector[i] = point[i]
