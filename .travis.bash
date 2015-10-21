34731b536819e1bbd8baba46638b67aba0fdcd2a
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-10-21 10:16:05 +0200

    [travis] Upgrade the architecture for trusty
    Should fix #668

--- .travis.bash
+++ .travis.bash
@@ -9,28 +9,17 @@ echo -e "#! /bin/sh\nblender -setaudio NULL \$@" > blender
 chmod +x blender
 export MORSE_BLENDER=$(pwd)/blender
 
-workspace=$(pwd)
-echo "Install virtualenv and numpy for python3.4"
-version=13.1.0
-wget https://github.com/pypa/virtualenv/archive/${version}.tar.gz
-tar xf ${version}.tar.gz
-cd virtualenv-${version}
-export PATH=${PATH}:${HOME}/.local/bin
-python3.4 setup.py install --user # install in ~/.local
+MORSE_PREFIX=~/morse_install
 
-virtualenv pyvenv
-source pyvenv/bin/activate
-pip install numpy
-cd ${workspace}
-
-export PYTHONPATH=${workspace}/pyvenv/lib/python3.4/site-packages:$PYTHONPATH
+mkdir -p ${MORSE_PREFIX}
 
 echo "Build and install MORSE"
 mkdir build && cd build
-cmake -DPYTHON_EXECUTABLE=$(which python3.4) -DPYMORSE_SUPPORT=ON -DCMAKE_INSTALL_PREFIX=${workspace}/pyvenv ..
+cmake -DPYTHON_EXECUTABLE=$(which python3.4) -DCMAKE_INSTALL_PREFIX=${MORSE_PREFIX} ..
 make install
 
-export PATH=${PATH}:${workspace}/pyvenv/bin
+export PATH=${PATH}:${MORSE_PREFIX}/bin
+export PYTHONPATH=${MORSE_PREFIX}/lib/python3/dist-packages:$PYTHONPATH
 
 morse_test() {
     echo "Run $1"
