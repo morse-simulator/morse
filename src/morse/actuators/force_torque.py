5746a8f3d5b85f0e904a4c93f11f1d8df6705391
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-11-03 11:54:05 +0100

    [actuators] Extend force_torque interface to allow force injection in global frame
    It would be useful to simulate wind for example.

--- src/morse/actuators/force_torque.py
+++ src/morse/actuators/force_torque.py
@@ -5,12 +5,13 @@ from morse.helpers.components import add_data, add_property
 
 class ForceTorque(morse.core.actuator.Actuator):
     """
-    This class will read force and torque as input 
-    from an external middleware.
-    The forces and torques are transformed from the actuator frame to the
-    parent robot frame and then applied to the robot blender object.
-    If the property RobotFrame is set to True it will be applied
-    directly in the robot frame without changes.
+    This class will read force and torque as input  from an external middleware.
+
+    If Frame is ACTUATOR, the forces and torques are transformed from
+    the actuator frame to the parent robot frame and then applied to the
+    robot blender object.
+    If Frame is ROBOT or GLOBAL, the force and torque are directly
+    applied in the specified frame.
     """
 
     _name = "Force/Torque Motion Controller"
@@ -20,10 +21,9 @@ class ForceTorque(morse.core.actuator.Actuator):
     add_data('force', [0.0, 0.0, 0.0], "vec3<float>", "force along x, y, z")
     add_data('torque', [0.0, 0.0, 0.0], "vec3<float>", "torque around x, y, z")
 
-    add_property('_robot_frame', False, 'RobotFrame', 'bool', 'If set to true '
-            'the inputs are applied in the Robot coordinate frame instead of the '
-            'actuator frame.')
-
+    add_property('_frame', 'ACTUATOR', 'Frame', 'string', 'A string in '
+                 '["ACTUATOR", "ROBOT", "GLOBAL"] which represents in which '
+                 'frame the force are applied')
 
     def __init__(self, obj, parent=None):
         logger.info('%s initialization' % obj.name)
@@ -38,11 +38,11 @@ class ForceTorque(morse.core.actuator.Actuator):
         # Get the the parent robot
         robot = self.robot_parent
 
-        if self._robot_frame:
-            # directly apply local forces and torques to the blender object of the parent robot
-            robot.bge_object.applyForce(self.local_data['force'], True)
-            robot.bge_object.applyTorque(self.local_data['torque'], True)
-        else:
+        if not self._frame in ['ACTUATOR', 'ROBOT', 'GLOBAL']:
+            logger.warning("Invalid frame %s " % self._frame)
+            return
+
+        if self._frame == 'ACTUATOR':
             (loc, rot, scale) = robot.position_3d.transformation3d_with(self.position_3d).matrix.decompose()
             # rotate into robot frame, but still at actuator origin
             force = rot * mathutils.Vector(self.local_data['force'])
@@ -51,3 +51,7 @@ class ForceTorque(morse.core.actuator.Actuator):
             torque += loc.cross(force)
             robot.bge_object.applyForce(force, True)
             robot.bge_object.applyTorque(torque, True)
+        else:
+            # directly apply forces and torques to the blender object of the parent robot
+            robot.bge_object.applyForce(self.local_data['force'], self._frame == 'ROBOT')
+            robot.bge_object.applyTorque(self.local_data['torque'], self._frame == 'ROBOT')
