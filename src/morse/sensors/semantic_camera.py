morse-simulator/morse

commit a463f24fedc25257823530bb8af55726515dd5b1

[sensor] Semantic Camera 'tag' and 'relative' properties
- 'tag' allows to select the kind of object visible
by the semantic camera
- 'relative' returns the visible objects position/orientation
in the camera frame

Fix #496

src/morse/sensors/semantic_camera.py
@@ -6,18 +6,19 @@ import morse.helpers.colors
 
 from morse.helpers import passive_objects
 from morse.helpers.components import add_data, add_property
+from morse.helpers.transformation import Transformation3d
 
 class SemanticCamera(morse.sensors.camera.Camera):
     """
     This sensor emulates a hight level camera that outputs the names of
     the objects that are located within the field of view of the camera.
 
-    The sensor determines first which objects are to be tracked (objects
-    marked with a **Logic Property** called ``Object``, cf documentation
-    on :doc:`passive objects <../others/passive_objects>` for more on
-    that). If the ``Label`` property is defined, it is used as exported
-    name. Else the Blender object name is used. If the ``Type`` property
-    is set, it is exported as well.
+    The sensor determines first which objects are to be tracked. The
+    tracked objects are either marked with a **Logic Property** being
+    the same as the ``tag`` property, of if their ``Type`` has the ``tag`` value.
+    
+    If the ``Label`` property is defined, it is used as exported
+    name. Else the Blender object name is used.
 
     Then a test is made to identify which of these objects are inside of
     the view frustum of the camera. Finally, a single visibility test is
@@ -47,9 +48,15 @@ class SemanticCamera(morse.sensors.camera.Camera):
                 - **orientation** (quaternion): the orientation of the \
                   object, in the blender frame")
 
+    add_property('relative', False, 'relative', 'bool', 'Return object position'
+                 ' relatively to the sensor frame.')
     add_property('noocclusion', False, 'noocclusion', 'bool', 'Do not check for'
                  ' objects possibly hiding each others (faster but less '
                  'realistic behaviour)')
+    add_property('tag', 'Object',  'tag',  "string",  "The type of "
+            "detected objects. This type is looked for as a game property of scene "
+            "objects or as their 'Type' property. You must then add fix this property to the objects you "
+            "want to be detected by the semantic camera.")
 
     def __init__(self, obj, parent=None):
         """ Constructor method.
@@ -76,22 +83,13 @@ class SemanticCamera(morse.sensors.camera.Camera):
         # TrackedObject is a dictionary containing the list of tracked objects
         # (->meshes with a class property set up) as keys
         #  and the bounding boxes of these objects as value.
-        if not 'trackedObjects' in blenderapi.persistantstorage():
-            logger.info('Initialization of tracked objects:')
-            blenderapi.persistantstorage().trackedObjects = \
-                            dict.fromkeys(passive_objects.active_objects())
-
-            # Store the bounding box of the marked objects
-            for obj in blenderapi.persistantstorage().trackedObjects.keys():
-
-                # bound_box returns the bounding box in local space
-                #  instead of world space.
-                blenderapi.persistantstorage().trackedObjects[obj] = \
-                                    blenderapi.objectdata(obj.name).bound_box
-
-                details = passive_objects.details(obj)
-                logger.info('    - {%s} (type:%s)'%
-                            (details['label'], details['type']))
+        self.trackedObjects = {}
+        for o in blenderapi.scene().objects:
+            tagged = ('Type' in o and o['Type'] == self.tag) or (self.tag in o and bool(o[self.tag]))
+                               
+            if tagged:
+                self.trackedObjects[o] = blenderapi.objectdata(o.name).bound_box
+                logger.warning('    - %s' % o.name)
 
         if self.noocclusion:
             logger.info("Semantic camera running in 'no occlusion' mode (fast mode).")
@@ -111,21 +109,29 @@ class SemanticCamera(morse.sensors.camera.Camera):
 
         # Create dictionaries
         self.local_data['visible_objects'] = []
-        for obj in blenderapi.persistantstorage().trackedObjects.keys():
-            if self._check_visible(obj):
+        for obj, bb in self.trackedObjects.items():
+            if self._check_visible(obj, bb):
                 # Create dictionary to contain object name, type,
                 # description, position and orientation
-                obj_dict = {'name': passive_objects.label(obj),
+                if self.relative:
+                    t3d = Transformation3d(obj)
+                    logger.debug("t3d(obj) = {t}".format(t=t3d))
+                    logger.debug("t3d(cam) = {t}".format(t=self.position_3d))
+                    transformation = self.position_3d.transformation3d_with(t3d)
+                    logger.debug("transform = {t}".format(t=transformation))
+                else:
+                    transformation = Transformation3d(obj)
+                obj_dict = {'name': obj.name,
                             'description': obj.get('Description', ''),
                             'type': obj.get('Type', ''),
-                            'position': obj.worldPosition,
-                            'orientation': obj.worldOrientation.to_quaternion()}
+                            'position': transformation.translation,
+                            'orientation': transformation.rotation}
                 self.local_data['visible_objects'].append(obj_dict)
-
+                
         logger.debug("Visible objects: %s" % self.local_data['visible_objects'])
 
 
-    def _check_visible(self, obj):
+    def _check_visible(self, obj, bb):
         """ Check if an object lies inside of the camera frustum. 
         
         The behaviour of this method is impacted by the sensor's 
@@ -135,7 +141,6 @@ class SemanticCamera(morse.sensors.camera.Camera):
         """
         # TrackedObjects was filled at initialization
         #  with the object's bounding boxes
-        bb = blenderapi.persistantstorage().trackedObjects[obj]
         pos = obj.position
         bbox = [[bb_corner[i] + pos[i] for i in range(3)] for bb_corner in bb]
 

testing/base/CMakeLists.txt
@@ -18,6 +18,8 @@ add_morse_test(pose_testing)
 add_morse_test(proximity_testing)
 add_morse_test(sick_testing)
 add_morse_test(semantic_camera_testing)
+add_morse_test(semantic_camera_tag_testing)
+add_morse_test(semantic_camera_relative_testing)
 add_morse_test(thermometer_testing)
 add_morse_test(search_and_rescue_testing)
 add_morse_test(velocity_testing)

testing/base/semantic_camera_relative_testing.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 """
-This script tests the Semantic Camera sensor in MORSE.
+This script tests the Semantic Camera sensor and its 'relative' property in MORSE.
 """
 
 import sys
@@ -25,9 +25,12 @@ class Semantic_Camera_Test(MorseTestCase):
         """ Defines the test scenario, using the Builder API.
         """
         robot = ATRV()
+        
         camera = SemanticCamera()
+        camera.properties(relative=True)
         robot.append(camera)
         camera.translate(x=0.3, z=0.762)
+        #camera.rotate(x=math.pi/2.0)
         camera.add_stream('socket')
 
         motion = Teleport()
@@ -51,24 +54,21 @@ class Semantic_Camera_Test(MorseTestCase):
             self.assertEqual(objects, [])
 
             # Change the orientation of the robot using the v_w socket
-            send_dest(teleport_client, morse, 0.0, 0.0, 5.0/4.0 * math.pi)
+            send_dest(teleport_client, morse, -5.0, 0.0, math.pi)
 
             # Second test for the sensor, with objects in front
             o = semantic_stream.get()
             objects= o['visible_objects']
             self.assertEqual(len(objects), 1)
-            self.assertEqual(objects[0]['name'],'BlueBox')
-            self.assertAlmostEqual(objects[0]['position'][0], -3.48, delta=0.1)
-            self.assertAlmostEqual(objects[0]['position'][1], -3.0, delta=0.1)
-
-            send_dest(teleport_client, morse, -5.0, 0.0, math.pi)
-
-            o = semantic_stream.get()
-            objects= o['visible_objects']
-            self.assertEqual(len(objects), 1)
             self.assertEqual(objects[0]['name'],'RedBox')
-            self.assertAlmostEqual(objects[0]['position'][0], -7.48, delta=0.1)
-            self.assertAlmostEqual(objects[0]['position'][1], 0.0, delta=0.1)
+            # RedBox in camera frame:
+            position = [0, -0.2, 2.2]
+            quaternion = {'x':0.5, 'y':0.5, 'z':-0.5, 'w':0.5}
+            # quaternion is equal to euler (pi, pi, 0) in XYZ mode
+            for i in [0,1,2]:
+                self.assertAlmostEqual(objects[0]['position'][i], position[i], delta=0.1)
+            for i in ['x', 'y', 'z', 'w']:
+                self.assertAlmostEqual(objects[0]['orientation'][i], quaternion[i], delta=.1)
 
 ########################## Run these tests ##########################
 if __name__ == "__main__":

testing/base/semantic_camera_tag_testing.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 """
-This script tests the Semantic Camera sensor in MORSE.
+This script tests the Semantic Camera sensor and its 'tag' property in MORSE.
 """
 
 import sys
@@ -26,6 +26,7 @@ class Semantic_Camera_Test(MorseTestCase):
         """
         robot = ATRV()
         camera = SemanticCamera()
+        camera.properties(tag="Box")
         robot.append(camera)
         camera.translate(x=0.3, z=0.762)
         camera.add_stream('socket')
@@ -36,7 +37,8 @@ class Semantic_Camera_Test(MorseTestCase):
 
         env = Environment('indoors-1/boxes')
         env.add_service('socket')
-
+        box = bpymorse.get_object('RedBox')
+        bpymorse.properties(box, Type="Box")
 
     def test_semantic_camera(self):
         """ This test is guaranteed to be started only when the simulator
@@ -53,16 +55,14 @@ class Semantic_Camera_Test(MorseTestCase):
             # Change the orientation of the robot using the v_w socket
             send_dest(teleport_client, morse, 0.0, 0.0, 5.0/4.0 * math.pi)
 
-            # Second test for the sensor, with objects in front
+            # Second test for the sensor, with BlueBox in front
             o = semantic_stream.get()
             objects= o['visible_objects']
-            self.assertEqual(len(objects), 1)
-            self.assertEqual(objects[0]['name'],'BlueBox')
-            self.assertAlmostEqual(objects[0]['position'][0], -3.48, delta=0.1)
-            self.assertAlmostEqual(objects[0]['position'][1], -3.0, delta=0.1)
+            self.assertEqual(objects, [])
 
             send_dest(teleport_client, morse, -5.0, 0.0, math.pi)
 
+            # Third test for the sensor, with RedBox in front
             o = semantic_stream.get()
             objects= o['visible_objects']
             self.assertEqual(len(objects), 1)

commit 83c5fcd705a2c7b36f3442aff5e58ab2844ce57e

[sensor] Fix behaviour of semantic_camera if object has a label
Fix a regression introduced in a463f24fedc, the sensor should return the
label of the object (if it exists) as its name.

Fix cat_and_mouse tutorial.

src/morse/sensors/semantic_camera.py
@@ -183,7 +183,7 @@ class SemanticCamera(morse.sensors.camera.Camera):
                     logger.debug("transform = {t}".format(t=transformation))
                 else:
                     transformation = Transformation3d(obj)
-                obj_dict = {'name': obj.name,
+                obj_dict = {'name': obj.get('Label', obj.name),
                             'description': obj.get('Description', ''),
                             'type': obj.get('Type', ''),
                             'position': transformation.translation,