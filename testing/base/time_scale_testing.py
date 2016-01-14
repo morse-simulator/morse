8aad5d4273b7e63231f83ef53afcfb6a23ea0f3f
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2016-01-14 17:15:09 +0100

    [testing] Rearrange time_scale testing, considering severin's remarks

--- testing/base/time_scale_testing.py
+++ testing/base/time_scale_testing.py
@@ -25,6 +25,7 @@ class TimeScale_Test(MorseMoveTestCase):
         robot = ATRV()
 
         pose = Pose()
+        pose.frequency(60)
         robot.append(pose)
         pose.add_stream('socket')
 
@@ -40,14 +41,19 @@ class TimeScale_Test(MorseMoveTestCase):
         env.add_service('socket')
 
     def send_speed(self, s, morse, v, w, t):
-        start = time.time()
+        """
+        Send the speed (v, w) to the robot controller during real-time t
+        seconds
+        Return the simulated time elapsed (in sec)
+        """
+        start = morse.time()
         self._count = 0
         morse.robot.pose.subscribe(self.count_value)
         s.publish({'v' : v, 'w' : w})
-        morse.sleep(t)
+        time.sleep(t)
         s.publish({'v' : 0.0, 'w' : 0.0})
         morse.robot.pose.unsubscribe(self.count_value)
-        return time.time() - start
+        return morse.time() - start
 
     def count_value(self, value):
         self._count += 1
@@ -62,7 +68,7 @@ class TimeScale_Test(MorseMoveTestCase):
 
             simu.deactivate('robot.teleport')
 
-            precision = 0.125
+            precision = 0.18
             time_precision = 0.04
         
             # Read the start position, it must be (0.0, 0.0, 0.0)
@@ -70,28 +76,38 @@ class TimeScale_Test(MorseMoveTestCase):
 
             v_w = simu.robot.motion
 
-            real_time_elapsed = self.send_speed(v_w, simu, 1.0, 0.0, 2.0)
+            # Normal speed: one real-time second == one simulated second
+            simu_time_elapsed = self.send_speed(v_w, simu, 1.0, 0.0, 2.0)
+            # as a consequence, the robot has moved forward by 2 m
             self.assertAlmostEqualPositionThenFix(simu, [2.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
-            self.assertAlmostEqual(real_time_elapsed, 2.0, delta=time_precision)
-            self.assertEqual(self._count, 121)
+            self.assertAlmostEqual(simu_time_elapsed, 2.0, delta=time_precision)
+            # We expect to get 60 (default fps) * 2 (s) pose measurement
+            self.assertAlmostEqual(self._count, 120, delta = 1)
 
-            real_time_elapsed = self.send_speed(v_w, simu, -1.0, 0.0, 2.0)
+            simu_time_elapsed = self.send_speed(v_w, simu, -1.0, 0.0, 2.0)
             self.assertAlmostEqualPositionThenFix(simu, [0.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
-            self.assertAlmostEqual(real_time_elapsed, 2.0, delta=precision)
-            self.assertEqual(self._count, 121)
+            self.assertAlmostEqual(simu_time_elapsed, 2.0, delta=precision)
+            self.assertAlmostEqual(self._count, 120, delta = 1)
 
 
+            # Slow down the simulation : one real-time second == 500 ms simulated
             simu.rpc('time', 'set_time_scale', 0.5)
-            real_time_elapsed = self.send_speed(v_w, simu, 1.0, 0.0, 2.0)
-            self.assertAlmostEqualPositionThenFix(simu, [2.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
-            self.assertAlmostEqual(real_time_elapsed, 4.0, delta=time_precision)
-            self.assertEqual(self._count, 121)
+            simu_time_elapsed = self.send_speed(v_w, simu, 1.0, 0.0, 2.0)
+            # as a consequence, we only have moved forward by 1m in
+            # 2 real-second (1 simulated second)
+            self.assertAlmostEqualPositionThenFix(simu, [1.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
+            self.assertAlmostEqual(simu_time_elapsed, 1.0, delta=time_precision)
+            # and so we get only 60 measures (as we will simulate 1 sec in simulate time)
+            self.assertAlmostEqual(self._count, 60, delta = 1)
 
+            # Accelerate the simulation : one real-time second == 2 simulated second
             simu.rpc('time', 'set_time_scale', 2.0)
-            real_time_elapsed = self.send_speed(v_w, simu, -1.0, 0.0, 2.0)
-            self.assertAlmostEqualPositionThenFix(simu, [0.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
-            self.assertAlmostEqual(real_time_elapsed, 1.0, delta=precision)
-            self.assertEqual(self._count, 121)
+            simu_time_elapsed = self.send_speed(v_w, simu, -1.0, 0.0, 2.0)
+            # as a consequence, we only have moved downward by 4m in 2 real-second (4 simulated second)
+            self.assertAlmostEqualPositionThenFix(simu, [-3.0, 0.0, 0.10, 0.0, 0.0, 0.0], precision)
+            self.assertAlmostEqual(simu_time_elapsed, 4.0, delta=precision)
+            # alse, we get 240 measures (60 * 4 simulated sec)
+            self.assertAlmostEqual(self._count, 240, delta = 2)
 
 
 ########################## Run these tests ##########################
