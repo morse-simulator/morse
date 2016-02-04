c82659fc694982f09718097211e10d7b6d600f27
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2016-02-04 10:22:58 +0100

    [bindings] Prepare for 1.4
    WHile here, update severin email, and mark it available for python 3.5

--- bindings/pymorse/setup.py
+++ bindings/pymorse/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='pymorse',
-      version='1.3',
+      version='1.4',
       license='BSD 3-clauses',
       description='Python bindings for the Modular OpenRobots Simulation Engine (MORSE)',
       author='Séverin Lemaignan, Pierrick Koch',
-      author_email='severin.lemaignan@epfl.ch, pierrick.koch@laas.fr',
+      author_email='severin.lemaignan@plymouth.ac.uk, pierrick.koch@laas.fr',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
@@ -15,6 +15,7 @@ setup(name='pymorse',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering',
       ],
       url='http://morse.openrobots.org',
