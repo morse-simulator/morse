dd95c9ae54e9227d74f9880e9aa8076a19a16600
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2016-02-25 16:11:27 +0100

    [core/wheeled_robot] Add interface for wheeled_robot

--- src/morse/core/wheeled_robot.py
+++ src/morse/core/wheeled_robot.py
@@ -4,6 +4,7 @@ import morse.core.robot
 from morse.core import blenderapi
 from morse.helpers.components import add_property
 from morse.helpers.joints import Joint6DoF
+import math
 
 class PhysicsWheelRobot(morse.core.robot.Robot):
     """ Abstract base class for robots with wheels that turn as
@@ -58,6 +59,10 @@ class PhysicsWheelRobot(morse.core.robot.Robot):
 
     def get_track_width(self):
         vec = self._wheels['FL'].getVectTo(self._wheels['FR'])
+        return vec[0]
+
+    def get_distance_axle(self):
+        vec = self._wheels['FL'].getVectTo(self._wheels['RL'])
         return vec[0]
 
     def get_wheel_radius(self, wheel_name):
@@ -177,4 +182,148 @@ class PhysicsDifferentialRobot(PhysicsWheelRobot):
 
             logger.debug("New speeds set: left=%.4f, right=%.4f" %
                          (w_ws_l, w_ws_r))
+
+class PhysicsAckermannRobot(PhysicsWheelRobot):
+    """
+    Base class for mobile robots following the Ackermann steering principle
+
+    This base class handles the simulation of the physical interactions
+    between Ackermann-like vehicle and the ground.
+    It assumes the vehicle has 4 wheels.
+    """
+
+    add_property('_max_steering_angle', 45.0, 'max_steering_angle', 'double', 
+                 'The bigger angle possible the vehicle is able to turn \
+                 its front wheel (in degree)')
+
+    def __init__ (self, obj, parent=None):
+        """ Constructor method. """
+        # Call the constructor of the parent class
+        PhysicsWheelRobot.__init__(self, obj, parent)
+
+        # get wheel references and ID's
+        self.get_wheels()
+
+        self._max_steering_angle = math.radians(self._max_steering_angle)
+
+        # construct the vehicle
+        self.build_vehicle()
+
+        self._axle_distance = self.get_distance_axle()
+
+        logger.warn("Using wheel separation of %.4f" % self._trackWidth)
+
+        # Force speed at 0.0 at startup
+        self.apply_vw_wheels(0.0, 0.0)
+
+
+    def build_vehicle(self):
+        """ Apply the constraints to the vehicle parts. """
+        # get track width
+        self._trackWidth = self.get_track_width()
+
+        # set up wheel constraints
+        # add wheels to either suspension arms or vehicle chassis
+        if self._has_suspension:
+            self.build_model_with_suspension()
+        else:
+            self.build_model_without_suspension()
+
+    def build_model_without_suspension(self):
+        """ Add all the constraints to attach the wheels to the body """
+        for index in ['FR', 'FL']:
+            self._wheel_joints[index] = self.attach_front_wheel_to_body(
+                    self._wheels[index], self.bge_object)
+        for index in ['RR', 'RL']:
+            self._wheel_joints[index] = self.attach_rear_wheel_to_body(
+                    self._wheels[index], self.bge_object)
+
+    def attach_front_wheel_to_body(self, wheel, parent):
+        """ Attaches the wheel to the given parent using a 6DOF constraint
+
+        Set the wheel positions relative to the robot in case the
+        chassis was moved by the builder script or manually in blender
+        """
+        joint = Joint6DoF(wheel, parent)
+        joint.limit_rotation_dof('Y', -self._max_steering_angle, self._max_steering_angle)
+        joint.free_rotation_dof('Z')
+        return joint # return a reference to the constraint
+
+
+    def attach_rear_wheel_to_body(self, wheel, parent):
+        """ Attaches the wheel to the given parent using a 6DOF constraint
+
+        Set the wheel positions relative to the robot in case the
+        chassis was moved by the builder script or manually in blender
+        """
+        joint = Joint6DoF(wheel, parent)
+        joint.free_rotation_dof('Z')
+        return joint # return a reference to the constraint
+
+    def apply_vw_wheels(self, vx, vw):
+        """ Apply (v, w) to the parent robot. """
+
+        velocity_control = 'Z' 
+        steering_control = 'Y'
+
+        # calculate desired wheel speeds and set them
+        if abs(vx) < 0.001 and abs(vw) < 0.001:
+            # stop the wheel when velocity is below a given threshold
+            for index in ['RL', 'RR']:
+                self._wheel_joints[index].angular_velocity(velocity_control, 0)
+            for index in ['FR', 'FL']:
+                self._wheel_joints[index].angular_velocity(steering_control, 0)
+
+            self._stopped = True
+        else:
+            # this is need to "wake up" the physic objects if they have
+            # gone to sleep apply a tiny impulse straight down on the
+            # object
+            if self._stopped:
+                self.bge_object.applyImpulse(
+                   self.bge_object.position, (0.0, 0.0, 0.000001))
+
+            # no longer stopped
+            self._stopped = False
+
+            # speed of rear wheels
+            wx = -1.0 * vx / self._wheel_radius
+            for index in ['RL', 'RR']:
+                self._wheel_joints[index].angular_velocity(velocity_control, wx)
+
+            logger.debug("Rear wheel speeds set to %.4f" % wx)
+
+            # Compute angle of steering wheels
+            if abs(vw) > 0.01:
+                radius = vx / vw
+                if abs(radius) < (self._trackWidth / 2):
+                    l_angle = math.copysign(self._max_steering_angle, radius)
+                    r_angle = math.copysign(self._max_steering_angle, radius)
+                else:
+                    cot_l_angle = self._axle_distance / (radius + (self._trackWidth / 2))
+                    cot_r_angle = self._axle_distance / (radius -  (self._trackWidth / 2))
+                    l_angle = math.atan(cot_l_angle)
+                    r_angle = math.atan(cot_r_angle)
+                    logger.debug('virtual angle %f' % (math.atan(self._axle_distance / radius)))
+            else:
+                l_angle = r_angle = 0.0
+
+            logger.info("l_angle %f r_angle %f" % (l_angle, r_angle))
+
+            if abs(l_angle) >= self._max_steering_angle or \
+               abs(r_angle) >= self._max_steering_angle:
+                logger.warning("Constraint (v = %f, w = %f) is not applicable \
+                               due to physical limitation" % (vx, vw))
+
+            current_l_angle = self._wheel_joints['FL'].euler_angle(steering_control)
+            current_r_angle = self._wheel_joints['FR'].euler_angle(steering_control)
+
+            diff_l_angle = l_angle - current_l_angle
+            diff_r_angle = r_angle - current_r_angle
+
+            self._wheel_joints['FL'].angular_velocity(steering_control, diff_l_angle)
+            self._wheel_joints['FR'].angular_velocity(steering_control, diff_r_angle)
+
+            logger.debug("Angle left w %f right w %f" % (diff_l_angle, diff_r_angle))
+
 
