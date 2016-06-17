dcdb930d0a52e43cb281658be43461ded4a35d79
Arnaud Degroote <arnaud.degroote@steery.io>
2016-06-17 09:02:20 +0200

    [testing] Increase the coverage of velocity test
    Test now both kind of computation, i.e. Velocity and Position

--- testing/base/velocity_testing.py
+++ testing/base/velocity_testing.py
@@ -30,10 +30,21 @@ class Velocity_Test(MorseMoveTestCase):
         robot.append(vel)
         vel.add_stream('socket')
 
+        vel_pos = Velocity()
+        robot.append(vel_pos)
+        vel_pos.add_stream('socket')
+        vel_pos.properties(ComputationMode = 'Position')
+
         vel_pi = Velocity()
         robot.append(vel_pi)
         vel_pi.rotate(z = math.pi / 2, y = math.pi / 2)
         vel_pi.add_stream('socket')
+
+        vel_pos_pi = Velocity()
+        robot.append(vel_pos_pi)
+        vel_pos_pi.rotate(z = math.pi / 2, y = math.pi / 2)
+        vel_pos_pi.add_stream('socket')
+        vel_pos_pi.properties(ComputationMode = 'Position')
         
         env = Environment('empty', fastmode = True)
         env.add_service('socket')
@@ -44,8 +55,10 @@ class Velocity_Test(MorseMoveTestCase):
         self.xyw.publish(command)
         morse.sleep(0.1)
 
-        vel = self.vel_stream.get()
+        vel = self.vel_stream.last()
+        vel_pos = self.vel_pos_stream.last()
         vel_pi = self.vel_pi_stream.last()
+        vel_pos_pi = self.vel_pos_pi_stream.last()
 
         self.assertAlmostEqual(vel['linear_velocity'][0], expected[0], delta = delta)
         self.assertAlmostEqual(vel['linear_velocity'][1], expected[1], delta = delta)
@@ -59,6 +72,17 @@ class Velocity_Test(MorseMoveTestCase):
         self.assertAlmostEqual(vel['world_linear_velocity'][1], expected[1], delta = delta)
         self.assertAlmostEqual(vel['world_linear_velocity'][2], expected[2], delta = delta)
 
+        self.assertAlmostEqual(vel_pos['linear_velocity'][0], expected[0], delta = delta)
+        self.assertAlmostEqual(vel_pos['linear_velocity'][1], expected[1], delta = delta)
+        self.assertAlmostEqual(vel_pos['linear_velocity'][2], expected[2], delta = delta)
+
+        self.assertAlmostEqual(vel_pos['angular_velocity'][0], expected[3], delta = delta)
+        self.assertAlmostEqual(vel_pos['angular_velocity'][1], expected[4], delta = delta)
+        self.assertAlmostEqual(vel_pos['angular_velocity'][2], expected[5], delta = delta)
+
+        self.assertAlmostEqual(vel_pos['world_linear_velocity'][0], expected[0], delta = delta)
+        self.assertAlmostEqual(vel_pos['world_linear_velocity'][1], expected[1], delta = delta)
+        self.assertAlmostEqual(vel_pos['world_linear_velocity'][2], expected[2], delta = delta)
 
         self.assertAlmostEqual(vel_pi['linear_velocity'][0], expected[2], delta = delta)
         self.assertAlmostEqual(vel_pi['linear_velocity'][1], - expected[0], delta = delta)
@@ -72,12 +96,26 @@ class Velocity_Test(MorseMoveTestCase):
         self.assertAlmostEqual(vel_pi['world_linear_velocity'][1], expected[1], delta = delta)
         self.assertAlmostEqual(vel_pi['world_linear_velocity'][2], expected[2], delta = delta)
 
+        self.assertAlmostEqual(vel_pos_pi['linear_velocity'][0], expected[2], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['linear_velocity'][1], - expected[0], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['linear_velocity'][2], expected[1], delta = delta)
+
+        self.assertAlmostEqual(vel_pos_pi['angular_velocity'][0], - expected[5], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['angular_velocity'][1], expected[3], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['angular_velocity'][2], expected[2], delta = delta)
+
+        self.assertAlmostEqual(vel_pos_pi['world_linear_velocity'][0], expected[0], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['world_linear_velocity'][1], expected[1], delta = delta)
+        self.assertAlmostEqual(vel_pos_pi['world_linear_velocity'][2], expected[2], delta = delta)
+
     def test_velocity_sensor(self):
         with Morse() as morse:
 
             self.xyw = morse.robot.motion
             self.vel_stream = morse.robot.vel
+            self.vel_pos_stream = morse.robot.vel_pos
             self.vel_pi_stream = morse.robot.vel_pi
+            self.vel_pos_pi_stream = morse.robot.vel_pos_pi
 
             self.assert_velocity(morse, {"x": 1.0, "y": 0.0, "w": 0.0},
                                  [1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
