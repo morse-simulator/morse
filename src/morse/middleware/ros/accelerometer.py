morse-simulator/morse

commit 6fe8aefb36000aa0227d4bd828f9213b61f091da

Added missing header data to accelerometer and infrared
The header data is required for some use cases like graphical
visualisation in rqt

src/morse/middleware/ros/accelerometer.py
@@ -10,6 +10,7 @@ class TwistPublisher(ROSPublisher):
 
     def default(self, ci='unused'):
         twist = Twist()
+        twist.header = self.get_ros_header()
 
         # Fill twist-msg with the values from the sensor
         twist.linear.x = self.data['velocity'][0]

src/morse/middleware/ros/infrared.py
@@ -8,6 +8,7 @@ class RangePublisher(ROSPublisher):
 
     def default(self, ci='unused'):
         msg = Range()
+        msg.header = self.get_ros_header()
         msg.radiation_type = Range.INFRARED
         msg.field_of_view = 20
         msg.min_range = 0

commit d40d23e0e61d6c355f3efe3439e9029ca1ed5453

[mw/ros] Fixed accelerometer message type
Fixe a bug introduced in 6fe8aef:

https://github.com/morse-simulator/morse/commit/6fe8aefb36000aa0227d4bd828f9213b61f091da

src/morse/middleware/ros/accelerometer.py
@@ -1,19 +1,19 @@
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 from morse.middleware.ros import ROSPublisher
 
 class TwistPublisher(ROSPublisher):
     """ Publish the velocity of the acceleromter sensor.
     No angular information, only linear ones.
     """
-    ros_class = Twist
+    ros_class = TwistStamped
 
     def default(self, ci='unused'):
-        twist = Twist()
+        twist = TwistStamped()
         twist.header = self.get_ros_header()
 
         # Fill twist-msg with the values from the sensor
-        twist.linear.x = self.data['velocity'][0]
-        twist.linear.y = self.data['velocity'][1]
-        twist.linear.z = self.data['velocity'][2]
+        twist.twist.linear.x = self.data['velocity'][0]
+        twist.twist.linear.y = self.data['velocity'][1]
+        twist.twist.linear.z = self.data['velocity'][2]
 
         self.publish(twist)