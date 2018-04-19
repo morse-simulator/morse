1637de59e7aea42126f692e75eb508ee2442e371
TestBench02 <julien.cornier@easymile.com>
2018-04-19 17:02:17 +0200

    [sensors] Add ROS publisher for GPS Raw level

--- src/morse/middleware/ros/gps.py
+++ src/morse/middleware/ros/gps.py
@@ -14,3 +14,17 @@ class NavSatFixPublisher(ROSPublisher):
         gps.altitude = self.data['z']
 
         self.publish(gps)
+
+class NavSatFixRawPublisher(ROSPublisher):
+    """ Publish the GPS position of the robot. """
+    ros_class = NavSatFix
+
+    def default(self, ci='unused'):
+        gps = NavSatFix()
+        gps.header = self.get_ros_header()
+
+        gps.latitude = self.data['latitude']
+        gps.longitude = self.data['longitude']
+        gps.altitude = self.data['altitude']
+
+        self.publish(gps)
