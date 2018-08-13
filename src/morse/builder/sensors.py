8c5266a2530c57ddb96d9651a85354d0b3ea07b6
Arnaud Degroote <arnaud.degroote@easymile.com>
2018-08-13 12:12:03 +0200

    [builder] Adjust the definition of VLP16_180, using keep_list
    While here, modify cam_focal to reduce place covered twice

--- src/morse/builder/sensors.py
+++ src/morse/builder/sensors.py
@@ -475,7 +475,13 @@ class VLP16_180(DepthCameraAggregator):
         DepthCameraAggregator.__init__(self, name)
         for i in range(3):
             cam = DepthCamera("cam%i"%i)
-            cam.properties(cam_width = 256, cam_height= 256, cam_focal = 25.0)
+            """
+            cam_focal = 27.7 => hfov ~= 60.0. 
+            As cam_height is half the cam_width, vfov ~= 30.0, so each pixel represent around 0.117°
+            Hence, the keep_list is a reasonnably fair approximation of the real angle of VLP16
+            """
+            cam.properties(cam_width=512, cam_height=256, cam_focal = 27.7,
+                           keep_list=str([0, 17, 34, 51, 68, 85, 102, 120, 137, 154, 171, 188, 205, 222, 239, 256]))
             cam.rotate(x=(i-1) * math.pi / 3)
             cam.frequency(10)
             cam.hide_mesh()
