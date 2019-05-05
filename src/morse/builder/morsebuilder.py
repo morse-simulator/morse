12e6522a7ade18bf2cc63e3104c590d0736ca34e
David Battle <david.battle@missionsystems.com.au>
2019-05-05 12:25:09 +1000

    Modification to append all Blender objects in target file, with a nominated root object

--- src/morse/builder/morsebuilder.py
+++ src/morse/builder/morsebuilder.py
@@ -139,17 +139,27 @@ class Component(AbstractComponent):
         AbstractComponent.__init__(self, filename=filename, category=category)
 
 
-        if blender_object_name is None:
-            imported_objects = self.append_meshes()
-        else:
-            imported_objects = self.append_meshes(objects=[blender_object_name])
-            if not imported_objects:
-                raise MorseBuilderNoComponentError("No object named <%s> in %s" % (blender_object_name, filename))
+        # if blender_object_name is None:
+        #     imported_objects = self.append_meshes()
+        # else:
+        #     imported_objects = self.append_meshes(objects=[blender_object_name])
+        #     if not imported_objects:
+        #         raise MorseBuilderNoComponentError("No object named <%s> in %s" % (blender_object_name, filename))
 
         
-        # Here we use the fact that after appending, Blender select the objects
-        # and the root (parent) object first ( [0] )
-        self.set_blender_object(imported_objects[0])
+        # # Here we use the fact that after appending, Blender select the objects
+        # # and the root (parent) object first ( [0] )
+        # self.set_blender_object(imported_objects[0])
+
+        imported_objects = self.append_meshes()
+
+        if blender_object_name is None:
+            # Here we use the fact that after appending, Blender select the objects
+            # and the root (parent) object first ( [0] )
+            self.set_blender_object(imported_objects[0])
+        else:
+            self.set_blender_object([o for o in imported_objects if o.name == blender_object_name][0])
+
         # If the object has no MORSE logic, add default one
         if make_morseable and category in ['sensors', 'actuators', 'robots'] \
                 and not self.is_morseable():
