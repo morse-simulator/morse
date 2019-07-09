4adf005b2f5519c692bd6ff3fa948fcb90f19e35
Arnaud Degroote <arnaud.degroote@easymile.com>
2019-07-09 14:55:30 +0200

    [middeware/ros] For velocity sensor, do not mix information in vehicle frame and in world frame
    Use everything in the vehicle frame.

--- src/morse/middleware/ros/velocity.py
+++ src/morse/middleware/ros/velocity.py
@@ -5,14 +5,14 @@ from morse.middleware.ros import ROSPublisher
 class TwistStampedPublisher(ROSPublisher):
     """ Publish the twist of the robot. """
     ros_class = TwistStamped
-    default_frame_id = '/map'
+    default_frame_id = '/base_link'
 
     def default(self, ci='unused'):
         twist = TwistStamped()
         twist.header = self.get_ros_header()
-        twist.twist.linear.x = self.data['world_linear_velocity'][0]
-        twist.twist.linear.y = self.data['world_linear_velocity'][1]
-        twist.twist.linear.z = self.data['world_linear_velocity'][2]
+        twist.twist.linear.x = self.data['linear_velocity'][0]
+        twist.twist.linear.y = self.data['linear_velocity'][1]
+        twist.twist.linear.z = self.data['linear_velocity'][2]
         twist.twist.angular.x = self.data['angular_velocity'][0]
         twist.twist.angular.y = self.data['angular_velocity'][1]
         twist.twist.angular.z = self.data['angular_velocity'][2]
