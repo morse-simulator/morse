bb9ea4c1f885159906dd7000a50380b0d3faaa38
Arnaud Degroote <arnaud.degroote@easymile.com>
2019-07-11 14:11:25 +0200

    [mw/ros] Use an AccelerationWithCovarianceStamped msg for acceleromter instead of a TwistStamped
    Keep the previous one for compatibility, but one should use Velocity
    sensor if it needs velocity

--- src/morse/middleware/ros/accelerometer.py
+++ src/morse/middleware/ros/accelerometer.py
@@ -1,5 +1,7 @@
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, \
+                              AccelWithCovarianceStamped
 from morse.middleware.ros import ROSPublisher
+
 
 class TwistPublisher(ROSPublisher):
     """ Publish the velocity of the acceleromter sensor.
@@ -17,3 +19,22 @@ class TwistPublisher(ROSPublisher):
         twist.twist.linear.z = self.data['velocity'][2]
 
         self.publish(twist)
+
+
+class AccelWithCovarianceStampedPublisher(ROSPublisher):
+
+    ros_class = AccelWithCovarianceStamped
+
+    def default(self, ci='unused'):
+        accel = AccelWithCovarianceStamped()
+        accel.header = self.get_ros_header()
+
+        accel.accel.accel.linear.x = self.data['acceleration'][0]
+        accel.accel.accel.linear.y = self.data['acceleration'][1]
+        accel.accel.accel.linear.z = self.data['acceleration'][2]
+
+        accel.accel.accel.angular.x = self.data['angular_acceleration'][0]
+        accel.accel.accel.angular.y = self.data['angular_acceleration'][1]
+        accel.accel.accel.angular.z = self.data['angular_acceleration'][2]
+
+        self.publish(accel)
