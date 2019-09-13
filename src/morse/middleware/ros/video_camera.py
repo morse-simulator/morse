0b35ad600e4b3750ae624ad6856b8848d3698e5d
dj <dj.missionsystems@gmail.com>
2019-09-13 16:17:58 +1000

    Fixed an issue stopping rtabmap using MORSE cameras through ROS

--- src/morse/middleware/ros/video_camera.py
+++ src/morse/middleware/ros/video_camera.py
@@ -60,7 +60,7 @@ class CameraPublisher(ROSPublisherTF):
         camera_info.height = image.height
         camera_info.width = image.width
         camera_info.distortion_model = 'plumb_bob'
-        camera_info.D = [0]
+        camera_info.D = []
         camera_info.K = [intrinsic[0][0], intrinsic[0][1], intrinsic[0][2],
                          intrinsic[1][0], intrinsic[1][1], intrinsic[1][2],
                          intrinsic[2][0], intrinsic[2][1], intrinsic[2][2]]
